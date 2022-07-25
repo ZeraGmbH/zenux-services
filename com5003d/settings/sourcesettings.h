4aa8325471c53b93ecf17e11141a1c48481e438b
Andreas Müller <schnitzeltony@gmail.com>
2022-07-25 21:22:00 +0200

    com5003d/settings/sourcesettings.h: sync to mt310s2
    git diff -w (later git show -w) helps a lot here:
    
    * add some includes
    * remove some newlines
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- com5003d/settings/sourcesettings.h
+++ com5003d/settings/sourcesettings.h
@@ -2,50 +2,49 @@
 #define SOURCESETTINGS_H
 
 #include <xmlsettings.h>
+#include <xmlconfigreader.h>
+#include <QString>
+#include <QList>
 
 namespace SourceSystem
 {
-enum configstate
-{
-    cfg0Alias,
-    cfg1Alias,
-    cfg2Alias,
-    cfg3Alias,
-    cfg0dspserver,
-    cfg1dspserver,
-    cfg2dspserver,
-    cfg3dspserver,
-    cfg0dspchannel,
-    cfg1dspchannel,
-    cfg2dspchannel,
-    cfg3dspchannel,
-    cfg0avail,
-    cfg1avail,
-    cfg2avail,
-    cfg3avail
-};
-
-struct cChannelSettings // what we want to get configured
-{
-    QString m_sAlias; // the names channel
-    quint16 m_nDspServerPort; // where to control the channel
-    quint8 m_nDspChannel; // where to configure the channel
-    bool avail; // is this channel available ?
-};
+    enum configstate
+    {
+        cfg0Alias,
+        cfg1Alias,
+        cfg2Alias,
+        cfg3Alias,
+        cfg0dspserver,
+        cfg1dspserver,
+        cfg2dspserver,
+        cfg3dspserver,
+        cfg0dspchannel,
+        cfg1dspchannel,
+        cfg2dspchannel,
+        cfg3dspchannel,
+        cfg0avail,
+        cfg1avail,
+        cfg2avail,
+        cfg3avail
+    };
+    struct cChannelSettings // what we want to get configured
+    {
+        QString m_sAlias; // the names channel
+        quint16 m_nDspServerPort; // where to control the channel
+        quint8 m_nDspChannel; // where to configure the channel
+        bool avail; // is this channel available ?
+    };
 }
 
 class cSourceSettings : public XMLSettings
 {
     Q_OBJECT
-
 public:
     cSourceSettings(Zera::XMLConfig::cReader *xmlread);
     ~cSourceSettings();
     QList<SourceSystem::cChannelSettings*>& getChannelSettings();
-
 public slots:
     virtual void configXMLInfo(QString key);
-
 private:
     QList<SourceSystem::cChannelSettings*> m_ChannelSettingsList;
 };
