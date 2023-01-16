f159cfc63856f0e13163e4c481e50c94bfa7646b
Andreas Müller <schnitzeltony@gmail.com>
2023-01-17 00:05:33 +0100

    cSCHeadChannel: prettify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/scpi-interfaces/scheadchannel.cpp
+++ zenux-service-common/lib/scpi-interfaces/scheadchannel.cpp
@@ -9,14 +9,12 @@ cSCHeadChannel::cSCHeadChannel(cSCPI *scpiinterface, QString description, quint8
     m_bAvail = cSettings->m_avail;
 }
 
-
 void cSCHeadChannel::initSCPIConnection(QString leadingNodes)
 {
-    cSCPIDelegate* delegate;
 
     if (leadingNodes != "")
         leadingNodes += ":";
-
+    cSCPIDelegate* delegate;
     delegate = new cSCPIDelegate(QString("%1%2").arg(leadingNodes).arg(m_sName),"ALIAS", SCPI::isQuery, m_pSCPIInterface, SCHEADChannel::cmdAlias);
     m_DelegateList.append(delegate);
     connect(delegate, &cSCPIDelegate::execute, this, &cSCHeadChannel::executeCommand);
@@ -24,7 +22,6 @@ void cSCHeadChannel::initSCPIConnection(QString leadingNodes)
     m_DelegateList.append(delegate);
     connect(delegate, &cSCPIDelegate::execute, this, &cSCHeadChannel::executeCommand);
 }
-
 
 void cSCHeadChannel::executeCommand(int cmdCode, cProtonetCommand *protoCmd)
 {
@@ -37,59 +34,46 @@ void cSCHeadChannel::executeCommand(int cmdCode, cProtonetCommand *protoCmd)
         protoCmd->m_sOutput = m_ReadChannelStatus(protoCmd->m_sInput);
         break;
     }
-
     if (protoCmd->m_bwithOutput)
         emit cmdExecutionDone(protoCmd);
 }
-
 
 QString &cSCHeadChannel::getName()
 {
     return m_sName;
 }
 
-
 QString &cSCHeadChannel::getAlias()
 {
     return m_sAlias;
 }
-
 
 QString &cSCHeadChannel::getDescription()
 {
     return m_sDescription;
 }
 
-
 bool cSCHeadChannel::isAvail()
 {
     return m_bAvail;
 }
 
-
 QString cSCHeadChannel::m_ReadAlias(QString &sInput)
 {
     cSCPICommand cmd = sInput;
-
     if (cmd.isQuery())
         return m_sAlias;
     else
         return SCPI::scpiAnswer[SCPI::nak];
 }
 
-
 QString cSCHeadChannel::m_ReadChannelStatus(QString &sInput)
 {
     cSCPICommand cmd = sInput;
-
-    if (cmd.isQuery())
-    {
-        quint32 r;
-        r = ((m_bAvail) ? 0 : 1 << 31);
-            return QString("%1").arg(r);
+    if (cmd.isQuery()) {
+        quint32 r = ((m_bAvail) ? 0 : 1 << 31);
+        return QString("%1").arg(r);
     }
     else
         return SCPI::scpiAnswer[SCPI::nak];
 }
-
-
