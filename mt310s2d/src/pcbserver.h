d248653697c3ad766f8389fb9fb3a4d5235a1909
Andreas Müller <schnitzeltony@gmail.com>
2023-01-18 22:24:47 +0100

    Remove cStatusInterface pointers
    They are not used in cPCBServer and hidden by per service specific classes!!!
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/src/pcbserver.h
+++ mt310s2d/src/pcbserver.h
@@ -25,7 +25,6 @@ enum commands
 };
 }
 
-class cStatusInterface;  // forward
 class cI2CSettings;
 class FInSettings;
 class ScInSettings;
@@ -52,7 +51,6 @@ public:
     FInSettings* m_finSettings;
     ScInSettings* m_pSCHeadSettings;
     HkInSettings* m_hkInSettings;
-    cStatusInterface* m_pStatusInterface;
 public slots:
     void sendAnswerProto(cProtonetCommand* protoCmd);
 protected:
