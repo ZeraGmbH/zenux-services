aa8e7743b14e2d1d5fe00d09a6b2996527d09600
Andreas Müller <schnitzeltony@gmail.com>
2023-01-18 22:54:38 +0100

    Move HkInSettings pointers to per project specific servers
    That is where they are used
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/src/mt310s2d.h
+++ mt310s2d/src/mt310s2d.h
@@ -8,6 +8,7 @@
 #include "ctrlsettings.h"
 #include "i2csettings.h"
 #include "sensesettings.h"
+#include "hkinsettings.h"
 #include <QTimer>
 
 class QStateMachine;
@@ -40,6 +41,7 @@ public:
     cStatusInterface* m_pStatusInterface;
     cI2CSettings* m_pI2CSettings;
     cSenseSettings* m_pSenseSettings;
+    HkInSettings* m_hkInSettings;
     cSystemInterface* m_pSystemInterface;
     cSenseInterface* m_pSenseInterface;
     cSamplingInterface* m_pSamplingInterface;
