8f0137a8e0a6272748f65c1c19bb8d7305e83431
Andreas Müller <schnitzeltony@gmail.com>
2023-01-25 22:37:42 +0100

    zenux-service-common/asynchnotificationsinterface: use addDelegate
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/scpi-interfaces/asynchnotificationsinterface.cpp
+++ zenux-service-common/lib/scpi-interfaces/asynchnotificationsinterface.cpp
@@ -15,13 +15,8 @@ AsynchNotificationsInterface::AsynchNotificationsInterface(cSCPI *scpiInterface)
 void AsynchNotificationsInterface::initSCPIConnection(QString leadingNodes)
 {
     ensureTrailingColonOnNonEmptyParentNodes(leadingNodes);
-    cSCPIDelegate* delegate;
-    delegate = new cSCPIDelegate(QString("%1SERVER").arg(leadingNodes), "REGISTER", SCPI::isCmdwP, m_pSCPIInterface, cmdRegister);
-    m_DelegateList.append(delegate);
-    //connect(delegate, &cSCPIDelegate::execute, this, &AsynchNotificationsInterface::executeCommand);
-    delegate = new cSCPIDelegate(QString("%1SERVER").arg(leadingNodes), "UNREGISTER", SCPI::isCmdwP, m_pSCPIInterface, cmdUnregister);
-    m_DelegateList.append(delegate);
-    //connect(delegate, &cSCPIDelegate::execute, this, &AsynchNotificationsInterface::executeCommand);
+    addDelegate(new cSCPIDelegate(QString("%1SERVER").arg(leadingNodes), "REGISTER", SCPI::isCmdwP, m_pSCPIInterface, cmdRegister));
+    addDelegate(new cSCPIDelegate(QString("%1SERVER").arg(leadingNodes), "UNREGISTER", SCPI::isCmdwP, m_pSCPIInterface, cmdUnregister));
 }
 
 void AsynchNotificationsInterface::executeCommand(int cmdCode, cProtonetCommand *protoCmd)
