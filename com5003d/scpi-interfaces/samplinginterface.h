e8604dae787cc95ede999e88cafd8ba68029a0ed
Andreas Müller <schnitzeltony@gmail.com>
2023-01-29 21:22:55 +0100

    com5003d/samplinginterface: Synchronize to mt310s2's varaiant
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- com5003d/scpi-interfaces/samplinginterface.h
+++ com5003d/scpi-interfaces/samplinginterface.h
@@ -1,60 +1,43 @@
 #ifndef SAMPLINGINTERFACE_H
 #define SAMPLINGINTERFACE_H
 
+#include "com5003d.h"
+#include "scpiconnection.h"
 #include "samplerange.h"
 #include "resource.h"
-#include "notificationstring.h"
-#include <QObject>
 #include <QList>
 #include <QStringList>
 
 namespace SamplingSystem
 {
-
-const QString Version = "V1.00";
-
-enum Commands
-{
-    cmdVersion,
-    cmdSampleRate,
-    cmdChannelCat,
-    cmdChannelAlias,
-    cmdChannelType,
-    cmdChannelStatus,
-    cmdChannelRange,
-    cmdChannelRangeCat,
-    cmdPLL,
-    cmdPLLCat
-};
-
+    const QString Version = "V1.00";
+    enum Commands
+    {
+        cmdVersion,
+        cmdSampleRate,
+        cmdChannelCat,
+        cmdChannelAlias,
+        cmdChannelType,
+        cmdChannelStatus,
+        cmdChannelRange,
+        cmdChannelRangeCat,
+        cmdPLL,
+        cmdPLLCat
+    };
 }
-
-class cCOM5003dServer;
 
 class cSamplingInterface: public cResource
 {
     Q_OBJECT
-
 public:
     cSamplingInterface(cCOM5003dServer* server);
     virtual void initSCPIConnection(QString leadingNodes) override;
     virtual void registerResource(RMConnection *rmConnection, quint16 port) override;
     virtual void unregisterResource(RMConnection *rmConnection) override;
-
 protected:
     void executeProtoScpi(int cmdCode, cProtonetCommand* protoCmd) override;
-
 private:
-    cCOM5003dServer *m_pMyServer;
-    quint16 m_nType;
-    QString m_sVersion;
-    QString m_sAlias;
-    QString m_sName; // the samplingsystem's name
-    QString m_sDescription; // the samplingsystem's brief description
-    bool m_bAvail; // is this sampling system available ?
-    QList<cSampleRange*> m_SampleRangeList;
-    QStringList m_pllChannelList;
-
+    void setNotifierSampleChannelRange();
     QString m_ReadVersion(QString& sInput);
     QString m_ReadSampleRate(QString& sInput);
     QString m_ReadSamplingChannelCatalog(QString& sInput);
@@ -66,10 +49,16 @@ private:
     QString m_ReadWritePLL(QString& sInput);
     QString m_ReadPLLCatalog(QString& sInput);
 
+    cCOM5003dServer *m_pMyServer;
+    quint16 m_nType;
+    QString m_sVersion;
+    QString m_sAlias;
+    QString m_sName; // the samplingsystem's name
+    QString m_sDescription; // the samplingsystem's brief description
+    bool m_bAvail; // is this sampling system available ?
+    QList<cSampleRange*> m_SampleRangeList;
+    QStringList m_pllChannelList;
     NotificationString notifierSampleChannelRange;
-
-    void setNotifierSampleChannelRange();
-
 };
 
 #endif // SAMPLINGINTERFACE_H
