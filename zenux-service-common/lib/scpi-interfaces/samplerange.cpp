12d954365e43895cf21d23e8399d18f86e76cff3
Andreas Müller <schnitzeltony@gmail.com>
2023-01-29 21:10:50 +0100

    cSampleRange: prettify
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/scpi-interfaces/samplerange.cpp
+++ zenux-service-common/lib/scpi-interfaces/samplerange.cpp
@@ -1,8 +1,9 @@
-#include <QString>
-#include <scpi.h>
-
 #include "samplerange.h"
-#include "protonetcommand.h"
+
+enum SampleRangeCommands
+{
+    SampleRangeSamples
+};
 
 cSampleRange::cSampleRange(cSCPI *scpiinterface, QString name, quint16 srate, quint8 selcode) :
     ScpiConnection(scpiinterface),
@@ -12,54 +13,44 @@ cSampleRange::cSampleRange(cSCPI *scpiinterface, QString name, quint16 srate, qu
 {
 }
 
-
 void cSampleRange::initSCPIConnection(QString leadingNodes)
 {
     ensureTrailingColonOnNonEmptyParentNodes(leadingNodes);
     addDelegate(QString("%1%2").arg(leadingNodes).arg(m_sName),"SRATE", SCPI::isQuery, m_pSCPIInterface, SampleRangeSamples);
 }
 
-
 QString &cSampleRange::getName()
 {
     return m_sName;
 }
-
 
 quint8 cSampleRange::getSelCode()
 {
     return m_nSelCode;
 }
 
-
 quint16 cSampleRange::getSRate()
 {
     return m_nSRate;
 }
-
 
 void cSampleRange::executeProtoScpi(int cmdCode, cProtonetCommand *protoCmd)
 {
     switch (cmdCode)
     {
     case SampleRangeSamples:
-        protoCmd->m_sOutput = m_ReadSRate(protoCmd->m_sInput);
+        protoCmd->m_sOutput = readSRate(protoCmd->m_sInput);
         break;
     }
-
     if (protoCmd->m_bwithOutput)
         emit cmdExecutionDone(protoCmd);
 }
 
-
-QString cSampleRange::m_ReadSRate(QString &sInput)
+QString cSampleRange::readSRate(QString &sInput)
 {
     cSCPICommand cmd = sInput;
-
     if (cmd.isQuery())
-    {
         return QString("%1").arg(m_nSRate);
-    }
     else
         return SCPI::scpiAnswer[SCPI::nak];
 }
