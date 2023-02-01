506067e58af0e85e6e735081a3d50d200b46f618
Anuja Mutalik <a.mutalik@zera.de>
2023-02-01 13:57:50 +0100

    Define scpi-interface in tests for every PCBTestServer
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- zenux-service-common/testlib/pcbtestserver.cpp
+++ zenux-service-common/testlib/pcbtestserver.cpp
@@ -1,12 +1,19 @@
 #include "pcbtestserver.h"
 #include "adjustmentstatusnull.h"
 
-PCBTestServer::PCBTestServer(QString name, QString version) :
-    cPCBServer(name, version)
+PCBTestServer::PCBTestServer(QString name, QString version, cSCPI *scpiInterface) :
+    cPCBServer(name, version, scpiInterface)
 {
     m_adjustmentStatusNull = new AdjustmentStatusNull();
+    scpiConnectionList.append(this);
     scpiConnectionList.append(m_pStatusInterface = new cStatusInterface(getSCPIInterface(), m_adjustmentStatusNull));
     initSCPIConnections();
+}
+
+PCBTestServer::~PCBTestServer()
+{
+    if (m_adjustmentStatusNull) delete m_adjustmentStatusNull;
+    if (m_pStatusInterface) delete m_pStatusInterface;
 }
 
 void PCBTestServer::doConfiguration()
