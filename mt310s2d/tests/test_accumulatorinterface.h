d6092006ef5e372a9c6133d9798263b32f61b45f
Asmae Kaouissi <a.kaouissi@zera.de>
2023-02-20 15:27:43 +0100

    More tests on AccumulatorInterface
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- mt310s2d/tests/test_accumulatorinterface.h
+++ mt310s2d/tests/test_accumulatorinterface.h
@@ -2,12 +2,21 @@
 #define TEST_ACCUMULATORINTERFACE_H
 
 #include <QObject>
+#include <memory>
+#include "accumulatorinterface.h"
 
 class test_accumulatorinterface : public QObject
 {
     Q_OBJECT
 private slots:
-    void Compile();
+    void init();
+    void cleanup();
+
+    void findObject();
+    void readAccumulatorStatus();
+private:
+    std::unique_ptr<cSCPI> m_scpiInterface;
+    AccumulatorInterface *m_accumulator;
 };
 
 #endif // TEST_ACCUMULATORINTERFACE_H
