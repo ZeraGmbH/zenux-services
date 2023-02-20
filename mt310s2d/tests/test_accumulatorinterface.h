426f28939bec0cb1452d68e035262f7ea09295b7
Asmae Kaouissi <a.kaouissi@zera.de>
2023-02-20 14:37:21 +0100

    Introduce and test AccumulatorInterface
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

---
+++ mt310s2d/tests/test_accumulatorinterface.h
@@ -0,0 +1,13 @@
+#ifndef TEST_ACCUMULATORINTERFACE_H
+#define TEST_ACCUMULATORINTERFACE_H
+
+#include <QObject>
+
+class test_accumulatorinterface : public QObject
+{
+    Q_OBJECT
+private slots:
+    void Compile();
+};
+
+#endif // TEST_ACCUMULATORINTERFACE_H
