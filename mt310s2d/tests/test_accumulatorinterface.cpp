f6ec51493c2dac0cb1c304869067f9dce72c437e
Anuja Mutalik <a.mutalik@zera.de>
2023-03-01 14:44:55 +0100

    accumulatorinterface: Check accu status & Soc only in polling timer
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- mt310s2d/tests/test_accumulatorinterface.cpp
+++ mt310s2d/tests/test_accumulatorinterface.cpp
@@ -1,4 +1,6 @@
 #include "test_accumulatorinterface.h"
+#include <timerfactoryqtfortest.h>
+#include <timemachinefortest.h>
 #include <QTest>
 
 QTEST_MAIN(test_accumulatorinterface);
@@ -8,16 +10,16 @@ static const char *systemAccumulatorSoc ="SYSTEM:ACCUMULATOR:SOC?";
 
 void test_accumulatorinterface::init()
 {
+    TimerFactoryQtForTest::enableTest();
     m_scpiInterface = std::make_unique<cSCPI>("");
-    m_atmelSysCntrl = new AtmelSysCntrlTest("", 0, 0);
-    m_accumulator = new AccumulatorInterface(m_scpiInterface.get(), m_atmelSysCntrl);
+    m_atmelSysCntrl = std::make_unique<AtmelSysCntrlTest>("", 0, 0);
+    m_accumulator = new AccumulatorInterface(m_scpiInterface.get(), m_atmelSysCntrl.get());
     m_accumulator->initSCPIConnection("");
 }
 
 void test_accumulatorinterface::cleanup()
 {
     delete m_accumulator;
-    delete m_atmelSysCntrl;
 }
 
 void test_accumulatorinterface::findObjects()
@@ -28,6 +30,7 @@ void test_accumulatorinterface::findObjects()
 
 void test_accumulatorinterface::readAccumulatorStatus()
 {
+    TimeMachineForTest::getInstance()->processTimers(1500);
     std::unique_ptr<cProtonetCommand> protoCmd = std::make_unique<cProtonetCommand>(nullptr, false, false, QByteArray(), 0, systemAccumulatorStatus);
     cSCPIObject* scpiObject = m_scpiInterface->getSCPIObject(systemAccumulatorStatus);
     cSCPIDelegate* scpiDelegate = static_cast<cSCPIDelegate*>(scpiObject);
@@ -37,6 +40,7 @@ void test_accumulatorinterface::readAccumulatorStatus()
 
 void test_accumulatorinterface::readAccumulatorSoc()
 {
+    TimeMachineForTest::getInstance()->processTimers(1500);
     std::unique_ptr<cProtonetCommand> protoCmd = std::make_unique<cProtonetCommand>(nullptr, false, false, QByteArray(), 0, systemAccumulatorSoc);
     cSCPIObject* scpiObject = m_scpiInterface->getSCPIObject(systemAccumulatorSoc);
     cSCPIDelegate* scpiDelegate = static_cast<cSCPIDelegate*>(scpiObject);
