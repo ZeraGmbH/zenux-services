d11b9cff0fab99f8427c5ebd7856b4fd1145f291
Asmae Kaouissi <a.kaouissi@zera.de>
2023-05-12 10:45:00 +0200

    atmeli2cmultiplexerfactory: Make use of std::make_shared
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- zenux-service-common/lib/micro-controller-io/atmeli2cmultiplexerfactory.cpp
+++ zenux-service-common/lib/micro-controller-io/atmeli2cmultiplexerfactory.cpp
@@ -4,10 +4,10 @@
 
 I2cMuxerInterface::Ptr AtmelI2cMultiplexerFactory::createClampMuxer(QString deviceNode, ushort i2cMuxAdress, quint8 ctrlChannel)
 {
-    return I2cMuxerInterface::Ptr(new I2cMuxer(deviceNode, i2cMuxAdress, (ctrlChannel-4) | 8, 0));
+    return std::make_shared<I2cMuxer>(deviceNode, i2cMuxAdress, (ctrlChannel-4) | 8, 0);
 }
 
 I2cMuxerInterface::Ptr AtmelI2cMultiplexerFactory::createNullMuxer()
 {
-    return I2cMuxerInterface::Ptr(new I2cMuxerNull());
+    return std::make_shared<I2cMuxerNull>();
 }
