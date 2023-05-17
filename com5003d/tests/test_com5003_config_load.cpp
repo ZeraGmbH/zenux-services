17153540f83f05962075b6c463f467f005480187
Andreas Müller <schnitzeltony@gmail.com>
2023-05-17 23:07:59 +0200

    com5003d: Add basic test on config load
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ com5003d/tests/test_com5003_config_load.cpp
@@ -0,0 +1,20 @@
+#include "test_com5003_config_load.h"
+#include "xmlconfigreader.h"
+#include <QSignalSpy>
+#include <QTest>
+
+QTEST_MAIN(test_com5003_config_load)
+
+void test_com5003_config_load::fileFound()
+{
+    QFile tmpXmlConfigFile(QStringLiteral(CONFIG_SOURCES_COM5003D) + "/" + "com5003d.xml");
+    qInfo("%s", qPrintable(tmpXmlConfigFile.fileName()));
+    QVERIFY(tmpXmlConfigFile.exists());
+}
+
+void test_com5003_config_load::fileLoaded()
+{
+    Zera::XMLConfig::cReader configReader;
+    QVERIFY(configReader.loadSchema(QStringLiteral(CONFIG_SOURCES_COM5003D) + "/" + "com5003d.xsd"));
+    QVERIFY(configReader.loadXMLFile(QStringLiteral(CONFIG_SOURCES_COM5003D) + "/" + "com5003d.xml"));
+}
