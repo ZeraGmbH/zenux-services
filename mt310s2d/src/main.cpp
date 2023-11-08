9172f92343d0cb284a842d0661defddd6f8cb7ee
Andreas Müller <schnitzeltony@gmail.com>
2023-11-08 22:38:15 +0100

    mt310s2d: Remove unused code
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/src/main.cpp
+++ mt310s2d/src/main.cpp
@@ -20,34 +20,7 @@ int main( int argc, char *argv[] )
     QCoreApplication* app = new QCoreApplication (argc, argv);
     cMT310S2dServer* mt310s2d = new cMT310S2dServer(); // this is our server
 
-    int r;
-
-#if !defined(MT310S2DDEBUG) && !defined(SYSTEMD_NOTIFICATION)
-    pid_t pid;
-    if ( (pid=fork() ) < 0 ) // we generate a child process
-    {
-        qCritical("fork() failed\n") ; // error message to syslogd if not
-        return (1);
-    }
-
-    if (pid==0) // if we have a child process now
-    {
-        syslog(LOG_INFO,"mt310s2d server child process created\n");
-        chdir ("/"); // it becomes a place to live
-        setsid();
-        close (STDIN_FILENO); // we disconnect from std io and std error o
-        close (STDOUT_FILENO);
-        close (STDERR_FILENO);
-        open ("/dev/null",O_RDWR); // a null device for ev. created output
-        dup (STDIN_FILENO);
-        dup (STDOUT_FILENO);
-        r = app->exec(); // and runs as daemon now
-
-    }
-#else
-        r = app->exec(); // id DEBUG -> no fork -> server runs in foreground
-#endif // MT310S2DDEBUG
-
+    int r = app->exec();
     if (r == parameterError)
         qCritical("Abort, wrong parameter count\n") ;
     if (r == pipeError)
