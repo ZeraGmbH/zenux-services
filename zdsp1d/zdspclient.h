aab0e31f674eb0cedf85b2fee76306225e14452e
Andreas Müller <schnitzeltony@gmail.com>
2023-11-11 20:58:24 +0100

    zdsp1d: Split out cZDSP1Client into sepearate h/cpp files
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zdsp1d/zdspclient.h
@@ -0,0 +1,65 @@
+#ifndef ZDSPCLIENT_H
+#define ZDSPCLIENT_H
+
+#include "dsp.h"
+#include <xiqnetpeer.h>
+
+class cZDSP1Server;
+
+class cZDSP1Client
+{
+public:
+    cZDSP1Client(){}
+    cZDSP1Client(int socket, XiQNetPeer *netclient, cZDSP1Server *server);
+    ~cZDSP1Client(){} //  allokierten speicher ggf. freigeben
+
+    QString& SetRavList(QString&);
+    QString& GetRavList();
+    QString& SetCmdListDef(QString& );
+    QString& GetCmdListDef();
+    QString& SetCmdIntListDef(QString&);
+    QString& GetCmdIntListDef();
+    void SetEncryption(int);
+    int GetEncryption();
+    bool GenCmdLists(QString&, ulong, ulong); // baut die cmdlisten  für den dsp zusammen wenn fehler -> false
+    cDspCmd GenDspCmd(QString&, bool*, ulong, ulong); // generiert ein dsp kommando aus einem string
+    QString &readActValues(QString&); // liess die messergebnisse (liste)
+    bool isActive();
+    void SetActive(bool); // merkt sich in m_bActive ob diese liste aktiv ist
+    ulong setStartAdr(ulong, ulong); // zum relokalisieren der userdaten
+    QString &DspVarListRead(QString&); // lesen dsp daten ganze Liste
+    bool DspVar(QString&,int&); // einen int (32bit) wert lesen
+    bool DspVar(QString&,float&); // eine float wert lesen
+    sDspVar* DspVarRead(QString&,QByteArray*); // lesen dsp variable;  name , länge stehen im parameter string; werte im anschluss im qbytearray
+    const char* DspVarWriteRM(QString&); // dito schreiben mit rückmeldung
+    bool DspVarWrite(QString&);  // schreiben  true wenn ok
+    QList<cDspCmd>& GetDspCmdList(); // damit der server die komplette liste aller clients
+    QList<cDspCmd>& GetDspIntCmdList(); // an den dsp übertragen kann
+    int getSocket();
+    cDspVarResolver DspVarResolver; // zum auflösen der variablen aus kommandos
+    int sock; // socket für den die verbindung besteht
+    XiQNetPeer* m_pNetClient; // our network client
+
+private:
+    void init(int socket, XiQNetPeer *netclient, cZDSP1Server* server);
+    cZDSP1Server* myServer;
+    bool m_bActive;
+    bool GenCmdList(QString&, QList<cDspCmd>& ,QString&,ulong,ulong);
+    bool syntaxCheck(QString&);
+
+    int Encryption;
+    char* qSEncryption(char*,int );
+    QString sOutput;
+    QString m_sCmdListDef; // kommando liste defintion
+    QString m_sIntCmdListDef; // interrupt kommando  liste defintion
+
+    //    ulong m_nStartAdr; // die absolute adresse an der ein variablen "block" im dsp steht
+    //    int m_nlen; // länge des gesamten datenblocks (in float bzw. long)
+    QList<cDspClientVar> m_DspVarList; // liste mit variablen beschreibungen
+    QList<cDspCmd> m_DspCmdList; // liste mit dsp kommandos (periodisch)
+    QList<cDspCmd>  m_DspIntCmdList; // liste mit dsp kommandos (interrupt)
+    QVector<sDspVar> varArray; // array von sDspVar
+    sMemSection msec; // eine memory section für den DspVarResolver für die variablen des clients
+
+};
+#endif // ZDSPCLIENT_H
