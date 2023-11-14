6b4eba29789ae305f22033e642837a1825603f7a
Anuja Mutalik <a.mutalik@zera.de>
2023-11-14 06:35:05 +0100

    Introduce mocksec1000d
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

---
+++ zenux-service-common/testlib/mocksec1000d.cpp
@@ -0,0 +1,12 @@
+#include "mocksec1000d.h"
+
+MockSec1000d::MockSec1000d() :
+    MockPcbServer("sec1000d")
+{
+    m_secSettings = std::make_unique<SecCalculatorSettings>(getConfigReader());
+    m_secInputSettings = std::make_unique<SecInputSettings>(getConfigReader());
+    setXmlSettings(XmlSettingsList{m_secSettings.get(), m_secInputSettings.get()});
+    m_secResource = std::make_unique<SecGroupResourceAndInterface>(-1, m_secSettings.get(), m_secInputSettings.get(), nullptr);
+    setResources(ResourcesList{m_secResource.get()});
+    start();
+}
