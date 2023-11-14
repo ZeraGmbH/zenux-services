a9920d728834649c658bdd7195a27101ee7c8d58
Anuja Mutalik <a.mutalik@zera.de>
2023-11-14 16:56:31 +0100

    mockmt310s2d: Add cSamplingInterface
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- zenux-service-common/testlib/mockmt310s2d.h
+++ zenux-service-common/testlib/mockmt310s2d.h
@@ -5,6 +5,7 @@
 #include "foutgroupresourceandinterface.h"
 #include "hkingroupresourceandinterface.h"
 #include "scingroupresourceandinterface.h"
+#include "samplinginterface.h"
 #include <memory>
 
 class MockMt310s2d : public MockPcbServer
@@ -16,9 +17,11 @@ private:
     std::unique_ptr<FOutSettings> m_foutSettings;
     std::unique_ptr<HkInSettings> m_hkInSettings;
     std::unique_ptr<ScInSettings> m_scInSettings;
+    std::unique_ptr<SamplingSettings> m_samplingSettings;
     std::unique_ptr<FOutGroupResourceAndInterface> m_foutResource;
     std::unique_ptr<HkInGroupResourceAndInterface> m_hkInResource;
     std::unique_ptr<ScInGroupResourceAndInterface> m_scInResource;
+    std::unique_ptr<cSamplingInterface> m_samplingInterface;
 };
 
 #endif // MOCKMT310S2D_H
