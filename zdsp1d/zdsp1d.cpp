e64446fe48f622478eda1e671a31a841af7e8d8b
Andreas Müller <schnitzeltony@gmail.com>
2023-11-16 13:36:40 +0100

    Interrupt signals: Move buffers into functions using them and make them const
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zdsp1d/zdsp1d.cpp
+++ zdsp1d/zdsp1d.cpp
@@ -67,10 +67,9 @@ static char dspnrunning[12]= "not running";
 cZDSP1Server* DSPServer;
 
 int pipeFD[2];
-static char pipeFDBuf[2] = "I";
-
 void SigHandler(int)
 {
+    const static char pipeFDBuf[2] = "I";
     write(pipeFD[1], pipeFDBuf, 1);
 }
 
