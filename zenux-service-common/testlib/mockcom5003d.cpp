0433b17324c8a071afb1595d4f3f4ecab2e8dd14
Anuja Mutalik <a.mutalik@zera.de>
2023-11-17 15:31:57 +0100

    Introduce MockCom5003d
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

---
+++ zenux-service-common/testlib/mockcom5003d.cpp
@@ -0,0 +1,18 @@
+#include "mockcom5003d.h"
+
+MockCom5003d::MockCom5003d() :
+    MockPcbServer("com5003d")
+{
+    m_foutSettings = std::make_unique<FOutSettings>(getConfigReader());
+    m_hkInSettings = std::make_unique<HkInSettings>(getConfigReader());
+    m_scInSettings = std::make_unique<ScInSettings>(getConfigReader());
+    m_samplingSettings = std::make_unique<SamplingSettings>(getConfigReader());
+    setXmlSettings(XmlSettingsList{m_foutSettings.get(), m_hkInSettings.get(), m_scInSettings.get(), m_samplingSettings.get()});
+
+    m_foutResource = std::make_unique<FOutGroupResourceAndInterface>(getSCPIInterface(), m_foutSettings.get());
+    m_hkInResource = std::make_unique<HkInGroupResourceAndInterface>(getSCPIInterface(), m_hkInSettings.get());
+    m_scInResource = std::make_unique<ScInGroupResourceAndInterface>(getSCPIInterface(), m_scInSettings.get());
+    m_samplingInterface = std::make_unique<cSamplingInterface>(getSCPIInterface(), m_samplingSettings.get());
+    setResources(ResourcesList{m_foutResource.get(), m_hkInResource.get(), m_scInResource.get(), m_samplingInterface.get()});
+    start();
+}
---
+++ zenux-service-common/testlib/mockcom5003d.h
@@ -0,0 +1,26 @@
+#ifndef MOCKCOM5003D_H
+#define MOCKCOM5003D_H
+
+#include "mockpcbserver.h"
+#include "foutgroupresourceandinterface.h"
+#include "hkingroupresourceandinterface.h"
+#include "scingroupresourceandinterface.h"
+#include "samplinginterface.h"
+#include <memory>
+
+class MockCom5003d : public MockPcbServer
+{
+public:
+    MockCom5003d();
+private:
+    std::unique_ptr<FOutSettings> m_foutSettings;
+    std::unique_ptr<HkInSettings> m_hkInSettings;
+    std::unique_ptr<ScInSettings> m_scInSettings;
+    std::unique_ptr<SamplingSettings> m_samplingSettings;
+    std::unique_ptr<FOutGroupResourceAndInterface> m_foutResource;
+    std::unique_ptr<HkInGroupResourceAndInterface> m_hkInResource;
+    std::unique_ptr<ScInGroupResourceAndInterface> m_scInResource;
+    std::unique_ptr<cSamplingInterface> m_samplingInterface;
+};
+
+#endif // MOCKCOM5003D_H
