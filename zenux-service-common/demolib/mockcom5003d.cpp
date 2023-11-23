c57a7965ef35c6f392ba944499ce6f1df662b502
Anuja Mutalik <a.mutalik@zera.de>
2023-11-23 09:02:43 +0100

    mockcom5003d, mockmt310s2d: Add more settings and resources
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- zenux-service-common/demolib/mockcom5003d.cpp
+++ zenux-service-common/demolib/mockcom5003d.cpp
@@ -3,16 +3,18 @@
 MockCom5003d::MockCom5003d() :
     MockPcbServer("com5003d")
 {
+    m_finSettings = std::make_unique<FInSettings>(getConfigReader());
     m_foutSettings = std::make_unique<FOutSettings>(getConfigReader());
     m_hkInSettings = std::make_unique<HkInSettings>(getConfigReader());
     m_scInSettings = std::make_unique<ScInSettings>(getConfigReader());
     m_samplingSettings = std::make_unique<SamplingSettings>(getConfigReader());
-    setXmlSettings(XmlSettingsList{m_foutSettings.get(), m_hkInSettings.get(), m_scInSettings.get(), m_samplingSettings.get()});
+    setXmlSettings(XmlSettingsList{m_finSettings.get(), m_foutSettings.get(), m_hkInSettings.get(), m_scInSettings.get(), m_samplingSettings.get()});
 
+    m_finResource = std::make_unique<FInGroupResourceAndInterface>(getSCPIInterface(), m_finSettings.get());
     m_foutResource = std::make_unique<FOutGroupResourceAndInterface>(getSCPIInterface(), m_foutSettings.get());
     m_hkInResource = std::make_unique<HkInGroupResourceAndInterface>(getSCPIInterface(), m_hkInSettings.get());
     m_scInResource = std::make_unique<ScInGroupResourceAndInterface>(getSCPIInterface(), m_scInSettings.get());
     m_samplingInterface = std::make_unique<cSamplingInterface>(getSCPIInterface(), m_samplingSettings.get());
-    setResources(ResourcesList{m_foutResource.get(), m_hkInResource.get(), m_scInResource.get(), m_samplingInterface.get()});
+    setResources(ResourcesList{m_finResource.get(), m_foutResource.get(), m_hkInResource.get(), m_scInResource.get(), m_samplingInterface.get()});
     start();
 }
