a758b5dce762e20874366843660f1b02d709f3c9
Andreas Müller <schnitzeltony@gmail.com>
2023-11-29 14:51:42 +0100

    zdsp1d: Get rid of syslog
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zdsp1d/main.cpp
+++ zdsp1d/main.cpp
@@ -2,15 +2,12 @@
 #include "zdspglobal.h"
 #include "pcbserver.h"
 #include <iostream>
-#include <syslog.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <QCoreApplication>
 
 int main( int argc, char *argv[] )
 {
-    openlog(ServerName, LOG_PID, LOG_DAEMON); // verbindung zum syslogd aufnehmen
-
     QCoreApplication* app = new QCoreApplication (argc, argv);
     ZDspServer* zdsp1d = new ZDspServer(); // this is our server
 
@@ -18,26 +15,24 @@ int main( int argc, char *argv[] )
 
     int r =  app->exec();
     if (r == parameterError)
-        syslog(LOG_EMERG,"Abort, wrong parameter count\n") ;
+        qCritical("Abort, wrong parameter count\n") ;
     if (r == pipeError)
-        syslog(LOG_EMERG,"Abort, could not open pipe\n") ;
+        qCritical("Abort, could not open pipe\n") ;
     if (r == xsdfileError)
-        syslog(LOG_EMERG,"Abort, xsd file error\n") ;
+        qCritical("Abort, xsd file error\n") ;
     if (r == xmlfileError)
-        syslog(LOG_EMERG,"Abort, xml file error\n") ;
+        qCritical("Abort, xml file error\n") ;
     if (r == dspDeviceError)
-        syslog(LOG_EMERG,"Abort, dsp device not found\n") ;
+        qCritical("Abort, dsp device not found\n") ;
     if (r == dspBootError)
-        syslog(LOG_EMERG,"Abort, dsp device boot error\n") ;
+        qCritical("Abort, dsp device boot error\n") ;
     if (r == dspBootFileError)
-        syslog(LOG_EMERG,"Abort, dsp device and bootfile mismatch\n") ;
+        qCritical("Abort, dsp device and bootfile mismatch\n") ;
     if (r == dspSetSamplingError)
-        syslog(LOG_EMERG,"Abort, dsp device set sampling system error\n") ;
+        qCritical("Abort, dsp device set sampling system error\n") ;
     if (r == rmConnectionError)
-        syslog(LOG_EMERG,"Abort, resourcemanager connection error\n") ;
+        qCritical("Abort, resourcemanager connection error\n") ;
 
-    syslog(LOG_INFO,"zdsp1d server child process terminated ret = %d\n", r);
     delete zdsp1d;
-    closelog();
     return (r);
 }
