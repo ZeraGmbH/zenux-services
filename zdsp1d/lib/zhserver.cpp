44b83d8d21813843bd38e790cf8e6b34549d94fe
Andreas Müller <schnitzeltony@gmail.com>
2023-11-29 14:20:59 +0100

    zhserver: Remove unused ctor / de-noise
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zdsp1d/lib/zhserver.cpp
+++ zdsp1d/lib/zhserver.cpp
@@ -1,22 +1,5 @@
-// implementation zhserver
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <iostream>
-#include <qstring.h>
-#include <qstringlist.h>
-
 #include "zdspglobal.h"
 #include "zhserver.h"
-#include "dsp1scpi.h"
-
-class cNode;
-
-// definition was ein client ist
-
 
 cZHServer::cZHServer()
 {
@@ -24,23 +7,11 @@ cZHServer::cZHServer()
     sServerName = ServerName;
 }
 
-
-cZHServer::cZHServer(cCmdInterpreter* ci)
-{
-    cZHServer();
-    pCmdInterpreter=ci;
-}
-
-
 cZHServer::~cZHServer()
 {
 }
-
 
 QString& cZHServer::GetSoftwareVersion()
 { // abfrage software version
     return (sSoftwareVersion);
 }
-
-
-
