e5063e29d39720390fbc3cdabf74b9d188fa5e05
Andreas Müller <schnitzeltony@gmail.com>
2023-11-30 20:16:22 +0100

    zdsp1d: We have a responding mocked server based on production code!!!
    * Let's hope that on device smoke test is going to pass once somebody can put
      hands on - we took small steps but shuffled a lot...
    * How about doing same for SEC?
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zdsp1d/tests/test_zdsp1d_runfull_server.cpp
+++ zdsp1d/tests/test_zdsp1d_runfull_server.cpp
@@ -3,7 +3,12 @@
 #include "mockpcbserver.h"
 #include "dspdevicenode.h"
 #include "dspdevicenodemock.h"
+#include "resmanrunfacade.h"
+#include "proxy.h"
+#include "dspinterface.h"
+#include "reply.h"
 #include <QAbstractEventDispatcher>
+#include <QSignalSpy>
 #include <QTest>
 
 QTEST_MAIN(test_zdsp1d_runfull_server)
@@ -18,9 +23,28 @@ void test_zdsp1d_runfull_server::startProductionServerToSeeFailingMessages()
 
 void test_zdsp1d_runfull_server::startMockedServer()
 {
+    ResmanRunFacade resman;
+    feedEventLoop();
+
     DspDeviceNodeInterfaceUPtr dspDeviceNode = std::make_unique<DspDeviceNodeMock>(DspDeviceNode::MAGIC_ID21362);
     ZDspServer server(std::move(dspDeviceNode), MockPcbServer::createParams("zdsp1d"));
     feedEventLoop();
+
+    Zera::ProxyClientPtr dspClient = Zera::Proxy::getInstance()->getConnectionSmart("127.0.0.1", 6310);
+    Zera::cDSPInterface dspIFace;
+    dspIFace.setClientSmart(dspClient);
+
+    Zera::Proxy::getInstance()->startConnectionSmart(dspClient);
+    feedEventLoop();
+
+    QSignalSpy responseSpy(&dspIFace, &Zera::cDSPInterface::serverAnswer);
+    int msgNr = dspIFace.readDeviceVersion();
+    feedEventLoop();
+
+    QCOMPARE(responseSpy.count(), 1);
+    QCOMPARE(responseSpy[0][0], QVariant(msgNr));
+    QCOMPARE(responseSpy[0][1], QVariant(ack));
+    QCOMPARE(responseSpy[0][2], QVariant("DSPLCA: V0.0;DSP V0.00"));
 }
 
 void test_zdsp1d_runfull_server::feedEventLoop()
