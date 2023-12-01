5afe7752f595ab6f25c5240ab9f1f87ae605d7dd
Andreas Müller <schnitzeltony@gmail.com>
2023-12-01 13:14:31 +0100

    test_mockservice_zdsp1d: Introduce another version test / get prepared for more
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/tests/test_mockservice_zdsp1d.cpp
+++ zenux-service-common/tests/test_mockservice_zdsp1d.cpp
@@ -1,6 +1,4 @@
 #include "test_mockservice_zdsp1d.h"
-#include "mockzdsp1d.h"
-#include "resmanrunfacade.h"
 #include "proxy.h"
 #include "dspinterface.h"
 #include "reply.h"
@@ -10,14 +8,29 @@
 
 QTEST_MAIN(test_mockservice_zdsp1d)
 
-void test_mockservice_zdsp1d::getVersion()
+void test_mockservice_zdsp1d::initTestCase()
 {
-    ResmanRunFacade resman;
-    feedEventLoop();
+    qputenv("QT_FATAL_CRITICALS", "1");
+}
 
-    MockZdsp1d zdsp1d;
+void test_mockservice_zdsp1d::init()
+{
+    m_resman = std::make_unique<ResmanRunFacade>();
     feedEventLoop();
+    m_zsdp1d = std::make_unique<MockZdsp1d>();
+    feedEventLoop();
+}
 
+void test_mockservice_zdsp1d::cleanup()
+{
+    m_zsdp1d = nullptr;
+    feedEventLoop();
+    m_resman = nullptr;
+    feedEventLoop();
+}
+
+void test_mockservice_zdsp1d::getDspLcaVersion()
+{
     Zera::ProxyClientPtr dspClient = Zera::Proxy::getInstance()->getConnectionSmart("127.0.0.1", 6310);
     Zera::cDSPInterface dspIFace;
     dspIFace.setClientSmart(dspClient);
@@ -33,6 +46,25 @@ void test_mockservice_zdsp1d::getVersion()
     QCOMPARE(responseSpy[0][0], QVariant(msgNr));
     QCOMPARE(responseSpy[0][1], QVariant(ack));
     QCOMPARE(responseSpy[0][2], QVariant("DSPLCA: V0.0;DSP V0.00"));
+}
+
+void test_mockservice_zdsp1d::getServerVersion()
+{
+    Zera::ProxyClientPtr dspClient = Zera::Proxy::getInstance()->getConnectionSmart("127.0.0.1", 6310);
+    Zera::cDSPInterface dspIFace;
+    dspIFace.setClientSmart(dspClient);
+
+    Zera::Proxy::getInstance()->startConnectionSmart(dspClient);
+    feedEventLoop();
+
+    QSignalSpy responseSpy(&dspIFace, &Zera::cDSPInterface::serverAnswer);
+    int msgNr = dspIFace.readServerVersion();
+    feedEventLoop();
+
+    QCOMPARE(responseSpy.count(), 1);
+    QCOMPARE(responseSpy[0][0], QVariant(msgNr));
+    QCOMPARE(responseSpy[0][1], QVariant(ack));
+    QCOMPARE(responseSpy[0][2], QVariant("zdsp1d V1.11"));
 }
 
 void test_mockservice_zdsp1d::feedEventLoop()
