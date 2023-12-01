fbd40e9e241654aacfbace920104863efe9bbdb2
Andreas Müller <schnitzeltony@gmail.com>
2023-12-01 13:14:31 +0100

    test_mockservice_zdsp1d: Introduce another version test / get prepared for more
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/tests/test_mockservice_zdsp1d.h
+++ zenux-service-common/tests/test_mockservice_zdsp1d.h
@@ -1,15 +1,24 @@
 #ifndef TEST_MOCKSERVICE_ZDSP1D_H
 #define TEST_MOCKSERVICE_ZDSP1D_H
 
+#include "resmanrunfacade.h"
+#include "mockzdsp1d.h"
 #include <QObject>
+#include <memory>
 
 class test_mockservice_zdsp1d : public QObject
 {
     Q_OBJECT
 private slots:
-    void getVersion();
+    void initTestCase();
+    void init();
+    void cleanup();
+    void getDspLcaVersion();
+    void getServerVersion();
 private:
     void feedEventLoop();
+    std::unique_ptr<ResmanRunFacade> m_resman;
+    std::unique_ptr<MockZdsp1d> m_zsdp1d;
 };
 
 #endif // TEST_MOCKSERVICE_ZDSP1D_H
