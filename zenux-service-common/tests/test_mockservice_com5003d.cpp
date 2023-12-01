cc6fb9fe9f638539ddba74a0e7b60be1fcfe507b
Andreas Müller <schnitzeltony@gmail.com>
2023-12-01 14:36:14 +0100

    test_mockservice_com5003d: Make adding more tests simpler
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/tests/test_mockservice_com5003d.cpp
+++ zenux-service-common/tests/test_mockservice_com5003d.cpp
@@ -1,6 +1,4 @@
 #include "test_mockservice_com5003d.h"
-#include "resmanrunfacade.h"
-#include "mockcom5003d.h"
 #include "proxy.h"
 #include "reply.h"
 #include "pcbinterface.h"
@@ -10,12 +8,29 @@
 
 QTEST_MAIN(test_mockservice_com5003d)
 
+void test_mockservice_com5003d::initTestCase()
+{
+    qputenv("QT_FATAL_CRITICALS", "1");
+}
+
+void test_mockservice_com5003d::init()
+{
+    m_resman = std::make_unique<ResmanRunFacade>();
+    TimeMachineObject::feedEventLoop();
+    m_com5003d = std::make_unique<MockCom5003d>();
+    TimeMachineObject::feedEventLoop();
+}
+
+void test_mockservice_com5003d::cleanup()
+{
+    m_com5003d = nullptr;
+    TimeMachineObject::feedEventLoop();
+    m_resman = nullptr;
+    TimeMachineObject::feedEventLoop();
+}
+
 void test_mockservice_com5003d::getFoutCat()
 {
-    ResmanRunFacade resman;
-    MockCom5003d mockCom5003d;
-    TimeMachineObject::feedEventLoop();
-
     Zera::ProxyClientPtr pcbClient = Zera::Proxy::getInstance()->getConnectionSmart("127.0.0.1", 6307);
     Zera::cPCBInterface pcbIFace;
     pcbIFace.setClientSmart(pcbClient);
