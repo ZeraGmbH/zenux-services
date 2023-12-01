444f0f91c1dffc0fb49946c409c3a87140ed2848
Andreas Müller <schnitzeltony@gmail.com>
2023-12-01 14:28:43 +0100

    test_mockservice_mt310s2d: Make adding more tests simpler / fail on criticals
    We had to remove connectInvalidIp() which was not of value exactly
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/tests/test_mockservice_mt310s2d.h
+++ zenux-service-common/tests/test_mockservice_mt310s2d.h
@@ -1,16 +1,25 @@
 #ifndef TEST_MOCKSERVICE_MT310S2D_H
 #define TEST_MOCKSERVICE_MT310S2D_H
 
+#include "mockmt310s2d.h"
+#include "resmanrunfacade.h"
 #include <QObject>
+#include <memory>
 
 class test_mockservice_mt310s2d : public QObject
 {
     Q_OBJECT
 private slots:
+    void initTestCase();
+    void init();
+    void cleanup();
+
     void runMock();
     void connectServer();
-    void connectInvalidIp();
     void getFoutCat();
+private:
+    std::unique_ptr<ResmanRunFacade> m_resman;
+    std::unique_ptr<MockMt310s2d> m_mt310s2d;
 };
 
 #endif // TEST_MOCKSERVICE_MT310S2D_H
