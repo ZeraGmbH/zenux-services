d8288077e29aeb856d16aecaa986bfb759d3b8a7
Andreas Müller <schnitzeltony@gmail.com>
2023-12-01 14:36:14 +0100

    test_mockservice_com5003d: Make adding more tests simpler
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/tests/test_mockservice_com5003d.h
+++ zenux-service-common/tests/test_mockservice_com5003d.h
@@ -1,13 +1,23 @@
 #ifndef TEST_MOCKSERVICE_COM5003D_H
 #define TEST_MOCKSERVICE_COM5003D_H
 
+#include "resmanrunfacade.h"
+#include "mockcom5003d.h"
 #include <QObject>
+#include <memory>
 
 class test_mockservice_com5003d : public QObject
 {
     Q_OBJECT
 private slots:
+    void initTestCase();
+    void init();
+    void cleanup();
+
     void getFoutCat();
+private:
+    std::unique_ptr<ResmanRunFacade> m_resman;
+    std::unique_ptr<MockCom5003d> m_com5003d;
 };
 
 #endif // TEST_MOCKSERVICE_COM5003D_H
