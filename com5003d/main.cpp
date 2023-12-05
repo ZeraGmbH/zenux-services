71afcd8cbf666e65bbe7ff17b77e3417da6b8927
Andreas Müller <schnitzeltony@gmail.com>
2023-12-05 22:41:28 +0100

    com5003d/main.cpp: Simplify a bit
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- com5003d/main.cpp
+++ com5003d/main.cpp
@@ -1,25 +1,14 @@
-// test programm scpi parser
-
-#include <iostream>
-#include <syslog.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <QCoreApplication>
-#include <QTextCodec>
-#include "com5003dglobal.h"
 #include "com5003d.h"
-
-
+#include "com5003dglobal.h"
+#include <QCoreApplication>
+#include <syslog.h>
 
 int main( int argc, char *argv[] )
 {
-
-    openlog(ServerName, LOG_PID, LOG_DAEMON); // open connection to syslogd
+    openlog(ServerName, LOG_PID, LOG_DAEMON); // we are still using it...
 
     QCoreApplication* app = new QCoreApplication (argc, argv);
-    cCOM5003dServer* com5003d=new cCOM5003dServer(); // this is our server
-
+    cCOM5003dServer* com5003d = new cCOM5003dServer(); // this is our server
     qInfo(ServerName " started");
 
     int r = app->exec();
@@ -30,7 +19,6 @@ int main( int argc, char *argv[] )
     if (r == atmelProgError)
         qCritical("Abort, atmel flash program error") ;
 
-    syslog(LOG_INFO,"com5003d server child process terminated ret = %d\n", r);
     delete com5003d;
     closelog();
     return (r);
