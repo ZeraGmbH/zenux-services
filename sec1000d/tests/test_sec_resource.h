e7861baea5f74b8f671b417a5cf31c2fa24a3c66
Anuja Mutalik <a.mutalik@zera.de>
2023-12-05 14:51:31 +0100

    test_sec_resource: Tests to free channels set by clients from same peer
    Signed-off-by: Anuja Mutalik <a.mutalik@zera.de>

--- sec1000d/tests/test_sec_resource.h
+++ sec1000d/tests/test_sec_resource.h
@@ -15,8 +15,11 @@ private slots:
     void setSecChannelsForAClient();
     void setAndFreeSecChannelsForAClient();
     void setSecChannelsForMultipleClientsFreeOneClient();
+    void setSecChannelsForMultipleClientsOnePeerFreePeer();
+    void setSecChannelsForMultipleClientsMultiplePeersFreeOnePeer();
+    void freeChannelsFromInvalidPeer();
 private:
-    QString sendScpiCommand(QByteArray clientID, QString cmd);
+    QString sendScpiCommand(XiQNetPeer *peer, QByteArray clientID, QString cmd);
     std::unique_ptr<Zera::XMLConfig::cReader> m_xmlConfigReader;
     std::unique_ptr<SecCalculatorSettings> m_ecalcSettings;
     std::unique_ptr<SecInputSettings> m_inputSettings;
