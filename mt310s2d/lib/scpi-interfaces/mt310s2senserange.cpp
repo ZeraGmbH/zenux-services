8c03be5ffdf4edc8f74f5b9127d5a9b78d1c1c15
Andreas Müller <schnitzeltony@gmail.com>
2023-12-17 17:09:23 +0100

    com5003d/mt310s2d: Move as much as possible to SenseRangeCommon / cleanup
    Before continuing we need to make other regression tests to verify our
    assumption on integer nature of rejection values
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/lib/scpi-interfaces/mt310s2senserange.cpp
+++ mt310s2d/lib/scpi-interfaces/mt310s2senserange.cpp
@@ -1,10 +1,6 @@
 #include "mt310s2senserange.h"
 #include "mt310s2justdata.h"
-#include "protonetcommand.h"
 #include "zscpi_response_definitions.h"
-#include <scpi.h>
-#include <scpicommand.h>
-
 
 Mt310s2SenseRange::Mt310s2SenseRange(cSCPI *scpiinterface,
                                      QString name,
@@ -37,32 +33,30 @@ Mt310s2SenseRange::~Mt310s2SenseRange()
     delete m_pJustdata;
 }
 
-
 void Mt310s2SenseRange::initSCPIConnection(QString leadingNodes)
 {
     SenseRangeCommon::initSCPIConnection(leadingNodes);
+
+    // the following are different from Com5003 so not yet ready to move to common
     ensureTrailingColonOnNonEmptyParentNodes(leadingNodes);
-    addDelegate(QString("%1%2").arg(leadingNodes).arg(m_sName),"TYPE",SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdType);
-    addDelegate(QString("%1%2").arg(leadingNodes).arg(m_sName),"REJECTION",SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdRejection);
-    addDelegate(QString("%1%2").arg(leadingNodes).arg(m_sName),"OVREJECTION",SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdOVRejection);
-    addDelegate(QString("%1%2").arg(leadingNodes).arg(m_sName),"ADCREJECTION",SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdADCRejection);
+    addDelegate(QString("%1%2").arg(leadingNodes, m_sName), "TYPE", SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdType);
+    addDelegate(QString("%1%2").arg(leadingNodes, m_sName), "REJECTION", SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdRejection);
+    addDelegate(QString("%1%2").arg(leadingNodes, m_sName), "OVREJECTION", SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdOVRejection);
+    addDelegate(QString("%1%2").arg(leadingNodes, m_sName), "ADCREJECTION", SCPI::isQuery, m_pSCPIInterface, SenseRange::cmdADCRejection);
 
     connect(m_pJustdata, &ScpiConnection::cmdExecutionDone, this, &ScpiConnection::cmdExecutionDone);
     m_pJustdata->initSCPIConnection(QString("%1%2").arg(leadingNodes).arg(m_sName));
 }
-
 
 quint8 Mt310s2SenseRange::getAdjustmentStatus()
 {
     return m_pJustdata->getAdjustmentStatus();
 }
 
-
 quint16 Mt310s2SenseRange::getMMask()
 {
     return m_nMMask;
 }
-
 
 Mt310s2JustRangeTripletOffsetGainPhase *Mt310s2SenseRange::getJustData()
 {
@@ -87,31 +81,24 @@ void Mt310s2SenseRange::computeJustData()
 
 void Mt310s2SenseRange::executeProtoScpi(int cmdCode, cProtonetCommand *protoCmd)
 {
-    switch (cmdCode)
-    {
-    case SenseRange::cmdType:
-        protoCmd->m_sOutput = m_ReadRangeType(protoCmd->m_sInput);
-        break;
-    case SenseRange::cmdAlias:
-        protoCmd->m_sOutput = handeScpiRangeAlias(protoCmd->m_sInput);
-        break;
-    case SenseRange::cmdAvail:
-        protoCmd->m_sOutput = handeScpiRangeAvail(protoCmd->m_sInput);
-        break;
-    case SenseRange::cmdUpperRangeValue:
-        protoCmd->m_sOutput = handeScpiRangeUpperRangeValue(protoCmd->m_sInput);
-        break;
-    case SenseRange::cmdRejection:
-        protoCmd->m_sOutput = m_ReadRangeRejection(protoCmd->m_sInput);
-        break;
-    case SenseRange::cmdOVRejection:
-        protoCmd->m_sOutput = m_ReadRangeOVRejection(protoCmd->m_sInput);
-        break;
-    case SenseRange::cmdADCRejection:
-        protoCmd->m_sOutput = m_ReadRangeADWRejection(protoCmd->m_sInput);
-        break;
+    if(!execScpi(cmdCode, protoCmd)) {
+        // the following are different from Com5003 so not yet ready to move to common
+        switch (cmdCode)
+        {
+        case SenseRange::cmdType:
+            protoCmd->m_sOutput = m_ReadRangeType(protoCmd->m_sInput);
+            break;
+        case SenseRange::cmdRejection:
+            protoCmd->m_sOutput = m_ReadRangeRejection(protoCmd->m_sInput);
+            break;
+        case SenseRange::cmdOVRejection:
+            protoCmd->m_sOutput = m_ReadRangeOVRejection(protoCmd->m_sInput);
+            break;
+        case SenseRange::cmdADCRejection:
+            protoCmd->m_sOutput = m_ReadRangeADWRejection(protoCmd->m_sInput);
+            break;
+        }
     }
-
     if (protoCmd->m_bwithOutput)
         emit cmdExecutionDone(protoCmd);
 }
@@ -119,7 +106,6 @@ void Mt310s2SenseRange::executeProtoScpi(int cmdCode, cProtonetCommand *protoCmd
 QString Mt310s2SenseRange::m_ReadRangeType(QString &sInput)
 {
     cSCPICommand cmd = sInput;
-
     if (cmd.isQuery())
         return QString("%1").arg(m_nMMask); // we return mmode mask and sensortype here
     else
@@ -130,32 +116,26 @@ QString Mt310s2SenseRange::m_ReadRangeType(QString &sInput)
 QString Mt310s2SenseRange::m_ReadRangeRejection(QString &sInput)
 {
     cSCPICommand cmd = sInput;
-
     if (cmd.isQuery())
         return QString("%1").arg(m_fRejection, 0, 'g', 8);
     else
         return ZSCPI::scpiAnswer[ZSCPI::nak];
 }
 
-
 QString Mt310s2SenseRange::m_ReadRangeOVRejection(QString &sInput)
 {
     cSCPICommand cmd = sInput;
-
     if (cmd.isQuery())
         return QString("%1").arg(m_fOVRejection, 0, 'g', 8);
     else
         return ZSCPI::scpiAnswer[ZSCPI::nak];
 }
 
-
 QString Mt310s2SenseRange::m_ReadRangeADWRejection(QString &sInput)
 {
     cSCPICommand cmd = sInput;
-
     if (cmd.isQuery())
         return QString("%1").arg(m_fADCRejection, 0, 'g', 8);
     else
         return ZSCPI::scpiAnswer[ZSCPI::nak];
 }
-
