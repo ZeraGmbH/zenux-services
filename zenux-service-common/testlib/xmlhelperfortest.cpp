3ae85ddf74a22b512e8e78faeac5733ecacccd00
Andreas Müller <schnitzeltony@gmail.com>
2023-12-20 14:35:55 +0100

    com5003/mt310s2: Fix export uninitalized checksum
    In regression tests we use Com5003Adjustment/Mt310s2AdjFlash differently
    than in production: In production flash is read first so checksum gets
    initialized.
    In test there is no flash mock yet so variable remains unitialized.
    
    Learnings this time:
    * Checksum is flash content and is just exported to xml
    * Checksum is not depending on XML generation date/time
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/testlib/xmlhelperfortest.cpp
+++ zenux-service-common/testlib/xmlhelperfortest.cpp
@@ -7,7 +7,5 @@ QString XmlHelperForTest::removeTimeDependentEntriesFromXml(QString xml)
     xml = xml.replace(regexDate, "<Date>nodate</Date>");
     static QRegularExpression regexTime("<Time>.*<\\/Time>");
     xml = xml.replace(regexTime, "<Time>notime</Time>");
-    static QRegularExpression regexChecksum("<Chksum>.*<\\/Chksum>");
-    xml = xml.replace(regexChecksum, "<Chksum>nocheck</Chksum>");
     return xml;
 }
