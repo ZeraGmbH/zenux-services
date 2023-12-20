3bb7c3e1a63e4247e6cd672f8cdc888f7514c056
Andreas Müller <schnitzeltony@gmail.com>
2023-12-20 16:20:21 +0100

    com5003d/mt310s2d: Add check for expected sense channels
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/tests/test_regression_sense_interface_mt310s2.h
+++ mt310s2d/tests/test_regression_sense_interface_mt310s2.h
@@ -2,6 +2,7 @@
 #define TEST_REGRESSION_SENSE_INTERFACE_MT310S2_H
 
 #include <QObject>
+#include <QStringList>
 
 class test_regression_sense_interface_mt310s2 : public QObject
 {
@@ -9,6 +10,9 @@ class test_regression_sense_interface_mt310s2 : public QObject
 private slots:
     void checkVersionsOfSystemInterface();
     void checkExportXml();
+    void checkChannelCatalogAsExpected();
+private:
+    static QStringList m_channelsExpectedAllOverThePlace;
 };
 
 #endif // TEST_REGRESSION_SENSE_INTERFACE_MT310S2_H
