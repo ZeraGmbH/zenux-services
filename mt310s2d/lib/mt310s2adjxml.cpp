f6c3dd21e47cd22484e16594f65b5b6d73fd4997
Andreas Müller <schnitzeltony@gmail.com>
2023-12-28 23:23:59 +0100

    com5003d/mt310s2d: Prettify XML adjustment code
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/lib/mt310s2adjxml.cpp
+++ mt310s2d/lib/mt310s2adjxml.cpp
@@ -10,60 +10,41 @@ Mt310s2AdjXML::Mt310s2AdjXML()
 bool Mt310s2AdjXML::exportAdTojXMLFile(QString file)
 {
     QString filename = file + ".xml";
-
     QFile adjfile(filename);
-    if ( !adjfile.open( QIODevice::WriteOnly ) )
-    {
-        qCritical("%s", "exportAdTojXMLFile: could not open xml file");
+    if (!adjfile.open(QIODevice::WriteOnly )) {
+        qCritical("exportAdTojXMLFile: could not open xml file: %s", qPrintable(filename));
         return false;
     }
-
-    QString sXML;
-
-    sXML = exportXMLString();
+    QString sXML = exportXMLString();
     QTextStream stream( &adjfile );
     stream << sXML;
     adjfile.close();
-
     return true;
 }
-
 
 bool Mt310s2AdjXML::importAdjXMLFile(QString file)
 {
     QString filename = file + ".xml";
-
     QFile adjfile(filename);
-    if ( !adjfile.open( QIODevice::ReadOnly ) )
-    {
-        qCritical("%s", "importAdjXMLFile: could not open xml file");
+    if(!adjfile.open(QIODevice::ReadOnly)) {
+        qCritical("ImportAdjXMLFile: could not open xml file: %s", qPrintable(filename));
         return false;
     }
-
-    QDomDocument justqdom( "TheDocument" );
-    if ( !justqdom.setContent( &adjfile ) )
-    {
-        adjfile.close();
-        qCritical("%s", "importAdjXMLFile: format error in xml file");
+    QDomDocument justqdom("TheDocument");
+    if(!justqdom.setContent(&adjfile)) {
+        qCritical("importAdjXMLFile: format error in xml file: %s", qPrintable(filename));
         return false;
     }
-
     adjfile.close();
-
     return importXMLDocument(&justqdom);
 }
-
 
 bool Mt310s2AdjXML::importAdjXMLString(QString &xml)
 {
-    QDomDocument justqdom( "TheDocument" );
-    if ( !justqdom.setContent(xml) )
-    {
-        qCritical("%s", "importAdjXMLString: format error in xml file");
+    QDomDocument justqdom("TheDocument");
+    if (!justqdom.setContent(xml)) {
+        qCritical("importAdjXMLString: format error in xml");
         return false;
     }
-
     return importXMLDocument(&justqdom);
 }
-
-
