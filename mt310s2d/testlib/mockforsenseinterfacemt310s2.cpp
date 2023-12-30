f9c4a836c1dc6d34b48366d0ed86752f0870e522
Andreas Müller <schnitzeltony@gmail.com>
2023-12-30 23:46:43 +0100

    MockForSenseInterfaceMt310s2: Add system interface for sense xml import/export
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/testlib/mockforsenseinterfacemt310s2.cpp
+++ mt310s2d/testlib/mockforsenseinterfacemt310s2.cpp
@@ -7,10 +7,15 @@ MockForSenseInterfaceMt310s2::MockForSenseInterfaceMt310s2() :
     m_senseSettings = std::make_unique<cSenseSettings>(getConfigReader(), 8);
     setXmlSettings(XmlSettingsList{m_i2cSettings.get(), m_senseSettings.get()});
 
-    m_systemInfo = std::make_unique<cSystemInfo>();
+    m_systemController = std::make_shared<cATMELSysCtrl>(m_i2cSettings->getDeviceNode(), m_i2cSettings->getI2CAdress(i2cSettings::sysCtrlI2cAddress), 0);
+    m_systemInfo = std::make_unique<Mt310s2SystemInfo>(m_systemController);
 
     m_senseInterface = std::make_unique<Mt310s2SenseInterface>(getSCPIInterface(), m_i2cSettings.get(), m_senseSettings.get(), m_systemInfo.get());
     m_clampInterface = std::make_unique<cClampInterface>(this, m_i2cSettings.get(), m_senseSettings.get(), m_senseInterface.get());
     setResources(ResourcesList{m_senseInterface.get()});
+
+    m_systemInterface = std::make_unique<Mt310s2SystemInterface>(this, m_systemInfo.get(), m_senseSettings.get(), m_senseInterface.get(), nullptr, nullptr);
+
+    setScpiConnections(ScpiConnectionList{m_systemInterface.get(), m_clampInterface.get()});
     start();
 }
