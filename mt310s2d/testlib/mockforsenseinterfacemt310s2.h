d972ce42d214fd28d0f77ef66cd596f2a0eddaa6
Andreas Müller <schnitzeltony@gmail.com>
2023-12-30 23:46:43 +0100

    MockForSenseInterfaceMt310s2: Add system interface for sense xml import/export
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/testlib/mockforsenseinterfacemt310s2.h
+++ mt310s2d/testlib/mockforsenseinterfacemt310s2.h
@@ -3,6 +3,7 @@
 
 #include "mockpcbserver.h"
 #include "mt310s2senseinterface.h"
+#include "mt310s2systeminterface.h"
 #include "clampinterface.h"
 
 class MockForSenseInterfaceMt310s2 : public MockPcbServer
@@ -16,9 +17,15 @@ public:
 private:
     std::unique_ptr<cI2CSettings> m_i2cSettings;
     std::unique_ptr<cSenseSettings> m_senseSettings;
-    std::unique_ptr<cSystemInfo> m_systemInfo;
+
+    std::shared_ptr<cATMELSysCtrl> m_systemController;
+
+    std::unique_ptr<Mt310s2SystemInfo> m_systemInfo;
+
     std::unique_ptr<Mt310s2SenseInterface> m_senseInterface;
     std::unique_ptr<cClampInterface> m_clampInterface;
+
+    std::unique_ptr<Mt310s2SystemInterface> m_systemInterface; // for adj xml import/export
 };
 
 #endif // MOCKFORSENSEINTERFACEMT310S2_H
