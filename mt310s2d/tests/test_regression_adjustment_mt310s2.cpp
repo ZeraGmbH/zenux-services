5af8035fcdc506e800c28925404a3dd0e6c7fdcd
Andreas Müller <schnitzeltony@gmail.com>
2024-01-01 18:44:46 +0100

    Start some simple adjustment interface tests / restructure resources
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- mt310s2d/tests/test_regression_adjustment_mt310s2.cpp
+++ mt310s2d/tests/test_regression_adjustment_mt310s2.cpp
@@ -1,6 +1,9 @@
 #include "test_regression_adjustment_mt310s2.h"
 #include "clampfactorytest.h"
 #include "proxy.h"
+#include "scpisingletransactionblocked.h"
+#include "zscpi_response_definitions.h"
+#include "xmlhelperfortest.h"
 #include <timemachineobject.h>
 #include <QSignalSpy>
 #include <QTest>
@@ -32,4 +35,32 @@ void test_regression_adjustment_mt310s2::cleanup()
     m_mockServer = nullptr;
     m_resmanServer = nullptr;
     TimeMachineObject::feedEventLoop();
+}
+
+void test_regression_adjustment_mt310s2::scpiGetInitialAdjXml()
+{
+    // Similar to test_regression_sense_interface_mt310s2::checkExportXml() but here we use
+    // interface
+    QFile xmlFile(":/regression_data_adj/adj_initial_export_no_clamps.xml");
+    QVERIFY(xmlFile.open(QFile::ReadOnly));
+    QString xmlExpected = xmlFile.readAll();
+    qInfo("Expected XML:");
+    qInfo("%s", qPrintable(xmlExpected));
+    xmlExpected = XmlHelperForTest::removeTimeDependentEntriesFromXml(xmlExpected);
+
+    QString xmlExported = ScpiSingleTransactionBlocked::query("SYSTEM:ADJUSTMENT:XML?");
+    QString xmlExportedPretty = XmlHelperForTest::prettify(xmlExported);
+    qInfo("Exported XML:");
+    qInfo("%s", qPrintable(xmlExportedPretty));
+    xmlExportedPretty = XmlHelperForTest::removeTimeDependentEntriesFromXml(xmlExportedPretty);
+
+    // if this turns fragile we have to use zera-scpi's xml-compare-testlib
+    QCOMPARE(xmlExportedPretty, xmlExpected);
+
+}
+
+void test_regression_adjustment_mt310s2::scpiSetPermissionQueryFail()
+{
+    QString ret = ScpiSingleTransactionBlocked::cmd("SYSTEM:ADJUSTMENT:XML", "foo");
+    QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::errexec]);
 }
