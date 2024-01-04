3d09465d5a207bdc1910752da800a42a0fbe4533
Andreas Müller <schnitzeltony@gmail.com>
2024-01-04 19:48:17 +0100

    XmlHelperForTest: Extend to upcoming tests
    * COM exports date & time empty with xml tag as <Date/>
    * Get checksum out. This is interesting as it shows us that our adj
      import/export tests were not close enough to production: There is
      no initial load from EEPROM performed so checksum remains unset
    * Add convenient function XmlHelperForTest::prepareForCompare
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/testlib/xmlhelperfortest.cpp
+++ zenux-service-common/testlib/xmlhelperfortest.cpp
@@ -19,11 +19,25 @@ QString XmlHelperForTest::prettify(QString xml)
     return doc.toString(4);
 }
 
+QString XmlHelperForTest::prepareForCompare(QString xml)
+{
+    return removeChecksumDependentEntriesFromXml(removeTimeDependentEntriesFromXml(prettify(xml)));
+}
+
 QString XmlHelperForTest::removeTimeDependentEntriesFromXml(QString xml)
 {
     static QRegularExpression regexDate("<Date>.*<\\/Date>");
     xml = xml.replace(regexDate, "<Date>nodate</Date>");
+    xml = xml.replace("<Date/>", "<Date>nodate</Date>");
     static QRegularExpression regexTime("<Time>.*<\\/Time>");
     xml = xml.replace(regexTime, "<Time>notime</Time>");
+    xml = xml.replace("<Time/>", "<Time>notime</Time>");
+    return xml;
+}
+
+QString XmlHelperForTest::removeChecksumDependentEntriesFromXml(QString xml)
+{
+    static QRegularExpression regexDate("<Chksum>.*<\\/Chksum>");
+    xml = xml.replace(regexDate, "<Chksum>nochksum</Chksum>");
     return xml;
 }
