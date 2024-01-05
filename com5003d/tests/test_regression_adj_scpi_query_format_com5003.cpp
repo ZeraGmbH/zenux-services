9b5b4b79602d341cb266d92adf25a24deaf5c2e9
Andreas Müller <schnitzeltony@gmail.com>
2024-01-05 12:28:07 +0100

    com5003d: Add adjusted value format test playground too
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ com5003d/tests/test_regression_adj_scpi_query_format_com5003.cpp
@@ -0,0 +1,68 @@
+#include "test_regression_adj_scpi_query_format_com5003.h"
+#include "proxy.h"
+#include "i2cflashiofactoryfortest.h"
+#include "flash24lc256mock.h"
+#include "scpisingletransactionblocked.h"
+#include <timemachineobject.h>
+#include <QSignalSpy>
+#include <QTest>
+
+QTEST_MAIN(test_regression_adj_scpi_query_format_com5003);
+
+static const QDateTime refTime = QDateTime::fromSecsSinceEpoch(0, Qt::UTC);
+
+void test_regression_adj_scpi_query_format_com5003::initTestCase()
+{
+    // permission tests are done in test_regression_adj_import_export_xml_<device>
+    m_permissionMock = AtmelPermissionMock::createAlwaysEnabled();
+}
+
+void test_regression_adj_scpi_query_format_com5003::init()
+{
+    Flash24LC256Mock::cleanAll();
+    I2cFlashIoFactoryForTest::enableMockFlash();
+    setupServers(m_permissionMock.get());
+
+    QString filenameShort = ":/import_scpi_format";
+    QVERIFY(QFile::exists(filenameShort + ".xml"));
+    QVERIFY(m_mockServer->getAdjustment()->importAdjXMLFile(filenameShort));
+}
+
+void test_regression_adj_scpi_query_format_com5003::cleanup()
+{
+    m_pcbIFace = nullptr;
+    m_pcbClient = nullptr;
+    m_mockServer = nullptr;
+    m_resmanServer = nullptr;
+    TimeMachineObject::feedEventLoop();
+}
+
+void test_regression_adj_scpi_query_format_com5003::queryGainCorrectionSingle()
+{
+    QString adjustedValue;
+
+    adjustedValue = ScpiSingleTransactionBlocked::cmd("SENS:m0:240V:CORR:ADJG?", "1");
+    qInfo("%s", qPrintable(adjustedValue));
+
+    adjustedValue = ScpiSingleTransactionBlocked::cmd("SENS:m1:240V:CORR:ADJG?", "1");
+    qInfo("%s", qPrintable(adjustedValue));
+
+    adjustedValue = ScpiSingleTransactionBlocked::cmd("SENS:m2:240V:CORR:ADJG?", "1");
+    qInfo("%s", qPrintable(adjustedValue));
+
+    // no assertions yet - we are investigating....
+    //QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
+}
+
+void test_regression_adj_scpi_query_format_com5003::setupServers(AtmelPermissionTemplate *permissionQueryHandler)
+{
+    m_resmanServer = std::make_unique<ResmanRunFacade>();
+    m_mockServer = std::make_unique<MockForSenseInterfaceCom5003>(permissionQueryHandler);
+    TimeMachineObject::feedEventLoop();
+
+    m_pcbClient = Zera::Proxy::getInstance()->getConnectionSmart("127.0.0.1", 6307);
+    m_pcbIFace = std::make_unique<Zera::cPCBInterface>();
+    m_pcbIFace->setClientSmart(m_pcbClient);
+    Zera::Proxy::getInstance()->startConnectionSmart(m_pcbClient);
+    TimeMachineObject::feedEventLoop();
+}
---
+++ com5003d/tests/test_regression_adj_scpi_query_format_com5003.h
@@ -0,0 +1,30 @@
+#ifndef TEST_REGRESSION_ADJ_SCPI_QUERY_FORMAT_COM5003_H
+#define TEST_REGRESSION_ADJ_SCPI_QUERY_FORMAT_COM5003_H
+
+#include "mockforsenseinterfacecom5003.h"
+#include "pcbinterface.h"
+#include "proxyclient.h"
+#include "resmanrunfacade.h"
+#include <QObject>
+
+class test_regression_adj_scpi_query_format_com5003 : public QObject
+{
+    Q_OBJECT
+private slots:
+    void initTestCase();
+    void init();
+    void cleanup();
+
+    void queryGainCorrectionSingle();
+
+private:
+    void setupServers(AtmelPermissionTemplate *permissionQueryHandler);
+
+    std::unique_ptr<MockForSenseInterfaceCom5003> m_mockServer;
+    std::unique_ptr<ResmanRunFacade> m_resmanServer;
+    Zera::ProxyClientPtr m_pcbClient;
+    std::unique_ptr<Zera::cPCBInterface> m_pcbIFace;
+    AtmelPermissionTemplatePtrU m_permissionMock;
+};
+
+#endif // TEST_REGRESSION_ADJ_SCPI_QUERY_FORMAT_COM5003_H
