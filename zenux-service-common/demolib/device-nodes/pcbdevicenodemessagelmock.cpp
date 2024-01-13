355aecc00dad1b1313236196c26275649b2d3f9e
Andreas Müller <schnitzeltony@gmail.com>
2024-01-14 00:14:28 +0100

    Remove PcbDeviceNodeMessagelMock
    It is not used and seems to be a typo accident
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/demolib/device-nodes/pcbdevicenodemessagelmock.cpp
+++
@@ -1,9 +0,0 @@
-#include "pcbdevicenodemessagelmock.h"
-#include "pcbdevicenodemessagemock.h"
-
-void PcbDeviceNodeMessagelMock::enableMock()
-{
-    m_createFunction = []() {
-        return std::make_unique<PcbDeviceNodeMessageMock>();
-    };
-}
