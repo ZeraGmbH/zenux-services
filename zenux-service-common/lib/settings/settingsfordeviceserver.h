25ec6e745d264ac66f97b3a04032e17df47158ab
Andreas Müller <schnitzeltony@gmail.com>
2024-01-14 23:51:39 +0100

    Introduce SettingsForDeviceServer
    * Required for full mock: cMT310S2dServer needs injection of
      AtmelCtrlFactoryInterface to run under test which needs I2c-settings. So for
      now have all necessary objects created outside
    * If complete (all settings / all servers) code will be in much better shape
    * For proof of concept for now let's contain I2c-settings (mandatory) and fpga-
      stiings (under test for merge)
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zenux-service-common/lib/settings/settingsfordeviceserver.h
@@ -0,0 +1,23 @@
+#ifndef SETTINGSFORDEVICESERVER_H
+#define SETTINGSFORDEVICESERVER_H
+
+#include "pcbserver.h"
+#include "i2csettings.h"
+#include "fpgasettings.h"
+#include <QObject>
+
+class SettingsForDeviceServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SettingsForDeviceServer(ServerParams params);
+    cI2CSettings *getI2cSettings();
+    FPGASettings *getFpgaSettings();
+private:
+    ServerParams m_params;
+    Zera::XMLConfig::cReader m_xmlConfigReader;
+    cI2CSettings m_i2cSettings;
+    FPGASettings m_fpgaSettings;
+};
+
+#endif // SETTINGSFORDEVICESERVER_H
