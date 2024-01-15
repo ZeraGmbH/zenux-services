7857e2b7f0b61fafe00cd0a9d6159cbff17aaa8d
Andreas Müller <schnitzeltony@gmail.com>
2024-01-15 02:04:04 +0100

    Rename AtmelCtrlFactoryStatic -> EmobCtrlFactory
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/micro-controller-io/emobctrlfactory.h
+++ zenux-service-common/lib/micro-controller-io/emobctrlfactory.h
@@ -1,9 +1,9 @@
-#ifndef ATMELCTRLFACTORYSTATIC_H
-#define ATMELCTRLFACTORYSTATIC_H
+#ifndef EMOBCTRLFACTORY_H
+#define EMOBCTRLFACTORY_H
 
 #include "atmelctrlemob.h"
 
-class AtmelCtrlFactoryStatic // TODO: Rename or make better
+class EmobCtrlFactory // TODO: Rename or make better
 {
 public:
     static AtmelCommonVersionsPtrS getEmobCtrl(QString devnode, quint8 adrCtrl, quint8 adrMux, quint8 muxChannel, quint8 debuglevel);
@@ -11,4 +11,4 @@ protected:
     static std::function<AtmelCommonVersionsPtrS(QString devnode, quint8 adrCtrl, quint8 adrMux, quint8 ctrlChannelForMux, quint8 debuglevel)> m_emobCreateFunction;
 };
 
-#endif // ATMELCTRLFACTORYSTATIC_H
+#endif // EMOBCTRLFACTORY_H
