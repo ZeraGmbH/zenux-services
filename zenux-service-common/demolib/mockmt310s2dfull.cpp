e69525224ee94ba7e688d3066043115ef4818bd3
Andreas Müller <schnitzeltony@gmail.com>
2024-01-16 20:30:01 +0100

    test_accumulatorinterface_mock: Test more with interesting results
    When using client interface client does not detect a difference between
    accumulator not supported no accumulator - maybe that is same for clients
    but how does zera-classes detect not to add scpi-interface?
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/demolib/mockmt310s2dfull.cpp
+++ zenux-service-common/demolib/mockmt310s2dfull.cpp
@@ -5,13 +5,15 @@
 #include "mockpcbdevicenodemessagesingleton.h"
 #include "mockfactorycontroller.h"
 
-MockMt310s2dFull::MockMt310s2dFull()
+MockMt310s2dFull::MockMt310s2dFull(QString alternateConfigXml)
 {
     MockI2cEEpromIoFactory::enableMock();
     MockPcbDeviceNodeCtrlSingleton::enableMock();
     MockPcbDeviceNodeMessageSingleton::enableMock();
 
     ServerParams params = MockPcbServer::createParams("mt310s2d");
+    if(!alternateConfigXml.isEmpty())
+        params.xmlFile = alternateConfigXml;
     std::shared_ptr<SettingsForDeviceServer> settings = std::make_shared<SettingsForDeviceServer>(params);
     m_server = std::make_unique<cMT310S2dServer>(settings, std::make_shared<MockFactoryController>(true));
 }
