9a2a41093394bfed7f92693173e40021d828b405
Andreas Müller <schnitzeltony@gmail.com>
2024-01-17 18:40:37 +0100

    Remove unused/unreachable I2cCtrlCommonVersions::readChannelStatus
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/micro-controller-io/i2cctrlcommonversions.cpp
+++ zenux-service-common/lib/micro-controller-io/i2cctrlcommonversions.cpp
@@ -9,7 +9,6 @@ enum hw_cmdcode
 {
     hwGetCtrlVersion = 0x0003,
     hwGetPCBVersion = 0x0005,
-    hwGetStatus = 0x1103
 };
 
 ZeraMControllerIoTemplate::atmelRM I2cCtrlCommonVersions::readPCBVersion(QString &answer)
@@ -20,17 +19,4 @@ ZeraMControllerIoTemplate::atmelRM I2cCtrlCommonVersions::readPCBVersion(QString
 ZeraMControllerIoTemplate::atmelRM I2cCtrlCommonVersions::readCTRLVersion(QString &answer)
 {
     return m_ctrlIo.readVariableLenText(hwGetCtrlVersion, answer);
-}
-
-ZeraMControllerIoTemplate::atmelRM I2cCtrlCommonVersions::readChannelStatus(quint8 channel, quint8 &stat)
-{
-    ZeraMControllerIo::atmelRM ret = ZeraMControllerIo::cmdexecfault;
-    quint8 answ[2];
-    hw_cmd CMD (hwGetStatus, channel, nullptr, 0);
-    m_ctrlIo.writeCommand(&CMD, answ, 2);
-    if(m_ctrlIo.getLastErrorMask() == 0) {
-        stat = answ[0];
-        ret = ZeraMControllerIo::cmddone;
-    }
-    return ret;
 }
