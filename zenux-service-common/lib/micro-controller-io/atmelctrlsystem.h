2c5ea0a6fe4623b09293bfee5c116ab251333935
Andreas Müller <schnitzeltony@gmail.com>
2024-01-17 18:46:11 +0100

    AtmelCtrlSystem: Replace readCTRLVersion/readPCBVersion by I2cCtrlCommonVersions
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/micro-controller-io/atmelctrlsystem.h
+++ zenux-service-common/lib/micro-controller-io/atmelctrlsystem.h
@@ -6,13 +6,11 @@
 #include <zeramcontrollerio.h>
 #include <QString>
 
-class AtmelCtrlSystem : public ZeraMControllerIo, public AbstractI2cCtrlCommonVersions, public AbstractI2cCtrlAccumulator
+class AtmelCtrlSystem : public ZeraMControllerIo, public AbstractI2cCtrlAccumulator
 {
 public:
     AtmelCtrlSystem(QString devnode, quint8 adr, quint8 debuglevel);
 
-    atmelRM readCTRLVersion(QString& answer) override;
-    atmelRM readPCBVersion(QString& answer) override;
     atmelRM readAccumulatorStatus(quint8& stat) override;
     atmelRM readAccuStateOfCharge(quint8& charge) override;
     atmelRM enableTestMode(qint32 testBits) override;
