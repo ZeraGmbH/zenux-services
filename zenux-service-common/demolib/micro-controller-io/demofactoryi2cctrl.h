dd0228894f9eff21701f5853b89b21c30d4616c8
Andreas Müller <schnitzeltony@gmail.com>
2024-01-19 23:45:42 +0100

    demofactoryi2cctrl: Set more reasonable device info
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/demolib/micro-controller-io/demofactoryi2cctrl.h
+++ zenux-service-common/demolib/micro-controller-io/demofactoryi2cctrl.h
@@ -25,9 +25,9 @@ private:
         bool m_permission = false;
         quint16 m_criticalStatus = 0;
         quint16 m_criticalStatusMask = 0;
-        QString m_deviceName = "Unknown";
-        QString m_serialNumber = "Unknown";
-        QString m_LCAVersion = "Unknown";
+        QString m_deviceName = "Device name";
+        QString m_serialNumber = "Serial number";
+        QString m_LCAVersion = "FPGA version";
         QString m_writablePcbVersion = "Unknown";
 
         quint8 m_pllChannel = 0;
