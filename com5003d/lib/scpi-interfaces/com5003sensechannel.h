8816d7a985809f6ca16bad86bef3da4066636c10
Andreas Müller <schnitzeltony@gmail.com>
2024-01-25 12:59:48 +0100

    Sense channels: Move - for now - final batch of methods to SenseChannelCommon
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- com5003d/lib/scpi-interfaces/com5003sensechannel.h
+++ com5003d/lib/scpi-interfaces/com5003sensechannel.h
@@ -5,19 +5,9 @@
 #include <QList>
 #include <sensechannelcommon.h>
 
-namespace SenseChannel
-{
-enum MMode
-{
-    modeAC,
-    modeREF,
-};
-}
-
 class Com5003SenseChannel : public SenseChannelCommon
 {
     Q_OBJECT
-
 public:
     Com5003SenseChannel(cSCPI* scpiinterface,
                         QString description,
@@ -27,13 +17,19 @@ public:
                         AbstractFactoryI2cCtrlPtr ctrlFactory);
     void setMMode(int m) override;
     QString getAlias() override;
-    void initSCPIConnection(QString leadingNodes) override;
-
-protected:
-    void executeProtoScpi(int cmdCode, cProtonetCommand* protoCmd) override;
 private:
     QString scpiReadWriteRange(QString& sInput) override;
     void setNotifierSenseChannelRange() override;
 };
+
+namespace SenseChannel
+{
+enum MMode
+{
+    modeAC,
+    modeREF,
+};
+}
+
 
 #endif // SENSECHANNEL_H
