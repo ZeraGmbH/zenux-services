8f5b89025dfbbfac2449e96c1b25bbec32b9a339
Andreas Müller <schnitzeltony@gmail.com>
2024-01-26 23:06:37 +0100

    zenux-service-common/testlib: Use TimeMachineObject::feedEventLoop
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/testlib/testallservicesmt310s2.cpp
+++ zenux-service-common/testlib/testallservicesmt310s2.cpp
@@ -1,14 +1,14 @@
 #include "testallservicesmt310s2.h"
-#include <demoeventloopfeeder.h>
+#include <timemachineobject.h>
 
 TestAllServicesMt310s2::TestAllServicesMt310s2(AbstractFactoryI2cCtrlPtr ctrlFactory)
 {
     m_resman = new ResmanRunFacade;
-    DemoEventLoopFeeder::feedEventLoop();
+    TimeMachineObject::feedEventLoop();;
     m_mockmt310s2d = new MockMt310s2d(ctrlFactory);
     m_mocksec1000d = new MockSec1000d;
     m_mockzdsp1d = new MockZdsp1d;
-    DemoEventLoopFeeder::feedEventLoop();
+    TimeMachineObject::feedEventLoop();
 }
 
 TestAllServicesMt310s2::~TestAllServicesMt310s2()
@@ -16,7 +16,7 @@ TestAllServicesMt310s2::~TestAllServicesMt310s2()
     delete m_mockzdsp1d;
     delete m_mocksec1000d;
     delete m_mockmt310s2d;
-    DemoEventLoopFeeder::feedEventLoop();
+    TimeMachineObject::feedEventLoop();
     delete m_resman;
-    DemoEventLoopFeeder::feedEventLoop();
+    TimeMachineObject::feedEventLoop();
 }
