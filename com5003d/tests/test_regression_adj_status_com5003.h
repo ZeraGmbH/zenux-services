0288cd013ec886e6c2977daa1e257bc644d93dbe
Andreas Müller <schnitzeltony@gmail.com>
2024-01-28 15:19:47 +0100

    com5003d/mt310s2d: Sync sense interface and add STATUS:PCB:ADJUSTMENT query
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ com5003d/tests/test_regression_adj_status_com5003.h
@@ -0,0 +1,27 @@
+#ifndef TEST_REGRESSION_ADJ_STATUS_COM5003_H
+#define TEST_REGRESSION_ADJ_STATUS_COM5003_H
+
+#include "testserverforsenseinterfacecom5003.h"
+#include "proxyclient.h"
+#include "resmanrunfacade.h"
+#include <QObject>
+
+class test_regression_adj_status_com5003 : public QObject
+{
+    Q_OBJECT
+private slots:
+    void initTestCase();
+    void cleanup();
+
+    void statusDefaultMachine();
+    void statusAllAdjusted();
+
+private:
+    void setupServers(AbstractFactoryI2cCtrlPtr ctrlFactory);
+
+    std::unique_ptr<TestServerForSenseInterfaceCom5003> m_testServer;
+    std::unique_ptr<ResmanRunFacade> m_resmanServer;
+    Zera::ProxyClientPtr m_pcbClient;
+};
+
+#endif // TEST_REGRESSION_ADJ_STATUS_COM5003_H
