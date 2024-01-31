fb47eddfff2f9d9b61237a17f236c592bdb6847e
Andreas Müller <schnitzeltony@gmail.com>
2024-01-31 22:53:52 +0100

    Demo: Ensure servers are up before starting modman & gui
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/demolib/demoallservicescom5003.cpp
+++ zenux-service-common/demolib/demoallservicescom5003.cpp
@@ -7,10 +7,16 @@ DemoAllServicesCom5003::DemoAllServicesCom5003()
     m_mockcom5003d = new MockCom5003d(std::make_shared<DemoFactoryI2cCtrl>());
     m_mocksec1000d = new MockSec1000d;
     m_mockzdsp1d = new MockZdsp1d;
+#ifdef GUI_SIMULATION
+    m_gui = new SimulQmlGui;
+#endif
 }
 
 DemoAllServicesCom5003::~DemoAllServicesCom5003()
 {
+#ifdef GUI_SIMULATION
+    delete m_gui;
+#endif
     delete m_mockzdsp1d;
     delete m_mocksec1000d;
     delete m_mockcom5003d;
--- zenux-service-common/demolib/demoallservicesmt310s2.cpp
+++ zenux-service-common/demolib/demoallservicesmt310s2.cpp
@@ -1,5 +1,6 @@
 #include "demoallservicesmt310s2.h"
 #include <demofactoryi2cctrl.h>
+#include <demoeventloopfeeder.h>
 
 DemoAllServicesMt310s2::DemoAllServicesMt310s2()
 {
@@ -7,10 +8,17 @@ DemoAllServicesMt310s2::DemoAllServicesMt310s2()
     m_mockmt310s2d = new MockMt310s2d(std::make_shared<DemoFactoryI2cCtrl>());
     m_mocksec1000d = new MockSec1000d;
     m_mockzdsp1d = new MockZdsp1d;
+    DemoEventLoopFeeder::feedEventLoop();
+#ifdef GUI_SIMULATION
+    m_gui = new SimulQmlGui;
+#endif
 }
 
 DemoAllServicesMt310s2::~DemoAllServicesMt310s2()
 {
+#ifdef GUI_SIMULATION
+    delete m_gui;
+#endif
     delete m_mockzdsp1d;
     delete m_mocksec1000d;
     delete m_mockmt310s2d;
