1999f6373d8670fab92582c8cb44a0b20cf54eb1
Andreas Müller <schnitzeltony@gmail.com>
2024-02-01 19:17:21 +0100

    zdsp1d: Add first learner's naive dsp variable test
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zdsp1d/tests/test_regression_dsp_var.h
@@ -0,0 +1,27 @@
+#ifndef TEST_REGRESSION_DSP_VAR_H
+#define TEST_REGRESSION_DSP_VAR_H
+
+#include "mockzdsp1d.h"
+#include "dspinterface.h"
+#include "proxyclient.h"
+#include "resmanrunfacade.h"
+#include <QObject>
+
+class test_regression_dsp_var : public QObject
+{
+    Q_OBJECT
+private slots:
+    void init();
+    void cleanup();
+    void createResultVariables();
+    void createTempVariables();
+    void createInternalVariables();
+    void createTempGlobalVariables();
+private:
+    std::unique_ptr<ResmanRunFacade> m_resmanServer;
+    std::unique_ptr<MockZdsp1d> m_dspService;
+    Zera::ProxyClientPtr m_proxyClient;
+    std::unique_ptr<Zera::cDSPInterface> m_dspIFace;
+};
+
+#endif // TEST_REGRESSION_DSP_VAR_H
