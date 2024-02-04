c006139098e1c1eef6e7600833d52426a0d3234d
Andreas Müller <schnitzeltony@gmail.com>
2024-02-04 23:46:43 +0100

    First step on making TestDeviceNodeDsp a spy
    Introduce TestSingletonDeviceNodeDsp::getInstancePtrTest: It gives access to
    TestDeviceNodeDsp object which we are going to extend to record activity on
    device nodes for regression tests.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zdsp1d/testlib/device-nodes/testsingletondevicenodedsp.h
+++ zdsp1d/testlib/device-nodes/testsingletondevicenodedsp.h
@@ -1,15 +1,17 @@
 #ifndef TESTSINGLETONDEVICENODEDSP_H
 #define TESTSINGLETONDEVICENODEDSP_H
 
+#include "testdevicenodedsp.h"
 #include <devicenodedsp.h>
 
 class TestSingletonDeviceNodeDsp
 {
 public:
     static AbstractDspDeviceNodePtr getInstancePtr(int dspMagicId);
+    static TestDeviceNodeDspPtr getInstancePtrTest(int dspMagicId  = DeviceNodeDsp::MAGIC_ID21362);
 private:
-    static AbstractDspDeviceNodePtr m_instanceMagicId21262;
-    static AbstractDspDeviceNodePtr m_instanceMagicId21362;
+    static TestDeviceNodeDspPtr m_instanceMagicId21262;
+    static TestDeviceNodeDspPtr m_instanceMagicId21362;
 };
 
 #endif // TESTSINGLETONDEVICENODEDSP_H
