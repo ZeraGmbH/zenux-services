c218cc37e5a7a1493d5c9cb48ef35f93bca375b8
Andreas Müller <schnitzeltony@gmail.com>
2024-02-05 22:09:53 +0100

    zdsp1d: Start listening device node activity on the example
    This is for now just a proof of concept.
    Seem we are missing something: DSP is reads on address 0 for both variables...
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zdsp1d/testlib/device-nodes/testdevicenodedsp.h
+++ zdsp1d/testlib/device-nodes/testdevicenodedsp.h
@@ -2,13 +2,14 @@
 #define TESTDEVICENODEDSP_H
 
 #include "abstractdspdevicenode.h"
+#include <QObject>
+#include <QVariant>
 
-class TestDeviceNodeDsp : public AbstractDspDeviceNode
+class TestDeviceNodeDsp : public QObject, public AbstractDspDeviceNode
 {
+    Q_OBJECT
 public:
     TestDeviceNodeDsp(int dspMagicId);
-    int open(QString devNodeFileName) override;
-    void close() override;
     bool dspReset() override;
     bool dspBoot(QString bootFileName) override;
     int dspRequestInt() override;
@@ -16,10 +17,15 @@ public:
     bool dspIsRunning() override;
     int lcaRawVersion() override;
 
+    int open(QString devNodeFileName) override;
+    void close() override;
     int lseek(ulong adr) override;
     bool write(ulong adr, const char* buf, int len) override;
     int read(char* buf, int len) override;
     void enableFasync() override;
+
+signals:
+    void sigIoOperation(QString funcName, QVariant par1 = QVariant(), QVariant par2 = QVariant(), QVariant par3 = QVariant());
 private:
     int m_dspMagicId;
 };
