ada2abf7e1c5100f199499a1bb274ccaa2fa4a23
Andreas Müller <schnitzeltony@gmail.com>
2024-02-09 13:28:20 +0100

    i2cctrlpll: Remove leftover declaration
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/micro-controller-io/i2cctrlpll.h
+++ zenux-service-common/lib/micro-controller-io/i2cctrlpll.h
@@ -7,7 +7,6 @@ class I2cCtrlPll : public AbstractI2cCtrlPll
 {
 public:
     I2cCtrlPll(QString deviceNodeName, quint8 i2cAddress, quint8 debugLevel);
-    ZeraMControllerIoTemplate::atmelRM extracted();
     ZeraMControllerIo::atmelRM setPLLChannel(quint8 chn) override;
     ZeraMControllerIo::atmelRM readPLLChannel(quint8& chn) override;
 private:
