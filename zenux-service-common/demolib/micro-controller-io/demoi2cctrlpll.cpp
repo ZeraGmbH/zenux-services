744096ba63a92bbe2e36bd5c5125eff7307de9f1
Andreas Müller <schnitzeltony@gmail.com>
2024-02-10 15:09:12 +0100

    Finally display PLL channel/mode in simulation GUI
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/demolib/micro-controller-io/demoi2cctrlpll.cpp
+++ zenux-service-common/demolib/micro-controller-io/demoi2cctrlpll.cpp
@@ -1,4 +1,5 @@
 #include "demoi2cctrlpll.h"
+#include "simulsystemstatus.h"
 
 DemoI2cCtrlPll::DemoI2cCtrlPll(SamplingSettings* samplingSettings, quint8 &pllChannel) :
     m_samplingSettings(samplingSettings),
@@ -6,9 +7,22 @@ DemoI2cCtrlPll::DemoI2cCtrlPll(SamplingSettings* samplingSettings, quint8 &pllCh
 {
 }
 
+void DemoI2cCtrlPll::setSimulPllDisplay(quint8 chn)
+{
+    // Again some over engineering: Code is preparerd for multiple PLL or whatever settings
+    // There have been one channel-setting eversince this code exist...
+    QList<SamplingSettings::ChannelSettings*>& channelSettings = m_samplingSettings->getChannelSettings();
+    if(channelSettings.count() > 0) {
+        QStringList pllModes = channelSettings[0]->m_pllChannels;
+        if(chn < pllModes.count())
+            SimulSystemStatus::getInstance()->setPllMode(pllModes[chn]);
+    }
+}
+
 ZeraMControllerIoTemplate::atmelRM DemoI2cCtrlPll::setPLLChannel(quint8 chn)
 {
     m_pllChannel = chn;
+    setSimulPllDisplay(chn);
     return ZeraMControllerIo::atmelRM::cmddone;
 }
 
