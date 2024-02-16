661e2986fd40a99e9520c392a8b37cb9d8bb57c8
Andreas Müller <schnitzeltony@gmail.com>
2024-02-17 00:36:21 +0100

    DspInterfaceCmdDecoder::setVarData: Code cleanup - no functional change yet
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-interfaces/lib/dspinterfacecmddecoder.cpp
+++ zenux-service-interfaces/lib/dspinterfacecmddecoder.cpp
@@ -2,41 +2,23 @@
 
 void DspInterfaceCmdDecoder::setVarData(cDspMeasData *memgroup, QString datalist, DSPDATA::dType type)
 {
-    QStringList DataEntryList, DataList; // werte zuorden
-
-    bool ok;
-
     // we expect something like Name1:0.2,0.4,0,3;Name2:1,2,3; ......
-
-    DataEntryList = datalist.split(";"); // wir haben jetzt eine stringliste mit allen werten
-
-    for ( QStringList::Iterator it = DataEntryList.begin(); it != DataEntryList.end(); ++it )
-    {
-        QString snamedata, sname, sdata;
-
-        snamedata = *it;
-        if (!snamedata.isEmpty())
-        {
-            sname = snamedata.section(":",0,0);
-            sdata = snamedata.section(":",1,1);
-
-            float *val = memgroup->data(sname);
-            if (val != 0)
-            {
-                DataList = sdata.split(",");
-                for ( QStringList::Iterator it2 = DataList.begin(); it2 != DataList.end(); ++it2,val++ )
-                {
-                    QString s;
-                    s = *it2;
-                    s.remove(';');
-                    if (type == DSPDATA::dInt)
-                    {
-                        ulong vul = s.toULong(&ok); // test auf ulong
-                        *((ulong*) val) = vul;
-                    }
-                    else
-                        *val = s.toFloat();
+    QStringList dataEntryList = datalist.split(";", Qt::SkipEmptyParts);
+    for (const QString &dspVarEntry : dataEntryList) {
+        QString varName = dspVarEntry.section(":",0,0);
+        QString varValues = dspVarEntry.section(":",1,1);
+        float *val = memgroup->data(varName);
+        if (val != 0) {
+            QStringList DataList = varValues.split(",");
+            for ( QStringList::Iterator it2 = DataList.begin(); it2 != DataList.end(); ++it2,val++ ) {
+                QString strValue = *it2;
+                strValue.remove(';');
+                if (type == DSPDATA::dInt) {
+                    ulong vul = strValue.toULong(); // test auf ulong
+                    *((ulong*) val) = vul;
                 }
+                else
+                    *val = strValue.toFloat();
             }
         }
     }
