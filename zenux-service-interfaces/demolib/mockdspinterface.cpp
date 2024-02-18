03d1899d043822cbbe6c065b7199946c4b886773
Andreas Müller <schnitzeltony@gmail.com>
2024-02-19 00:16:47 +0100

    mockdspinterface: Increase readability
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-interfaces/demolib/mockdspinterface.cpp
+++ zenux-service-interfaces/demolib/mockdspinterface.cpp
@@ -1,5 +1,6 @@
 #include "mockdspinterface.h"
 #include "notzeronumgen.h"
+#include "reply.h"
 
 void MockDspInterface::fireActValInterrupt(QVector<float> actualValues, int irqNo)
 {
@@ -8,20 +9,20 @@ void MockDspInterface::fireActValInterrupt(QVector<float> actualValues, int irqN
     // 'DSPINT': they look just for irq number (after ':'). See how this
     // evolves once working on SEC.
     QString message = QString("DSPINT:%1").arg(irqNo);
-    emit serverAnswer(0, 0, message);
+    constexpr quint32 messageNum = 0; // async interrupt
+    emit serverAnswer(messageNum, ack, message);
 }
 
 quint32 MockDspInterface::dataAcquisition(cDspMeasData *memgroup)
 {
     memgroup->setData(m_actualValues);
     quint32 messageNum = NotZeroNumGen::getMsgNr();
-    quint8 reply = 0;
     QVariant answer = QString();
     QMetaObject::invokeMethod(this,
                               "serverAnswer",
                               Qt::QueuedConnection,
                               Q_ARG(quint32, messageNum),
-                              Q_ARG(quint8, reply),
+                              Q_ARG(quint8, ack),
                               Q_ARG(QVariant, answer)
                               );
     return messageNum;
