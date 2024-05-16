ZeraGmbH/zenux-services

commit 42407cb183e7336d925ddf5337a86a1fe783b010

cClamp: Use AdjustmentEepromReadWrite instead of inheriting AdjustmentEeprom
Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

zenux-service-common/lib/scpi-interfaces/clampinterface.cpp
@@ -162,7 +162,7 @@ QString cClampInterface::writeAllClamps(QString &sInput)
                     bool done = true;
                     QDateTime now = QDateTime::currentDateTime();
                     for(auto clamp : qAsConst(m_clampHash))
-                        done = done && clamp->exportAdjFlash(now);
+                        done = done && clamp->exportAdjData(now);
                     if (!done)
                         return ZSCPI::scpiAnswer[ZSCPI::errexec];
                 }
@@ -227,7 +227,7 @@ QString cClampInterface::importClampXmls(QString allXML, bool computeAndExport)
                     m_pSenseInterface->computeSenseAdjData();
                     // then we let it compute its new adjustment coefficients... we simply call senseinterface's compute
                     // command. we compute a little bit to much but this doesn't matter at all
-                    if (!pClamp4Use->exportAdjFlash(QDateTime::currentDateTime())) // and then we program the clamp
+                    if (!pClamp4Use->exportAdjData(QDateTime::currentDateTime())) // and then we program the clamp
                         return ZSCPI::scpiAnswer[ZSCPI::errexec];
                 }
             }

zenux-service-common/lib/scpi-interfaces/clamp.cpp
@@ -25,7 +25,8 @@ enum Commands
 };
 
 cClamp::cClamp() :
-    ScpiConnection(nullptr) // TODO get rid of dummy clamp
+    ScpiConnection(nullptr), // TODO get rid of dummy clamp
+    m_adjReadWrite("", 0, nullptr)
 {
 }
 
@@ -37,14 +38,14 @@ cClamp::cClamp(cPCBServer *server,
                I2cMuxerInterface::Ptr i2cMuxer,
                quint8 ctrlChannelSecondary,
                quint8 type) :
-    AdjustmentEeprom(i2cSettings->getDeviceNode(),
-                    i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
-                    i2cMuxer),
     ScpiConnection(server->getSCPIInterface()),
     m_pSenseInterface(senseInterface),
     m_sChannelName(channelName),
     m_nCtrlChannel(ctrlChannel),
-    m_nCtrlChannelSecondary(ctrlChannelSecondary)
+    m_nCtrlChannelSecondary(ctrlChannelSecondary),
+    m_adjReadWrite(i2cSettings->getDeviceNode(),
+                   i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
+                   i2cMuxer)
 {
     if(type == undefined)
         type = readClampType();
@@ -58,7 +59,7 @@ cClamp::cClamp(cPCBServer *server,
     connect(server, &cPCBServer::removeSubscribers, this, &ScpiConnection::onRemoveSubscribers);
 
     if (type != undefined) {
-        importAdjFlash();
+        importAdjData();
         addSense();
         addSenseInterface();
     }
@@ -130,8 +131,11 @@ void cClamp::executeProtoScpi(int cmdCode, cProtonetCommand *protoCmd)
     }
 }
 
-void cClamp::exportAdjData(QDataStream &stream, QDateTime dateTimeWrite)
+bool cClamp::exportAdjData(QDateTime dateTimeWrite)
 {
+    QByteArray ba;
+    QDataStream stream(&ba,QIODevice::ReadWrite);
+    stream.setVersion(QDataStream::Qt_5_4);
     m_AdjDateTime = dateTimeWrite;
     stream << m_nType;
     stream << m_nFlags;
@@ -150,41 +154,47 @@ void cClamp::exportAdjData(QDataStream &stream, QDateTime dateTimeWrite)
         stream << spec;
         range->getJustData()->Serialize(stream);
     }
+    m_adjReadWrite.setAdjData(ba);
+    return m_adjReadWrite.exportAdjFlash();
 }
 
-bool cClamp::importAdjData(QByteArray& ba)
+bool cClamp::importAdjData()
 {
-    QDataStream stream(&ba, QIODevice::ReadOnly);
-    stream.setVersion(QDataStream::Qt_5_4);
+    if(m_adjReadWrite.importAdjFlash()) {
+        QByteArray ba = m_adjReadWrite.getAdjData();
+        QDataStream stream(&ba, QIODevice::ReadOnly);
+        stream.setVersion(QDataStream::Qt_5_4);
 
-    stream.skipRawData(6);
-    stream >> m_nType;
-    stream >> m_nFlags;
-    QString clampTypeNameDummy;
-    stream >> clampTypeNameDummy; // for sake of compatibility
-    stream >> m_sVersion;
-    stream >> m_sSerial;
-    QString dts;
-    stream >> dts;
+        stream.skipRawData(6);
+        stream >> m_nType;
+        stream >> m_nFlags;
+        QString clampTypeNameDummy;
+        stream >> clampTypeNameDummy; // for sake of compatibility
+        stream >> m_sVersion;
+        stream >> m_sSerial;
+        QString dts;
+        stream >> dts;
 
-    int n = 0;
-    m_AdjDateTime = QDateTime::fromString(dts);
-    while (!stream.atEnd()) {
-        QString rngName;
-        stream >> rngName;
-        SenseRangeCommon* range = getRange(rngName);
-        if (range != 0) {
-            n++;
-            range->getJustData()->Deserialize(stream);
+        int n = 0;
+        m_AdjDateTime = QDateTime::fromString(dts);
+        while (!stream.atEnd()) {
+            QString rngName;
+            stream >> rngName;
+            SenseRangeCommon* range = getRange(rngName);
+            if (range != 0) {
+                n++;
+                range->getJustData()->Deserialize(stream);
+            }
+            else {
+                // range not found: read dummy to keep serialization in sync
+                RangeAdjInterface *dummy = new RangeAdjInterface(m_pSCPIInterface, AdjustScpiValueFormatterFactory::createMt310s2AdjFormatter());
+                dummy->Deserialize(stream); // we read the data from stream to keep it in flow
+                delete dummy;
+            }
         }
-        else {
-            // range not found: read dummy to keep serialization in sync
-            RangeAdjInterface *dummy = new RangeAdjInterface(m_pSCPIInterface, AdjustScpiValueFormatterFactory::createMt310s2AdjFormatter());
-            dummy->Deserialize(stream); // we read the data from stream to keep it in flow
-            delete dummy;
-        }
+        return (n == m_RangeList.count() + m_RangeListSecondary.count()); // it's ok if we found data for all ranges in our list
     }
-    return (n == m_RangeList.count() + m_RangeListSecondary.count()); // it's ok if we found data for all ranges in our list
+    return false;
 }
 
 QString cClamp::exportXMLString(int indent)
@@ -230,7 +240,7 @@ QString cClamp::exportXMLString(int indent)
 
     QDomElement chksumtag = justqdom.createElement("Chksum");
     adjtag.appendChild(chksumtag);
-    t = justqdom.createTextNode(QString("0x%1").arg(getChecksum(), 0, 16));
+    t = justqdom.createTextNode(QString("0x%1").arg(m_adjReadWrite.getChecksum(), 0, 16));
     chksumtag.appendChild(t);
 
     QDomElement typeTag = justqdom.createElement( "Sense");
@@ -410,9 +420,8 @@ quint8 cClamp::getAdjStatus()
 
 cClamp::ClampTypes cClamp::readClampType()
 {
-    I2cMuxerScopedOnOff i2cMuxOnOff(getI2cMuxer());
-    QByteArray ba;
-    if (readEepromChecksumValidated(ba)) {
+    if(m_adjReadWrite.importAdjFlash()) {
+        QByteArray ba = m_adjReadWrite.getAdjData();
         quint8 type;
         QDataStream stream(&ba, QIODevice::ReadWrite);
         stream.setVersion(QDataStream::Qt_5_4);
@@ -888,7 +897,7 @@ QString cClamp::scpiReadWriteType(QString& scpi)
             if ( (type > undefined) && (type < anzCL)) {
                 removeAllRanges();
                 initClamp(type);
-                if (exportAdjFlash(QDateTime::currentDateTime())) {
+                if (exportAdjData(QDateTime::currentDateTime())) {
                     addSense();
                     addSenseInterface();
                     answer = ZSCPI::scpiAnswer[ZSCPI::ack];
@@ -926,7 +935,7 @@ QString cClamp::scpiWriteFlash(QString& scpi)
     QString answer;
     cSCPICommand cmd = scpi;
     if (cmd.isCommand(1) && (cmd.getParam(0) == "")) {
-        if (exportAdjFlash(QDateTime::currentDateTime())) {
+        if (exportAdjData(QDateTime::currentDateTime())) {
             answer = ZSCPI::scpiAnswer[ZSCPI::ack];
         }
         else {
@@ -945,7 +954,7 @@ QString cClamp::scpiReadFlash(QString& scpi)
     cSCPICommand cmd = scpi;
     if (cmd.isCommand(1) && (cmd.getParam(0) == "")) {
         if (readClampType() == m_nType) { // we first look whether the type matches
-            importAdjFlash();
+            importAdjData();
             answer = ZSCPI::scpiAnswer[ZSCPI::ack];
         }
         else {
@@ -963,7 +972,7 @@ QString cClamp::scpiResetFlash(QString &scpi)
     QString answer;
     cSCPICommand cmd = scpi;
     if (cmd.isCommand(1) && (cmd.getParam(0) == "")) {
-        if (resetAdjFlash()) {
+        if (m_adjReadWrite.resetAdjFlash()) {
             answer = ZSCPI::scpiAnswer[ZSCPI::ack];
         }
         else {
@@ -981,7 +990,7 @@ QString cClamp::scpiReadChksum(QString& scpi)
     QString answer;
     cSCPICommand cmd = scpi;
     if (cmd.isQuery()) {
-        answer = QString("0x%1").arg(getChecksum(),0,16); // hex output
+        answer = QString("0x%1").arg(m_adjReadWrite.getChecksum(),0,16); // hex output
     }
     else {
         answer = ZSCPI::scpiAnswer[ZSCPI::nak];

zenux-service-common/lib/scpi-interfaces/clamp.h
@@ -5,11 +5,12 @@
 #include "pcbserver.h"
 #include "adjustmenteeprom.h"
 #include "adjustmentxmlimportexporttemplate.h"
+#include "adjustmenteepromreadwrite.h"
 #include <QList>
 #include <QDateTime>
 #include <QDomElement>
 
-class cClamp: public AdjustmentEeprom, public AdjustmentXmlImportExportTemplate, public ScpiConnection
+class cClamp: public AdjustmentXmlImportExportTemplate, public ScpiConnection
 {
 public:
     enum ClampTypes // APPEND NEW TYPES AT BOTTOM
@@ -49,11 +50,11 @@ public:
     QString getSerial();
     virtual QString exportXMLString(int indent = 1) override;
     bool importXMLDocument(QDomDocument *qdomdoc, bool ignoreType);
+    bool exportAdjData(QDateTime dateTimeWrite);
+    bool importAdjData();
 
 protected:
     void executeProtoScpi(int cmdCode, cProtonetCommand* protoCmd) override;
-    void exportAdjData(QDataStream& stream, QDateTime dateTimeWrite) override;
-    bool importAdjData(QByteArray& ba) override;
     bool importXMLDocument(QDomDocument* qdomdoc) override;
 
 private:
@@ -96,6 +97,7 @@ private:
     quint8 m_nType = undefined;
     quint32 m_nFlags = 0; // for future purpose
     QDateTime m_AdjDateTime;
+    AdjustmentEepromReadWrite m_adjReadWrite;
 };
 
 #endif // CLAMP_H

zenux-service-common/lib/scpi-interfaces/senseinterfacecommon.cpp
@@ -11,11 +11,11 @@ SenseInterfaceCommon::SenseInterfaceCommon(cSCPI *scpiInterface,
                                            SystemInfo *systemInfo,
                                            AbstractFactoryI2cCtrlPtr ctrlFactory) :
     cResource(scpiInterface),
-    m_adjReadWrite(i2cSettings->getDeviceNode(),
-                     i2cSettings->getI2CAdress(i2cSettings::flashlI2cAddress),
-                     I2cMultiplexerFactory::createNullMuxer()),
     m_systemInfo(systemInfo),
-    m_ctrlFactory(ctrlFactory)
+    m_ctrlFactory(ctrlFactory),
+    m_adjReadWrite(i2cSettings->getDeviceNode(),
+                   i2cSettings->getI2CAdress(i2cSettings::flashlI2cAddress),
+                   I2cMultiplexerFactory::createNullMuxer())
 {
 }
 

commit 3786341f9b1f6bb4ddd073caefeba79f299fa201

Add regression tests for setting fresh clamps - failing for regression detected
We introduced a regression with commit 42407cb183e7336d925ddf5337a86a1fe783b010
so the test was created and the tests data taken one commit before

Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

mt310s2d/testlib/testserverforsenseinterfacemt310s2.cpp
@@ -39,11 +39,11 @@ TestServerForSenseInterfaceMt310s2::TestServerForSenseInterfaceMt310s2(AbstractF
     start();
 }
 
-void TestServerForSenseInterfaceMt310s2::addClamp(int clampTypeNo, QString channelAlias1)
+cClamp * TestServerForSenseInterfaceMt310s2::addClamp(int clampTypeNo, QString channelAlias1)
 {
     ClampFactoryTest::setTestClampType(clampTypeNo);
     SenseSystem::cChannelSettings *channelSettingClamps = getSenseSettings()->findChannelSettingByAlias1(channelAlias1);
-    getClampInterface()->addClamp(channelSettingClamps, I2cMultiplexerFactory::createNullMuxer());
+    return getClampInterface()->addClamp(channelSettingClamps, I2cMultiplexerFactory::createNullMuxer());
 }
 
 void TestServerForSenseInterfaceMt310s2::removeAllClamps()

mt310s2d/testlib/testserverforsenseinterfacemt310s2.h
@@ -19,7 +19,7 @@ public:
     I2cSettings *getI2cSettings() { return m_settings->getI2cSettings(); }
     Mt310s2SystemInfo *getSystemInfo() { return m_systemInfo.get(); }
 
-    void addClamp(int clampTypeNo, QString channelAlias1);
+    cClamp *addClamp(int clampTypeNo, QString channelAlias1);
     void removeAllClamps();
 
 private:

zenux-service-common/lib/scpi-interfaces/clampinterface.cpp
@@ -30,7 +30,7 @@ void cClampInterface::initSCPIConnection(QString leadingNodes)
     addDelegate(QString("%1SYSTEM:ADJUSTMENT:CLAMP").arg(leadingNodes),"XML",SCPI::isQuery | SCPI::isCmdwP, m_pSCPIInterface, ClampSystem::cmdClampImportExport);
 }
 
-void cClampInterface::addClamp(const SenseSystem::cChannelSettings *chSettings, I2cMuxerInterface::Ptr i2cMuxer)
+cClamp *cClampInterface::addClamp(const SenseSystem::cChannelSettings *chSettings, I2cMuxerInterface::Ptr i2cMuxer)
 {
     m_nClampStatus |= (1<<chSettings->m_nPluggedBit);
 
@@ -47,6 +47,7 @@ void cClampInterface::addClamp(const SenseSystem::cChannelSettings *chSettings, 
         qInfo("Add secondary clamp ranges for \"%s\"", qPrintable(m_senseSettings->findChannelSettingByMxName(channelNameSecondary)->m_sAlias1));
     }
     generateAndNotifyClampChannelList();
+    return clamp;
 }
 
 QString cClampInterface::exportXMLString(int indent)

zenux-service-common/lib/scpi-interfaces/clampinterface.h
@@ -33,7 +33,7 @@ public:
     virtual void initSCPIConnection(QString leadingNodes) override;
     void actualizeClampStatus(quint16 devConnectedMask);
     // lazy: public for test
-    void addClamp(const SenseSystem::cChannelSettings *chSettings, I2cMuxerInterface::Ptr i2cMuxer);
+    cClamp* addClamp(const SenseSystem::cChannelSettings *chSettings, I2cMuxerInterface::Ptr i2cMuxer);
     QString exportXMLString(int indent = 1);
     QString importClampXmls(QString allXML, bool computeAndExport);
 

mt310s2d/tests/regression_data_adj_import_export/regression_data.qrc
@@ -11,5 +11,8 @@
         <file>import_missing_type.xml</file>
         <file>import_modified.xml</file>
         <file>import_unknown_entry.xml</file>
+        <file>initial_clamp_type_i.eeprom</file>
+        <file>initial_clamp_type_u.eeprom</file>
+        <file>initial_clamp_type_ui.eeprom</file>
     </qresource>
 </RCC>

mt310s2d/tests/test_regression_adj_import_export_eeprom_mt310s2.cpp
@@ -161,6 +161,96 @@ void test_regression_adj_import_export_eeprom_mt310s2::loadArbitraryVersionToEEp
     QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
 }
 
+void test_regression_adj_import_export_eeprom_mt310s2::freshClampSetTypeIOnlyEepromGen()
+{
+    setupServers(std::make_shared<TestFactoryI2cCtrl>(true));
+    cClamp *clamp = m_testServer->addClamp(0, "IAUX");
+
+    QString ret = ScpiSingleTransactionBlocked::cmd("SYSTEM:CLAMP:m7:TYPE", QString::number(cClamp::CL120A));
+    QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
+    QVERIFY(clamp->exportAdjData(refTime));
+
+    I2cSettings *i2cSettings = m_testServer->getI2cSettings();
+    QVERIFY(MockEEprom24LC::mockWriteToFile(i2cSettings->getDeviceNode(),
+                                            i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
+                                            "/tmp/initial_clamp_type_i.eeprom"));
+}
+
+void test_regression_adj_import_export_eeprom_mt310s2::freshClampSetTypeIOnlyEepromCheck()
+{
+    setupServers(std::make_shared<TestFactoryI2cCtrl>(true));
+    cClamp *clamp = m_testServer->addClamp(0, "IAUX");
+
+    QString ret = ScpiSingleTransactionBlocked::cmd("SYSTEM:CLAMP:m7:TYPE", QString::number(cClamp::CL120A));
+    QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
+    QVERIFY(clamp->exportAdjData(refTime));
+
+    I2cSettings *i2cSettings = m_testServer->getI2cSettings();
+    QVERIFY(MockEEprom24LC::mockCompareWithFile(i2cSettings->getDeviceNode(),
+                                                i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
+                                                ":/initial_clamp_type_i.eeprom"));
+}
+
+void test_regression_adj_import_export_eeprom_mt310s2::freshClampSetTypeUOnlyEepromGen()
+{
+    setupServers(std::make_shared<TestFactoryI2cCtrl>(true));
+    cClamp *clamp = m_testServer->addClamp(0, "IAUX");
+
+    QString ret = ScpiSingleTransactionBlocked::cmd("SYSTEM:CLAMP:m7:TYPE", QString::number(cClamp::CL1000VDC));
+    QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
+    QVERIFY(clamp->exportAdjData(refTime));
+
+    I2cSettings *i2cSettings = m_testServer->getI2cSettings();
+    QVERIFY(MockEEprom24LC::mockWriteToFile(i2cSettings->getDeviceNode(),
+                                            i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
+                                            "/tmp/initial_clamp_type_u.eeprom"));
+}
+
+void test_regression_adj_import_export_eeprom_mt310s2::freshClampSetTypeUOnlyEepromCheck()
+{
+    setupServers(std::make_shared<TestFactoryI2cCtrl>(true));
+    cClamp *clamp = m_testServer->addClamp(0, "IAUX");
+
+    QString ret = ScpiSingleTransactionBlocked::cmd("SYSTEM:CLAMP:m7:TYPE", QString::number(cClamp::CL1000VDC));
+    QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
+    QVERIFY(clamp->exportAdjData(refTime));
+
+    I2cSettings *i2cSettings = m_testServer->getI2cSettings();
+    QVERIFY(MockEEprom24LC::mockCompareWithFile(i2cSettings->getDeviceNode(),
+                                                i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
+                                                ":/initial_clamp_type_u.eeprom"));
+}
+
+void test_regression_adj_import_export_eeprom_mt310s2::freshClampSetTypeUIEepromGen()
+{
+    setupServers(std::make_shared<TestFactoryI2cCtrl>(true));
+    cClamp *clamp = m_testServer->addClamp(0, "IAUX");
+
+    QString ret = ScpiSingleTransactionBlocked::cmd("SYSTEM:CLAMP:m7:TYPE", QString::number(cClamp::CL200ADC1000VDC));
+    QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
+    QVERIFY(clamp->exportAdjData(refTime));
+
+    I2cSettings *i2cSettings = m_testServer->getI2cSettings();
+    QVERIFY(MockEEprom24LC::mockWriteToFile(i2cSettings->getDeviceNode(),
+                                            i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
+                                            "/tmp/initial_clamp_type_ui.eeprom"));
+}
+
+void test_regression_adj_import_export_eeprom_mt310s2::freshClampSetTypeUIEepromCheck()
+{
+    setupServers(std::make_shared<TestFactoryI2cCtrl>(true));
+    cClamp *clamp = m_testServer->addClamp(0, "IAUX");
+
+    QString ret = ScpiSingleTransactionBlocked::cmd("SYSTEM:CLAMP:m7:TYPE", QString::number(cClamp::CL200ADC1000VDC));
+    QCOMPARE(ret, ZSCPI::scpiAnswer[ZSCPI::ack]);
+    QVERIFY(clamp->exportAdjData(refTime));
+
+    I2cSettings *i2cSettings = m_testServer->getI2cSettings();
+    QVERIFY(MockEEprom24LC::mockCompareWithFile(i2cSettings->getDeviceNode(),
+                                                i2cSettings->getI2CAdress(i2cSettings::clampFlashI2cAddress),
+                                                ":/initial_clamp_type_ui.eeprom"));
+}
+
 void test_regression_adj_import_export_eeprom_mt310s2::setupServers(AbstractFactoryI2cCtrlPtr ctrlFactory)
 {
     m_resmanServer = std::make_unique<ResmanRunFacade>();

mt310s2d/tests/test_regression_adj_import_export_eeprom_mt310s2.h
@@ -25,7 +25,13 @@ private slots:
 
     void directExportFlashArbitraryVersionGen();
     void loadArbitraryVersionToEEprom();
-    // all tests for clamps
+
+    void freshClampSetTypeIOnlyEepromGen();
+    void freshClampSetTypeIOnlyEepromCheck();
+    void freshClampSetTypeUOnlyEepromGen();
+    void freshClampSetTypeUOnlyEepromCheck();
+    void freshClampSetTypeUIEepromGen();
+    void freshClampSetTypeUIEepromCheck();
 private:
     void setupServers(AbstractFactoryI2cCtrlPtr ctrlFactory);
 