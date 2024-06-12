3e33a2e8f06be870575cc9ae228bdf1dc1652aaa
Andreas Müller <schnitzeltony@gmail.com>
2024-06-12 19:16:36 +0200

    Add support for systemd socket
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/app/cmdserver.cpp
+++ auto-journal-logger/app/cmdserver.cpp
@@ -1,5 +1,6 @@
 #include "cmdserver.h"
 #include "cmdparserbase.h"
+#include <systemd/sd-daemon.h>
 
 CmdServer::CmdServer(QObject* parent) : QObject(parent)
 {
@@ -9,11 +10,35 @@ CmdServer::CmdServer(QObject* parent) : QObject(parent)
 
 bool CmdServer::Setup(quint16 ui16IPPort, QSimpleCmdParserSocketBase *pParser)
 {
+    bool retVal = false;
     m_pParser = pParser;
     pParser->SetListenPort(ui16IPPort);
-    return m_Server.listen(QHostAddress::Any, ui16IPPort);
-}
 
+    //check if systemd has handed us a socket (service socket activation)
+    int tmpSocketDescriptor = -1;
+    const int socketCount = sd_listen_fds(0);
+    for(int i=0; i<socketCount; ++i) {
+        if(sd_is_socket_inet(SD_LISTEN_FDS_START+i, AF_UNSPEC, SOCK_STREAM, 1, ui16IPPort)) {
+            tmpSocketDescriptor = SD_LISTEN_FDS_START+i;
+            break;
+        }
+    }
+    if(tmpSocketDescriptor >= SD_LISTEN_FDS_START) {
+        if(m_Server.setSocketDescriptor(tmpSocketDescriptor)) {
+            retVal = true;
+            qInfo()<<"Inherited socket descriptor from systemd, listening on port:" << ui16IPPort;
+        }
+    }
+    if(!m_Server.isListening()) {
+        if(m_Server.listen(QHostAddress::Any, ui16IPPort)) {
+            retVal = true;
+            qInfo()<<"Server Started on port:" << ui16IPPort;
+        }
+        else
+            qCritical() << "Server could not listen on port:" << ui16IPPort << "error:" << m_Server.errorString();
+    }
+    return retVal;
+}
 
 void CmdServer::OnClientNew()
 {
