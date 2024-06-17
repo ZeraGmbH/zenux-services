c53f63f7d2f68cb2fadb66036d10e21b34fb7c6a
Andreas Müller <schnitzeltony@gmail.com>
2024-06-17 22:55:44 +0200

    Use server implementation from simple-parser
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/lib/logcreatorjournalsimple.cpp
+++ auto-journal-logger/lib/logcreatorjournalsimple.cpp
@@ -4,7 +4,7 @@
 bool LogCreatorJournalSimple::storeLogs(QString dir)
 {
     QDateTime now = QDateTime::currentDateTime();
-    QString fileName = dir + "/zenux-" + now.toString("yyyy_MM_dd-HH_mm_ss") + ".log";
+    QString fileName = dir + "/" + now.toString("yyyy_MM_dd-HH_mm_ss") + ".log";
     QString command = "journalctl -o short-precise --boot >> " + fileName;
     if(system(qPrintable(command)) == 0)
         return true;
