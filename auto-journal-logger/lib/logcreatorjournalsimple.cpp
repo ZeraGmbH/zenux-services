a463df323ba2ee53c3da078bd872890e2dfa2e60
Andreas Müller <schnitzeltony@gmail.com>
2024-06-18 00:20:09 +0200

    LogCreatorJournalSimple::storeLogs: Save last three boots / each in a file
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/lib/logcreatorjournalsimple.cpp
+++ auto-journal-logger/lib/logcreatorjournalsimple.cpp
@@ -4,9 +4,11 @@
 bool LogCreatorJournalSimple::storeLogs(QString dir)
 {
     QDateTime now = QDateTime::currentDateTime();
-    QString fileName = dir + "/" + now.toString("yyyy_MM_dd-HH_mm_ss") + ".log";
-    QString command = "journalctl -o short-precise --boot >> " + fileName;
-    if(system(qPrintable(command)) == 0)
-        return true;
-    return false;
+    QString fileName = dir + "/" + now.toString("yyyy_MM_dd-HH_mm_ss");
+    for(int boot = 0; boot<3; boot++) {
+        QString command = QString("journalctl -o short-precise --boot -%1 >> %2.log-%1").arg(boot).arg(fileName);
+        if(system(qPrintable(command)) != 0)
+            return false;
+    }
+    return true;
 }
