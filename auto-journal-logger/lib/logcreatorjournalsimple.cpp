0e24f241670876f520a1d790191813be40e5e65f
Andreas Müller <schnitzeltony@gmail.com>
2024-06-19 23:14:57 +0200

    auto-journal-logger: Rework folder/file name structure
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/lib/logcreatorjournalsimple.cpp
+++ auto-journal-logger/lib/logcreatorjournalsimple.cpp
@@ -3,10 +3,9 @@
 
 bool LogCreatorJournalSimple::storeLogs(QString dir)
 {
-    QDateTime now = QDateTime::currentDateTime();
-    QString fileName = dir + "/" + now.toString("yyyy_MM_dd-HH_mm_ss");
+    QString fileName = dir + "/journal.log";
     for(int boot = 0; boot<3; boot++) {
-        QString command = QString("journalctl -o short-precise --boot -%1 >> %2.log-%1").arg(boot).arg(fileName);
+        QString command = QString("journalctl -o short-precise --boot -%1 >> %2-%1").arg(boot).arg(fileName);
         if(system(qPrintable(command)) != 0 && boot == 0) // at least one boot must be there
             return false;
     }
