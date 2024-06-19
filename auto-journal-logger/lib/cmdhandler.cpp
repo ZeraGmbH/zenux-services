db2e818339678195029f4f42e2c015c7190fba45
Andreas Müller <schnitzeltony@gmail.com>
2024-06-19 23:14:57 +0200

    auto-journal-logger: Rework folder/file name structure
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/lib/cmdhandler.cpp
+++ auto-journal-logger/lib/cmdhandler.cpp
@@ -16,7 +16,9 @@ void CmdHandler::StartCmd(SimpleCmdData *pCmd, QVariantList params)
     switch(static_cast<enum ControlCmdIds>(pCmd->GetCmdID())) {
     case CMD_SAVE_LOG_AND_DUMPS:
         QString hostName = FileUtils::getLocalHostname();
-        QString path = params[0].toString() + "/" + hostName + "/logs-and-dumps";
+        QDateTime now = QDateTime::currentDateTime();
+        QString dateTime = now.toString("yyyy_MM_dd-HH_mm_ss");
+        QString path = params[0].toString() + "/" + hostName + "/logs-and-dumps/" + dateTime;
         QDir dir;
         if(!dir.mkpath(path)) {
             emit OperationFinish(true, QStringLiteral("Could not create dir '%1'").arg(path));
@@ -33,8 +35,7 @@ void CmdHandler::StartCmd(SimpleCmdData *pCmd, QVariantList params)
                 emit OperationFinish(true, QStringLiteral("Version file %1 does not exist").arg(versionFilePath));
                 return;
             }
-            QDateTime now = QDateTime::currentDateTime();
-            QString fileName = path + "/" + now.toString("yyyy_MM_dd-HH_mm_ss") + "-version.json";
+            QString fileName = path + "/zenux-version.json";
             versionFile.copy(fileName);
         }
         emit OperationFinish(false, "");
