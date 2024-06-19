356ee63fdb2e2b557d45691ecc1ec6cfcc58f1e3
Andreas Müller <schnitzeltony@gmail.com>
2024-06-19 14:36:47 +0200

    auto-journal-logger: Add parameter & functionality to store file containing versions
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/lib/cmdhandler.cpp
+++ auto-journal-logger/lib/cmdhandler.cpp
@@ -2,6 +2,7 @@
 #include "cmdparser.h"
 #include "fileutils.h"
 #include <QDir>
+#include <QDateTime>
 
 CmdHandler::CmdHandler(AbstractLogCreatorPtr logGenerator, QObject *parent) :
     QSimpleCmdHandlerBase(parent),
@@ -21,8 +22,21 @@ void CmdHandler::StartCmd(SimpleCmdData *pCmd, QVariantList params)
             emit OperationFinish(true, QStringLiteral("Could not create dir '%1'").arg(path));
             return;
         }
-        if(!m_logGenerator->storeLogs(path))
+        if(!m_logGenerator->storeLogs(path)) {
             emit OperationFinish(true, QStringLiteral("Could not write journal").arg(path));
+            return;
+        }
+        QString versionFilePath = params[1].toString();
+        if(!versionFilePath.isEmpty()) {
+            QFile versionFile(versionFilePath);
+            if(!versionFile.exists()) {
+                emit OperationFinish(true, QStringLiteral("Version file %1 does not exist").arg(versionFilePath));
+                return;
+            }
+            QDateTime now = QDateTime::currentDateTime();
+            QString fileName = path + "/" + now.toString("yyyy_MM_dd-HH_mm_ss") + "-version.json";
+            versionFile.copy(fileName);
+        }
         emit OperationFinish(false, "");
         break;
     }
