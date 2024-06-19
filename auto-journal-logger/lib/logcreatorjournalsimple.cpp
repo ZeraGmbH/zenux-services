1aaed0391a7cc0c750046670fbba965ea5a96d61
Andreas Müller <schnitzeltony@gmail.com>
2024-06-19 20:12:02 +0200

    auto-journal-logger: Ignore failures on not first boot
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/lib/logcreatorjournalsimple.cpp
+++ auto-journal-logger/lib/logcreatorjournalsimple.cpp
@@ -7,7 +7,7 @@ bool LogCreatorJournalSimple::storeLogs(QString dir)
     QString fileName = dir + "/" + now.toString("yyyy_MM_dd-HH_mm_ss");
     for(int boot = 0; boot<3; boot++) {
         QString command = QString("journalctl -o short-precise --boot -%1 >> %2.log-%1").arg(boot).arg(fileName);
-        if(system(qPrintable(command)) != 0)
+        if(system(qPrintable(command)) != 0 && boot == 0) // at least one boot must be there
             return false;
     }
     return true;
