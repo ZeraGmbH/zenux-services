675749e758362ae0dc0f36ad95f8a2199070d4e9
Andreas Müller <schnitzeltony@gmail.com>
2024-06-20 16:30:27 +0200

    auto-journal-logger: Split out actions into separate functions
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- auto-journal-logger/lib/cmdhandler.cpp
+++ auto-journal-logger/lib/cmdhandler.cpp
@@ -21,41 +21,67 @@ void CmdHandler::StartCmd(SimpleCmdData *pCmd, QVariantList params)
         QDateTime now = QDateTime::currentDateTime();
         QString dateTime = now.toString("yyyy_MM_dd-HH_mm_ss");
         QString path = params[0].toString() + "/" + hostName + "/logs-and-dumps/" + dateTime;
-        QDir dir;
-        if(!dir.mkpath(path)) {
-            emit OperationFinish(true, QStringLiteral("Could not create dir '%1'").arg(path));
+
+        if(!makeDirWithParents(path))
             return;
-        }
-
-        if(!m_logGenerator->storeLogs(path)) {
-            emit OperationFinish(true, QStringLiteral("Could not write journal").arg(path));
+        if(!storeLogs(path))
             return;
-        }
-
-        QString coreFilePath = "/var/lib/systemd/coredump/";
-        QDir coreDir(coreFilePath);
-        QFileInfoList fileList = coreDir.entryInfoList(QDir::NoDotAndDotDot | QDir::Files);
-        for(auto &entry: fileList) {
-            QString outputPath = path + "/" + entry.fileName();
-            QString sourcePath = entry.absoluteFilePath();
-            QString cmd = QString("mv %1 %2").arg(sourcePath, outputPath);
-            if(system(qPrintable(cmd)) != 0) {
-                emit OperationFinish(true, QStringLiteral("Could not move core file %1 to %2").arg(sourcePath, outputPath));
-                return;
-            }
-        }
-
+        if(!storeCoreDumps(path))
+            return;
         QString versionFilePath = params[1].toString();
-        if(!versionFilePath.isEmpty()) {
-            QString fileName = path + "/zenux-version.json";
-            QString cmd = QString("mv %1 %2").arg(versionFilePath, fileName);
-            if(system(qPrintable(cmd)) != 0) {
-                emit OperationFinish(true, QStringLiteral("Could not move version file from %1 to %2").arg(versionFilePath, fileName));
-                return;
-            }
-        }
+        if(!storeVersionFile(path, versionFilePath))
+            return;
 
         emit OperationFinish(false, "");
         break;
     }
+}
+
+bool CmdHandler::makeDirWithParents(QString path)
+{
+    QDir dir;
+    if(!dir.mkpath(path)) {
+        emit OperationFinish(true, QStringLiteral("Could not create dir '%1'").arg(path));
+        return false;
+    }
+    return true;
+}
+
+bool CmdHandler::storeLogs(QString path)
+{
+    if(!m_logGenerator->storeLogs(path)) {
+        emit OperationFinish(true, QStringLiteral("Could not write journal").arg(path));
+        return false;
+    }
+    return true;
+}
+
+bool CmdHandler::storeCoreDumps(QString path)
+{
+    QString coreFilePath = "/var/lib/systemd/coredump/";
+    QDir coreDir(coreFilePath);
+    QFileInfoList fileList = coreDir.entryInfoList(QDir::NoDotAndDotDot | QDir::Files);
+    for(auto &entry : fileList) {
+        QString outputPath = path + "/" + entry.fileName();
+        QString sourcePath = entry.absoluteFilePath();
+        QString cmd = QString("mv %1 %2").arg(sourcePath, outputPath);
+        if(system(qPrintable(cmd)) != 0) {
+            emit OperationFinish(true, QStringLiteral("Could not move core file %1 to %2").arg(sourcePath, outputPath));
+            return false;
+        }
+    }
+    return true;
+}
+
+bool CmdHandler::storeVersionFile(QString path, QString versionFilePath)
+{
+    if(!versionFilePath.isEmpty()) {
+        QString fileName = path + "/zenux-version.json";
+        QString cmd = QString("mv %1 %2").arg(versionFilePath, fileName);
+        if(system(qPrintable(cmd)) != 0) {
+            emit OperationFinish(true, QStringLiteral("Could not move version file from %1 to %2").arg(versionFilePath, fileName));
+            return false;
+        }
+    }
+    return true;
 }
