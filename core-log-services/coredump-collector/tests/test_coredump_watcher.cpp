448beea7d9deb330498086da6719dbc38453df74
mdoerr2s <mika.doerr@smail.inf.h-brs.de>
2024-06-21 12:56:00 +0200

    core-log-services: Add tests and functionality to move core files
    - during move permissions for the category other are changed
    
    Signed-off-by: mdoerr2s <mika.doerr@smail.inf.h-brs.de>

--- core-log-services/coredump-collector/tests/test_coredump_watcher.cpp
+++ core-log-services/coredump-collector/tests/test_coredump_watcher.cpp
@@ -7,29 +7,114 @@
 
 QTEST_MAIN(test_coredump_watcher)
 
-const static char* testCoreDumpDir = "/tmp/test_coredump_watcher";
+const static char* testCoreDumpSourceDir = "/tmp/test_coredump_watcher/input";
+const static char* testCoreDumpDestDir = "/tmp/test_coredump_watcher/output";
 
 void test_coredump_watcher::init()
 {
     QDir dir;
-    dir.mkpath(testCoreDumpDir);
+    dir.mkpath(testCoreDumpSourceDir);
+    dir.mkpath(testCoreDumpDestDir);
 }
 
 void test_coredump_watcher::cleanup()
 {
-    QDir dir(testCoreDumpDir);
+    QDir dir(testCoreDumpSourceDir);
     dir.removeRecursively();
+    QDir destDir(testCoreDumpDestDir);
+    destDir.removeRecursively();
 }
 
-void test_coredump_watcher::test_initial_find_one()
+void test_coredump_watcher::test_single_core_file_ok()
 {
-    QFile file("core.vf-declarative-.10000.ignore.ignore.ignore.xz");
+    CoreDumpWatcher watcher(testCoreDumpSourceDir, testCoreDumpDestDir, QList<int>() << 10000 << 15000);
+    watcher.startWatching();
+    QSignalSpy spy(&watcher, &CoreDumpWatcher::sigCoredumpCreated);
+
+    writeTestCoreDump(10000, "vf-declarative-");
+
+    TimeMachineObject::feedEventLoop();
+    QFile file(QString(testCoreDumpDestDir) + "/core.vf-declarative-.10000.ignore.ignore.ignore.xz");
+    QFileDevice::Permissions pems  = QFileDevice::ReadOwner | QFileDevice::WriteOwner |
+                                    QFileDevice::ReadGroup |
+                                    QFileDevice::ReadOther | QFileDevice::WriteOther |
+                                    QFileDevice::WriteUser | QFileDevice::ReadUser;
+    QFileDevice::Permissions actual = file.permissions();
+    QCOMPARE(actual, pems);
+
+    QCOMPARE(spy.count(), 1);
+}
+
+void test_coredump_watcher::test_multiple_core_file_ok()
+{
+    CoreDumpWatcher watcher(testCoreDumpSourceDir, testCoreDumpDestDir, QList<int>() << 10000 << 15000);
+    watcher.startWatching();
+    QSignalSpy spy(&watcher, &CoreDumpWatcher::sigCoredumpCreated);
+
+    writeTestCoreDump(10000, "vf-declarative-");
+    writeTestCoreDump(10000, "vf-declarative2-");
+    writeTestCoreDump(10000, "vf-declarative3-");
+
+    TimeMachineObject::feedEventLoop();
+    QCOMPARE(spy.count(), 3);
+}
+
+
+
+
+void test_coredump_watcher::test_dir_instead_of_dump_file()
+{
+    CoreDumpWatcher watcher(testCoreDumpSourceDir, testCoreDumpDestDir, QList<int>() << 10000 << 15000);
+    watcher.startWatching();
+    QSignalSpy spy(&watcher, &CoreDumpWatcher::sigCoredumpCreated);
+
+    QDir testDir;
+    testDir.mkpath(QString(testCoreDumpSourceDir) + "/testDir/");
+
+    TimeMachineObject::feedEventLoop();
+    QCOMPARE(spy.count(), 0);
+}
+
+void test_coredump_watcher::test_only_filtered_uid_copy()
+{
+    CoreDumpWatcher watcher(testCoreDumpSourceDir, testCoreDumpDestDir, QList<int>() << 10000 << 15000);
+    watcher.startWatching();
+    QSignalSpy spy(&watcher, &CoreDumpWatcher::sigCoredumpCreated);
+
+    writeTestCoreDump(10000, "vf-declarative-");
+    writeTestCoreDump(10001, "vf-declarative2-");
+    writeTestCoreDump(15000, "vf-declarative3-");
+    writeTestCoreDump(15001, "vf-declarative3-");
+
+    TimeMachineObject::feedEventLoop();
+    QCOMPARE(spy.count(), 2);
+}
+
+void test_coredump_watcher::test_misnamed_core_file()
+{
+    CoreDumpWatcher watcher(testCoreDumpSourceDir, testCoreDumpDestDir, QList<int>() << 10000 << 15000);
+    watcher.startWatching();
+    QSignalSpy spy(&watcher, &CoreDumpWatcher::sigCoredumpCreated);
+
+    // introduce "." randomly into name
+    writeTestCoreDump(10000, "vf-declar.ative-");
+    // butcher the name completely
+    QFile file(QString(testCoreDumpSourceDir) + "/foo");
     QVERIFY(file.open(QFile::WriteOnly));
     QVERIFY(file.write("123"));
     file.close();
 
-    CoreDumpWatcher watcher(testCoreDumpDir, QList<int>() << 10000 << 15000);
-    QSignalSpy spy(&watcher, &CoreDumpWatcher::sigCoredumpCreated);
     TimeMachineObject::feedEventLoop();
-    QCOMPARE(spy.count(), 1);
+    QCOMPARE(spy.count(), 0);
+}
+
+bool test_coredump_watcher::writeTestCoreDump(int userId, QString programName)
+{
+    QFile file(QString(testCoreDumpSourceDir) + "/core." + programName + "." + QString::number(userId) + ".ignore.ignore.ignore.xz");
+    if(!file.open(QFile::WriteOnly))
+        return false;
+    if(!file.write("123"))
+        return false;
+    file.close();
+    return true;
 }
--- core-log-services/coredump-collector/tests/test_coredump_watcher.h
+++ core-log-services/coredump-collector/tests/test_coredump_watcher.h
@@ -9,9 +9,13 @@ class test_coredump_watcher : public QObject
 private slots:
     void init();
     void cleanup();
-    void test_initial_find_one();
+    void test_single_core_file_ok();
+    void test_multiple_core_file_ok();
+    void test_dir_instead_of_dump_file();
+    void test_only_filtered_uid_copy();
+    void test_misnamed_core_file();
 private:
-    bool writeTestCoreDump(int userId);
+    bool writeTestCoreDump(int userId, QString programName);
 };
 
 #endif // TEST_COREDUMP_WATCHER_H
