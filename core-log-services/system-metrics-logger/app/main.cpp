55ed47c55cd36b59fdffed407b875658b6b1f8a6
Andreas Müller <schnitzeltony@gmail.com>
2024-06-22 21:41:44 +0200

    Get system-metrics (no vein) from zera-classes
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ core-log-services/system-metrics-logger/app/main.cpp
@@ -0,0 +1,15 @@
+#include "systemmetrics.h"
+#include <QCoreApplication>
+#include <memory>
+
+int main( int argc, char *argv[] )
+{
+    std::unique_ptr<QCoreApplication> app = std::make_unique<QCoreApplication>(argc, argv);
+
+    std::unique_ptr<SystemMetrics> systemMetrics = std::make_unique<SystemMetrics>();
+    systemMetrics->startCpuLoadPollTimer(1000);
+    systemMetrics->startMemoryPollTimer(5000);
+    systemMetrics->getCpuLoad()->setWarningLimit(0.2);
+
+    return app->exec();
+}
