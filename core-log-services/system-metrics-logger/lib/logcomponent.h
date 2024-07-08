0e3a109ba198a1d19fb964c2b56fcad8ddf600dc
Andreas Müller <schnitzeltony@gmail.com>
2024-07-09 00:30:01 +0200

    LogComponent: Move LogStrategyMinMaxMean -> AbstractLogStrategy
    While at it: Get rid of unused QObject code fragments
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- core-log-services/system-metrics-logger/lib/logcomponent.h
+++ core-log-services/system-metrics-logger/lib/logcomponent.h
@@ -2,25 +2,20 @@
 #define LOGCOMPONENT_H
 
 #include "abstractlogvaluegetter.h"
-#include "logstrategyminmaxmean.h"
-
-#include <QObject>
+#include "abstractlogstrategy.h"
 #include <QList>
 #include <memory>
 
 class LogComponent
 {
-
 public:
-    LogComponent(std::unique_ptr<AbstractLogValueGetter> logValueGetter, std::unique_ptr<LogStrategyMinMaxMean> logStrategy);
-
+    LogComponent(std::unique_ptr<AbstractLogValueGetter> logValueGetter, std::unique_ptr<AbstractLogStrategy> logStrategy);
     QList<float> getBuffer();
-public slots:
     void tryLogOne();
 
 private:
     std::unique_ptr<AbstractLogValueGetter> m_logValueGetter;
-    std::unique_ptr<LogStrategyMinMaxMean> m_strategy;
+    std::unique_ptr<AbstractLogStrategy> m_strategy;
     QList<float> m_buffer;
 };
 
