fa798ae92327a216d0a4cd5fbcb58b47bc625614
mdoerr2s <m.doerr@zera.de>
2024-07-15 18:19:43 +0200

    logStrategyFuture: react on NaN values
    Signed-off-by: mdoerr2s <m.doerr@zera.de>

--- core-log-services/system-metrics-logger/lib/logstrategyfutureminmaxmean.cpp
+++ core-log-services/system-metrics-logger/lib/logstrategyfutureminmaxmean.cpp
@@ -1,28 +1,32 @@
 #include "logstrategyfutureminmaxmean.h"
 
-LogStrategyFutureMinMaxMean::LogStrategyFutureMinMaxMean(QString valueLabel, QString unitLabel) :
+LogStrategyFutureMinMaxMean::LogStrategyFutureMinMaxMean(QString valueLabel, QString unitLabel, std::function<void (QString)> loggingFunction) :
     m_valueLabel(valueLabel),
-    m_unitLabel(unitLabel)
+    m_unitLabel(unitLabel),
+    m_loggingFunction(loggingFunction)
 {
 }
 
 void LogStrategyFutureMinMaxMean::addValue(QList<float> &values, float newValue)
 {
-    if(!qIsNaN(newValue)) {
-        values.append(newValue);
-        if (values.size() >= 10) {
-            float min = *std::min_element(values.begin(), values.end());
-            float max = *std::max_element(values.begin(), values.end());
-            float mean = std::accumulate(values.begin(), values.end(), 0.0)/values.size();
-            qInfo("%s (%s) min: %.1f, max: %.1f, mean: %.1f",
-                  qPrintable(m_valueLabel), qPrintable(m_unitLabel), min, max, mean);
-            values.clear();
+    values.append(newValue);
+    if (values.size() >= 10) {
+        QList<float> noNaNValues;
+        for(auto value : values) {
+            if(!qIsNaN(value))
+                noNaNValues.append(value);
         }
-    }
-    else {
-        // keep the log rhythm... to be made beautiful
-        values.append(0.0);
-        if (values.size() >= 10)
-            values.clear();
+        if(!noNaNValues.isEmpty()) {
+            float min = *std::min_element(noNaNValues.begin(), noNaNValues.end());
+            float max = *std::max_element(noNaNValues.begin(), noNaNValues.end());
+            float mean = std::accumulate(noNaNValues.begin(), noNaNValues.end(), 0.0)/noNaNValues.size();
+            QString logString = QString("%1 (%2) min: %3, max: %4, mean: %5").
+                                arg(m_valueLabel, m_unitLabel).
+                                arg(min, 0, 'f', 1).
+                                arg(max, 0, 'f', 1).
+                                arg(mean, 0, 'f', 1);
+            m_loggingFunction(logString);
+        }
+        values.clear();
     }
 }
