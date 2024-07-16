7b381a9db3a50e85cc4c243bd0dfe122b2ded565
mdoerr2s <m.doerr@zera.de>
2024-07-16 08:42:27 +0200

    logStrategyMinMaxMean: consolidate MinMaxMean and FutureMinMaxMean together
    Signed-off-by: mdoerr2s <m.doerr@zera.de>

--- core-log-services/system-metrics-logger/lib/logstrategyminmaxmean.h
+++ core-log-services/system-metrics-logger/lib/logstrategyminmaxmean.h
@@ -2,15 +2,19 @@
 #define LOGSTRATEGYMINMAXMEAN_H
 
 #include "abstractlogstrategy.h"
+#include <functional>
 
 class LogStrategyMinMaxMean : public AbstractLogStrategy
 {
 public:
-    LogStrategyMinMaxMean(QString valueLabel, QString unitLabel);
+    LogStrategyMinMaxMean(QString valueLabel, QString unitLabel, std::function<void(QString)> loggingFunction = [](QString logString){
+            qInfo("%s",qPrintable(logString));
+        });
     void addValue(QList<float> &values, float newValue) override;
 private:
     QString m_valueLabel;
     QString m_unitLabel;
+    std::function<void(QString)> m_loggingFunction;
 };
 
 #endif // LOGSTRATEGYMINMAXMEAN_H
