063a31f17779d0efc7cf5120fc5facd5743c7647
mdoerr2s <m.doerr@zera.de>
2024-07-18 00:24:34 +0200

    plotter: enhance to handle dsp load and read write
    Signed-off-by: mdoerr2s <m.doerr@zera.de>

--- core-log-services/analysis-scripts/scripts/plotter/csvHandler.py
+++ core-log-services/analysis-scripts/scripts/plotter/csvHandler.py
@@ -5,7 +5,13 @@ def saveAsCSV(inputList, logType):
     Path("/tmp/plotter_out/").mkdir(parents=True, exist_ok=True)
     with open('/tmp/plotter_out/' + logType + '.csv', 'w') as file:
         writer = csv.writer(file)
-        writer.writerow(["datetime", logType.replace("_", " ")])
+        if inputList[0].value2:
+            writer.writerow(["datetime", logType.replace("_", " "), "dsp write"])
+        else:
+            writer.writerow(["datetime", logType.replace("_", " ")])
         for values in inputList:
-            writer.writerow([values.time, values.value])
+            if values.value2:
+                writer.writerow([values.time, values.value, values.value2])
+            else:
+                writer.writerow([values.time, values.value])
     return True    
--- core-log-services/analysis-scripts/scripts/plotter/plot.gnuplot
+++ core-log-services/analysis-scripts/scripts/plotter/plot.gnuplot
@@ -8,3 +8,9 @@ set key autotitle columnhead # use the first line as title
 set xlabel 'Time' # label for the X axis
 set ylabel "Measurement" # label for the Y axis
 plot filename using 1:2  smooth acsplines lt rgb "#00725c" lw 2
+
+if (multi == "1") {
+    plot filename using 1:2 smooth acsplines lt rgb "#00725c" lw 2, filename using 1:3 smooth acsplines lt rgb "#00725c" lw 2  
+}else{
+    plot filename using 1:2 smooth acsplines lt rgb "#00725c" lw 2
+}
