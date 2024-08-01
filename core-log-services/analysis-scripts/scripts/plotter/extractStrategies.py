be29a41b55a4164e8b27d6a66fd03ef12e3ae672
Andreas Müller <schnitzeltony@gmail.com>
2024-08-01 13:40:46 +0200

    extractStrategies.py: Extract common code to get_timestamp()
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- core-log-services/analysis-scripts/scripts/plotter/extractStrategies.py
+++ core-log-services/analysis-scripts/scripts/plotter/extractStrategies.py
@@ -1,5 +1,11 @@
 import structs
 
+def get_timestamp(input_text, search_label):
+    partsSplit = input_text.split(search_label)
+    partsSplit = partsSplit[0].rstrip().rsplit(" ", 1)
+    timeStamp = partsSplit[0].strip().split('.')[0]
+    return timeStamp
+
 def extract_min_max_mean(input_line):
     input_line = input_line.rstrip()
     splitString = input_line.rsplit(',',1)
@@ -10,10 +16,7 @@ def extract_min_max_mean(input_line):
             partsSplit = parts.split(' ')
             returnOutput.value = partsSplit[1]
         if "system-metrics-logger" in parts:
-            partsSplit = parts.split("system-metrics-logger")
-            partsSplit = partsSplit[0].rstrip().rsplit(" ", 1)
-            timeStamp = partsSplit[0].strip().split('.')[0]
-            returnOutput.time = timeStamp
+            returnOutput.time = get_timestamp(parts, "system-metrics-logger")
     return returnOutput
 
 def extract_dsp(input_line):
@@ -26,10 +29,7 @@ def extract_dsp(input_line):
             value = parts.rsplit(" ", 1)
             returnOutput.value = value[1].strip()
         if "zera-modulemanager" in parts:
-            partsSplit = parts.split("zera-modulemanager")
-            partsSplit = partsSplit[0].rstrip().rsplit(" ", 1)
-            timeStamp = partsSplit[0].strip().split('.')[0]
-            returnOutput.time = timeStamp
+            returnOutput.time = get_timestamp(parts, "zera-modulemanager")
     return returnOutput
 
 def extract_dsp_read_write(input_line):
@@ -45,8 +45,5 @@ def extract_dsp_read_write(input_line):
             value = parts.rstrip().rsplit(" ",1)[1].strip()
             returnOutput.value2 = value
         if "zdsp1d" in parts:
-            partsSplit = parts.split("zdsp1d")
-            partsSplit = partsSplit[0].rstrip().rsplit(" ", 1)
-            timeStamp = partsSplit[0].strip().split('.')[0]
-            returnOutput.time = timeStamp
+            returnOutput.time = get_timestamp(parts, "zdsp1d")
     return returnOutput
