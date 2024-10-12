88fdad2b00f4aabde488e1318a5feedd1ba25acb
Andreas Müller <schnitzeltony@gmail.com>
2024-10-12 22:42:03 +0200

    SystemMetrics::initLogComponents: Stop creating all logger objects twice
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- core-log-services/system-metrics-logger/lib/systemmetrics.cpp
+++ core-log-services/system-metrics-logger/lib/systemmetrics.cpp
@@ -32,31 +32,31 @@ void SystemMetrics::initLogComponents()
 
     currValueGetter = std::make_unique<CpuTemp>();
     if(currValueGetter->canGetValue())
-        m_logComponents.push_back(std::make_unique<LogComponent>(std::make_unique<CpuTemp>(), std::make_unique<LogStrategyMinMaxMean>("CPU Temperature", "°C")));
+        m_logComponents.push_back(std::make_unique<LogComponent>(std::move(currValueGetter), std::make_unique<LogStrategyMinMaxMean>("CPU Temperature", "°C")));
     else
         qWarning("CpuTemp does not work in this environment - ignore!");
 
     currValueGetter = std::make_unique<CpuLoad>(0);
     if(currValueGetter->canGetValue())
-        m_logComponents.push_back(std::make_unique<LogComponent>(std::make_unique<CpuLoad>(0), std::make_unique<LogStrategyMinMaxMean>("CPU Load", "%")));
+        m_logComponents.push_back(std::make_unique<LogComponent>(std::move(currValueGetter), std::make_unique<LogStrategyMinMaxMean>("CPU Load", "%")));
     else
         qWarning("CpuLoad does not work in this environment - ignore");
 
     currValueGetter = std::make_unique<CpuFreq>();
     if(currValueGetter->canGetValue())
-        m_logComponents.push_back(std::make_unique<LogComponent>(std::make_unique<CpuFreq>(), std::make_unique<LogStrategyMinMaxMean>("CPU Frequency", "MHz")));
+        m_logComponents.push_back(std::make_unique<LogComponent>(std::move(currValueGetter), std::make_unique<LogStrategyMinMaxMean>("CPU Frequency", "MHz")));
     else
         qWarning("CpuFrequency does not work in this environment - ignore");
 
     currValueGetter = std::make_unique<TotalMemoryTracker>();
     if(currValueGetter->canGetValue())
-        m_logComponents.push_back(std::make_unique<LogComponent>(std::make_unique<TotalMemoryTracker>(), std::make_unique<LogStrategyMinMaxMean>("RAM usage", "%")));
+        m_logComponents.push_back(std::make_unique<LogComponent>(std::move(currValueGetter), std::make_unique<LogStrategyMinMaxMean>("RAM usage", "%")));
     else
         qWarning("RAM usage does not work in this environment - ignore");
 
     currValueGetter = std::make_unique<FpgaInterrupts>();
     if(currValueGetter->canGetValue())
-        m_logComponents.push_back(std::make_unique<LogComponent>(std::make_unique<FpgaInterrupts>(), std::make_unique<LogStrategyMinMaxMean>("Fpga Interrupts", "interrupt/s")));
+        m_logComponents.push_back(std::make_unique<LogComponent>(std::move(currValueGetter), std::make_unique<LogStrategyMinMaxMean>("Fpga Interrupts", "interrupt/s")));
     else
         qWarning("FPGA interrupts do not work in this environment - ignore");
 
