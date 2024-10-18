5c8e7bd4afd9a46c9b8e63058b59922d2eb2d92d
Andreas Müller <schnitzeltony@gmail.com>
2024-10-18 18:17:29 +0200

    Introduce ProcPidIoDecoder
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ core-log-services/system-metrics-logger/lib/decoder/procpidiodecoder.cpp
@@ -0,0 +1,46 @@
+#include "procpidiodecoder.h"
+#include "systeminfofilelocator.h"
+#include <QFile>
+
+DiskValues ProcPidIoDecoder::getReadWrites(int pid)
+{
+    DiskValues values;
+    QString procDir = SystemInfoFileLocator::getProcBasePath();
+    if(!procDir.endsWith("/"))
+        procDir += "/";
+    const QString fileName = QString("%1%2/io").arg(procDir).arg(pid);
+    QFile ioFile(fileName);
+    if(ioFile.open(QFile::ReadOnly)) {
+        const QString contents = ioFile.readAll();
+        const QStringList lines = contents.split("\n");
+        for(const auto &line : lines) {
+            const QStringList lineParts = line.split(" ", Qt::SkipEmptyParts);
+            if(lineParts.size() == 2) {
+                if(lineParts[0] == "read_bytes:") {
+                    bool ok = false;
+                    quint64 val = lineParts[1].toULongLong(&ok);
+                    if(ok)
+                        values.bytesRead = val;
+                }
+                else if(lineParts[0] == "write_bytes:") {
+                    bool ok = false;
+                    quint64 val = lineParts[1].toULongLong(&ok);
+                    if(ok)
+                        values.bytesWritten = val;
+                }
+            }
+        }
+    }
+    return values;
+}
+
+QMap<int, DiskValues> ProcPidIoDecoder::getPidGroupReadWrites(QVector<int> pids)
+{
+    QMap<int, DiskValues> groupValues;
+    for(int pid : pids) {
+        DiskValues values = getReadWrites(pid);
+        if(values.bytesRead > 0 || values.bytesWritten > 0)
+            groupValues[pid] = values;
+    }
+    return groupValues;
+}
---
+++ core-log-services/system-metrics-logger/tests/test_proc_pid_io_decoder.cpp
@@ -0,0 +1,52 @@
+#include "test_proc_pid_io_decoder.h"
+#include "procpidiodecoder.h"
+#include "testsysteminfofilelocator.h"
+#include <QTest>
+
+QTEST_MAIN(test_proc_pid_io_decoder)
+
+void test_proc_pid_io_decoder::pidInvalid()
+{
+    TestSystemInfoFileLocator::setProcBasePath(":/proc_1");
+    DiskValues readWrites = ProcPidIoDecoder::getReadWrites(0);
+    QCOMPARE(readWrites.bytesRead, 0);
+    QCOMPARE(readWrites.bytesWritten, 0);
+}
+
+void test_proc_pid_io_decoder::pidValid1()
+{
+    TestSystemInfoFileLocator::setProcBasePath(":/proc_1");
+    DiskValues readWrites = ProcPidIoDecoder::getReadWrites(1);
+    QCOMPARE(readWrites.bytesRead, 0);
+    QCOMPARE(readWrites.bytesWritten, 20);
+}
+
+void test_proc_pid_io_decoder::pidValid1TrailingPathSeparator()
+{
+    TestSystemInfoFileLocator::setProcBasePath(":/proc_1/");
+    DiskValues readWrites = ProcPidIoDecoder::getReadWrites(1);
+    QCOMPARE(readWrites.bytesRead, 0);
+    QCOMPARE(readWrites.bytesWritten, 20);
+}
+
+void test_proc_pid_io_decoder::pidValid2()
+{
+    TestSystemInfoFileLocator::setProcBasePath(":/proc_1");
+    DiskValues readWrites = ProcPidIoDecoder::getReadWrites(2);
+    QCOMPARE(readWrites.bytesRead, 20);
+    QCOMPARE(readWrites.bytesWritten, 0);
+}
+
+void test_proc_pid_io_decoder::pidValid12_Invalid3()
+{
+    TestSystemInfoFileLocator::setProcBasePath(":/proc_1");
+    QVector<int> pids = QVector<int>() << 1 << 2 << 3;
+    QMap<int, DiskValues> readWrites = ProcPidIoDecoder::getPidGroupReadWrites(pids);
+    QCOMPARE(readWrites.size(), 2);
+    QVERIFY(readWrites.contains(1));
+    QVERIFY(readWrites.contains(2));
+    QCOMPARE(readWrites[1].bytesRead, 0);
+    QCOMPARE(readWrites[1].bytesWritten, 20);
+    QCOMPARE(readWrites[2].bytesRead, 20);
+    QCOMPARE(readWrites[2].bytesWritten, 0);
+}
