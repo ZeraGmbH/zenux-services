e3f625559fa118317a6ce012ffdfbaad9f438891
Andreas Müller <schnitzeltony@gmail.com>
2024-10-21 19:34:32 +0200

    LogStrategyDiskIoTopRanking: First working implementation
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- core-log-services/system-metrics-logger/tests/test_log_strategy_disk_io_top_ranking.h
+++ core-log-services/system-metrics-logger/tests/test_log_strategy_disk_io_top_ranking.h
@@ -7,10 +7,11 @@ class test_log_strategy_disk_io_top_ranking : public QObject
 {
     Q_OBJECT
 private slots:
-    void oneProcessWithValuesCreates100PercentReadWriteLines();
-    /*void twoProcessesTwoRankingsProduceFourLines();
-    void oneProcesseTwoRankingsProduceTwoLines();
-    void twoProcessesOneRankingsProduceTwoLines();*/
+    void multipleProcessesSameLoad();
+    void limitRankingDepth();
+    void multipleProcessesDifferentLoads();
+    void noLogNoRead();
+    void noLogNoWrite();
 };
 
 #endif // TEST_LOG_STRATEGY_DISK_IO_TOP_RANKING_H
