b23d781bd16ba5edbfc628bad18e4610457c7715
Andreas Müller <schnitzeltony@gmail.com>
2024-10-28 22:34:43 +0100

    test_regression_adj_status_com5003: Switch to mock network
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- com5003d/tests/test_regression_adj_status_com5003.cpp
+++ com5003d/tests/test_regression_adj_status_com5003.cpp
@@ -7,7 +7,7 @@
 #include "testfactoryi2cctrl.h"
 #include <timemachineobject.h>
 #include <mockeeprom24lc.h>
-#include <tcpnetworkfactory.h>
+#include <mocktcpnetworkfactory.h>
 #include <QTest>
 
 QTEST_MAIN(test_regression_adj_status_com5003);
@@ -57,7 +57,7 @@ void test_regression_adj_status_com5003::setupServers(AbstractFactoryI2cCtrlPtr 
 {
     PermissionFunctions::setPermissionCtrlFactory(ctrlFactory);
 
-    VeinTcp::AbstractTcpNetworkFactoryPtr tcpNetworkFactory = VeinTcp::TcpNetworkFactory::create();
+    VeinTcp::AbstractTcpNetworkFactoryPtr tcpNetworkFactory = VeinTcp::MockTcpNetworkFactory::create();
     m_resmanServer = std::make_unique<ResmanRunFacade>(tcpNetworkFactory);
     m_testServer = std::make_unique<TestServerForSenseInterfaceCom5003>(ctrlFactory, tcpNetworkFactory);
     TimeMachineObject::feedEventLoop();
