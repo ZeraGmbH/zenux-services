a25069378e2718a4e53f46b0a74a624b11eb91d5
i-sundermann <i.sundermann@zera.de>
2024-10-31 09:56:01 +0100

    i2cctrlcputemperature: More debug info to analyse i2c communication
    Signed-off-by: i-sundermann <i.sundermann@zera.de>

--- zenux-service-common/lib/micro-controller-io/i2cctrlcputemperature.cpp
+++ zenux-service-common/lib/micro-controller-io/i2cctrlcputemperature.cpp
@@ -16,8 +16,9 @@ ZeraMControllerIoTemplate::atmelRM I2cCtrlCpuTemperature::sendCpuTemperature(flo
 {
     ZeraMControllerIo::atmelRM ret;
 
-    if ((temperature < 0.0) || (temperature > 120.0)) {
+    if ((temperature < 0.0) || (temperature > 120000.0)) {
         ret = ZeraMControllerIo::cmdfault;
+        qInfo("temperatur out of limit");
     }
     else {
         quint32 ui32Temp;
@@ -27,6 +28,8 @@ ZeraMControllerIoTemplate::atmelRM I2cCtrlCpuTemperature::sendCpuTemperature(flo
         ba[1] = (ui32Temp >> 16) & 0xFF;
         ba[2] = (ui32Temp >> 8) & 0xFF;
         ba[3] = (ui32Temp & 0xFF);
+
+         qInfo("ba[0]:%i  ba[1]:%i  ba[2]:%i  ba[3]:%i", ba[0], ba[1], ba[2], ba[3]);
 
         hw_cmd CMD(hwSendTemperature, 0, ba, 4);        // ba fluppt so?
         m_ctrlIo.writeCommand(&CMD);
