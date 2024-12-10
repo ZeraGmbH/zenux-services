dbc0c4c90d5c7857710aadcfdb21b83b75840b4d
Andreas Müller <schnitzeltony@gmail.com>
2024-12-10 23:32:56 +0100

    dspinterface: Make addCycListItem(s) ovrridable
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-interfaces/lib/dspinterface.h
+++ zenux-service-interfaces/lib/dspinterface.h
@@ -31,8 +31,8 @@ public:
     quint32 cmdList2Dsp(); // send cyclic command list to the dsp server
     int cmdListCount(); // returns the number of command in cyclist program list
     void clearCmdList(); // clears all cmd lists
-    void addCycListItem(QString cmd); // appends new command to cyclic list
-    void addCycListItems(const QStringList &cmds); // appends new commands to cyclic list
+    virtual void addCycListItem(QString cmd); // appends new command to cyclic list
+    virtual void addCycListItems(const QStringList &cmds); // appends new commands to cyclic list
     cDspMeasData* getMemHandle(QString name); // init a new memory group and return handle
     void deleteMemHandle(cDspMeasData* memhandle);
     quint32 deactivateInterface(); // unload ...
