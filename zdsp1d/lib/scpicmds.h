baecf6b66c8d832529939122292e93fe4ee519b1
Andreas Müller <schnitzeltony@gmail.com>
2024-12-26 11:23:29 +0100

    Fix indentation & linefeeds
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zdsp1d/lib/scpicmds.h
+++ zdsp1d/lib/scpicmds.h
@@ -3,65 +3,64 @@
 #ifndef SCPIDSPS_H
 #define SCPIDSPS_H
 
-enum SCPICmdType  {	nixCmd, // 0 is nix
-			
-			// die routinen für das system modell
-						
-                                        	SetDspCommandStat,
-			GetDspCommandStat,
-			SetEN61850DestAdr,
-			GetEN61850DestAdr,
-			SetEN61850SourceAdr,
-			GetEN61850SourceAdr,
-			SetEN61850EthTypeAppId,
-			GetEN61850EthTypeAppId,
-		              SetEN61850PriorityTagged,
-			GetEN61850PriorityTagged,
-			SetEN61850EthSync,
-			GetEN61850EthSync,
-			SetEN61850DataCount,
-			GetEN61850DataCount,
-			SetEN61850SyncLostCount,
-			GetEN61850SyncLostCount,
-			TriggerIntListHKSK,
-			TriggerIntListALL,
-			ResetMaxima,
-            TestDsp,
-			ResetDsp,
-			BootDsp,
-			GetPCBSerialNumber,
-			GetDeviceVersion,
-			GetServerVersion,  
-			SetSamplingSystem,
-			GetSamplingSystem,
-			SetCommEncryption,
-                                          GetCommEncryption,
-					  
-			// die routinen für das status modell
-			ResetDeviceLoadMax,
-			GetDeviceLoadMax,
-			GetDeviceLoadAct,
-			GetDspStatus,
-			GetDeviceStatus,
-  
-			// die routinen für das measure modell
-    
-			Fetch,
-			Initiate,
-			UnloadCmdList,
-			LoadCmdList,
-			SetRavList,
-			GetRavList,
-			SetCmdIntList,
-			GetCmdIntList,
-			SetCmdList,
-			GetCmdList,
-			Measure,
-    
-			// die routinen für das memory modell
-    
-			DspMemoryRead,
-			DspMemoryWrite  };
+enum SCPICmdType  {
+    nixCmd, // 0 is nix
+
+    // die routinen für das system modell
+    SetDspCommandStat,
+    GetDspCommandStat,
+    SetEN61850DestAdr,
+    GetEN61850DestAdr,
+    SetEN61850SourceAdr,
+    GetEN61850SourceAdr,
+    SetEN61850EthTypeAppId,
+    GetEN61850EthTypeAppId,
+    SetEN61850PriorityTagged,
+    GetEN61850PriorityTagged,
+    SetEN61850EthSync,
+    GetEN61850EthSync,
+    SetEN61850DataCount,
+    GetEN61850DataCount,
+    SetEN61850SyncLostCount,
+    GetEN61850SyncLostCount,
+    TriggerIntListHKSK,
+    TriggerIntListALL,
+    ResetMaxima,
+    TestDsp,
+    ResetDsp,
+    BootDsp,
+    GetPCBSerialNumber,
+    GetDeviceVersion,
+    GetServerVersion,
+    SetSamplingSystem,
+    GetSamplingSystem,
+    SetCommEncryption,
+    GetCommEncryption,
+
+    // die routinen für das status modell
+    ResetDeviceLoadMax,
+    GetDeviceLoadMax,
+    GetDeviceLoadAct,
+    GetDspStatus,
+    GetDeviceStatus,
+
+    // die routinen für das measure modell
+    Fetch,
+    Initiate,
+    UnloadCmdList,
+    LoadCmdList,
+    SetRavList,
+    GetRavList,
+    SetCmdIntList,
+    GetCmdIntList,
+    SetCmdList,
+    GetCmdList,
+    Measure,
+
+    // die routinen für das memory modell
+    DspMemoryRead,
+    DspMemoryWrite
+};
 
 class cbIFace { // call back interface 
 public:    
