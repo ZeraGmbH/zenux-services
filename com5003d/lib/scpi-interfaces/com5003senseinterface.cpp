386dcc47555619912f921c493b6d48d55f7afa4c
Andreas Müller <schnitzeltony@gmail.com>
2025-03-28 14:53:45 +0100

    Sense interfaces: Cleanup range generation code I
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- com5003d/lib/scpi-interfaces/com5003senseinterface.cpp
+++ com5003d/lib/scpi-interfaces/com5003senseinterface.cpp
@@ -45,70 +45,56 @@ QList<SenseChannelCommon*> Com5003SenseInterface::setChannelAndRanges(cSenseSett
 {
     constexpr quint16 rangeFlagsDevice = modeAC;
     constexpr quint16 rangeFlagsReference = modeREF;
-    Q_UNUSED(adjData) // not use in for same signature as mt310s2
+    Q_UNUSED(adjData) // not use in com5003 for same signature as mt310s2
     //constexpr int rangeFlagsIntern = 0; // not used yet no clamp plugs
 
     QList<SenseSystem::cChannelSettings*> channelSettings = senseSettings->getChannelSettings();
     QList<SenseChannelCommon*> channels;
+    channels.append(new Com5003SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(0), 0, ctrlFactory));
+    channels.append(new Com5003SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(1), 1, ctrlFactory));
+    channels.append(new Com5003SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(2), 2, ctrlFactory));
+    channels.append(new Com5003SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(3), 3, ctrlFactory));
+    channels.append(new Com5003SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(4), 4, ctrlFactory));
+    channels.append(new Com5003SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(5), 5, ctrlFactory));
 
-    // default our sense has 3 voltage and 3 current measuring channels
-    SenseChannelCommon* pChannel;
-    pChannel = new Com5003SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(0), 0, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Com5003SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(1), 1, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Com5003SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(2), 2, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Com5003SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(3), 3, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Com5003SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(4), 4, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Com5003SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(5), 5, ctrlFactory);
-    channels.append(pChannel);
+    for (int i = 0; i < 3; i++) {
+        QList<SenseRangeCommon*> rngListU;
+        rngListU.append(new Com5003SenseRange(scpi,  "480V", true, 480.0, 4712563.0, 5890704.0, 0, rangeFlagsDevice));
+        rngListU.append(new Com5003SenseRange(scpi,  "240V", true, 240.0, 4712563.0, 5890704.0, 1, rangeFlagsDevice));
+        rngListU.append(new Com5003SenseRange(scpi,  "120V", true, 120.0, 4712563.0, 5890704.0, 2, rangeFlagsDevice));
+        rngListU.append(new Com5003SenseRange(scpi,   "60V", true,  60.0, 4712563.0, 5890704.0, 3, rangeFlagsDevice));
 
-    QList<SenseRangeCommon*> rngList;
+        rngListU.append(new Com5003SenseRange(scpi,   "12V", true,  12.0, 3887864.8, 4859831.0, 4, rangeFlagsDevice));
+        rngListU.append(new Com5003SenseRange(scpi,    "5V", true,   5.0, 4516206.0, 5645258.0, 5, rangeFlagsDevice));
 
-    int i;
-    for (i = 0; i < 3; i++)
-    {
-        rngList.clear();
-        rngList.append(new Com5003SenseRange(scpi,  "480V", true, 480.0, 4712563.0, 5890704.0, 0, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,  "240V", true, 240.0, 4712563.0, 5890704.0, 1, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,  "120V", true, 120.0, 4712563.0, 5890704.0, 2, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,   "60V", true,  60.0, 4712563.0, 5890704.0, 3, rangeFlagsDevice));
-
-        rngList.append(new Com5003SenseRange(scpi,   "12V", true,  12.0, 3887864.8, 4859831.0, 4, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,    "5V", true,   5.0, 4516206.0, 5645258.0, 5, rangeFlagsDevice));
-
-        rngList.append(new Com5003SenseRange(scpi,   "R0V", false,  9.0, 3839668.2, 5332873.0, 14, rangeFlagsReference));
-        rngList.append(new Com5003SenseRange(scpi,  "R10V", false, 10.0, 4266298.0, 5332873.0, 15, rangeFlagsReference));
-        channels.at(i)->setRangeList(rngList);
+        rngListU.append(new Com5003SenseRange(scpi,   "R0V", false,  9.0, 3839668.2, 5332873.0, 14, rangeFlagsReference));
+        rngListU.append(new Com5003SenseRange(scpi,  "R10V", false, 10.0, 4266298.0, 5332873.0, 15, rangeFlagsReference));
+        channels.at(i)->setRangeList(rngListU);
     }
 
-    for (i = 3; i < 6; i++)
-    {
-        rngList.clear();
-        rngList.append(new Com5003SenseRange(scpi,  "200A", true, 200.0, 6257236.0, 5256077.0, 0, rangeFlagsDevice)); // ???
-        rngList.append(new Com5003SenseRange(scpi,  "100A", true, 100.0, 4692928.0, 5866160.0, 1, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,   "50A", true,  50.0, 4692928.0, 5866160.0, 2, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,   "25A", true, 25.0 , 4692928.0, 5866160.0, 3, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,   "10A", true, 10.0 , 4692928.0, 5866160.0, 4, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,    "5A", true,  5.0 , 4692928.0, 5866160.0, 5, rangeFlagsDevice));
+    for (int i = 3; i < 6; i++) {
+        QList<SenseRangeCommon*> rngListI;
+        rngListI.append(new Com5003SenseRange(scpi,  "200A", true, 200.0, 6257236.0, 5256077.0, 0, rangeFlagsDevice)); // ???
+        rngListI.append(new Com5003SenseRange(scpi,  "100A", true, 100.0, 4692928.0, 5866160.0, 1, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,   "50A", true,  50.0, 4692928.0, 5866160.0, 2, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,   "25A", true, 25.0 , 4692928.0, 5866160.0, 3, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,   "10A", true, 10.0 , 4692928.0, 5866160.0, 4, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,    "5A", true,  5.0 , 4692928.0, 5866160.0, 5, rangeFlagsDevice));
 
-        rngList.append(new Com5003SenseRange(scpi,  "2.5A", true, 2.5  , 4692928.0, 5866160.0, 6, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,  "1.0A", true, 1.0  , 4692928.0, 5866160.0, 7, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi, "500mA", true, 0.5  , 4692928.0, 5866160.0, 8, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi, "250mA", true, 0.25 , 4692928.0, 5866160.0, 9, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi, "100mA", true, 0.1  , 4692928.0, 5866160.0, 10, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,  "50mA", true, 0.05 , 4692928.0, 5866160.0, 11, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,  "25mA", true, 0.025, 4692928.0, 5866160.0, 12, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,  "10mA", true, 0.01 , 4692928.0, 5866160.0, 13, rangeFlagsDevice));
-        rngList.append(new Com5003SenseRange(scpi,   "5mA", true, 0.005, 4692928.0, 5866160.0, 14, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,  "2.5A", true, 2.5  , 4692928.0, 5866160.0, 6, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,  "1.0A", true, 1.0  , 4692928.0, 5866160.0, 7, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi, "500mA", true, 0.5  , 4692928.0, 5866160.0, 8, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi, "250mA", true, 0.25 , 4692928.0, 5866160.0, 9, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi, "100mA", true, 0.1  , 4692928.0, 5866160.0, 10, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,  "50mA", true, 0.05 , 4692928.0, 5866160.0, 11, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,  "25mA", true, 0.025, 4692928.0, 5866160.0, 12, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,  "10mA", true, 0.01 , 4692928.0, 5866160.0, 13, rangeFlagsDevice));
+        rngListI.append(new Com5003SenseRange(scpi,   "5mA", true, 0.005, 4692928.0, 5866160.0, 14, rangeFlagsDevice));
 
-        rngList.append(new Com5003SenseRange(scpi,   "R0V", false,  9.0, 3839668.2, 5332873.0, 15, rangeFlagsReference));
-        rngList.append(new Com5003SenseRange(scpi,  "R10V", false, 10.0, 4266298.0, 5332873.0, 16, rangeFlagsReference));
+        rngListI.append(new Com5003SenseRange(scpi,   "R0V", false,  9.0, 3839668.2, 5332873.0, 15, rangeFlagsReference));
+        rngListI.append(new Com5003SenseRange(scpi,  "R10V", false, 10.0, 4266298.0, 5332873.0, 16, rangeFlagsReference));
 
-        channels.at(i)->setRangeList(rngList);
+        channels.at(i)->setRangeList(rngListI);
     }
     return channels;
 }
--- mt310s2d/lib/scpi-interfaces/mt310s2senseinterface.cpp
+++ mt310s2d/lib/scpi-interfaces/mt310s2senseinterface.cpp
@@ -45,165 +45,150 @@ QList<SenseChannelCommon*> Mt310s2SenseInterface::setChannelAndRanges(cSenseSett
 
     QList<SenseSystem::cChannelSettings*> channelSettings = senseSettings->getChannelSettings();
     QList<SenseChannelCommon*> channels;
+    channels.append(new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(0), 0, ctrlFactory));
+    channels.append(new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(1), 1, ctrlFactory));
+    channels.append(new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(2), 2, ctrlFactory));
+    channels.append(new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(6), 6, ctrlFactory));
+    channels.append(new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(3), 3, ctrlFactory));
+    channels.append(new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(4), 4, ctrlFactory));
+    channels.append(new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(5), 5, ctrlFactory));
+    channels.append(new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(7), 7, ctrlFactory));
 
-    // for com5003 our sense had 3 voltage and 3 current measuring channels
-    // for mt310 we need 4 voltage and 4 current measuring channels
-    SenseChannelCommon* pChannel;
-    pChannel = new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(0), 0, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(1), 1, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(2), 2, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Mt310s2SenseChannel(scpi, sVoltageChannelDescription,"V", channelSettings.at(6), 6, ctrlFactory);
-    channels.append(pChannel);
-
-    pChannel = new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(3), 3, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(4), 4, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(5), 5, ctrlFactory);
-    channels.append(pChannel);
-    pChannel = new Mt310s2SenseChannel(scpi, sCurrentChannelDescription,"A", channelSettings.at(7), 7, ctrlFactory);
-    channels.append(pChannel);
-
-    QList<SenseRangeCommon*> rngList;
-
-    int i;
-    for (i = 0; i < 4; i++) {
-        rngList.clear();
+    for (int i = 0; i < 4; i++) {
+        QList<SenseRangeCommon*> rngListUAll;
 
         // TODO:
         // * Do checks isInvalidAdjDataOrChannelRangeAvail only on ranges introduced later
 
         QString rangeName = "250V";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true, 250.0, 4415057.0, 5518821.0, 0, rangeFlagsDevice));
+            rngListUAll.append(new Mt310s2SenseRange(scpi, rangeName, true, 250.0, 4415057.0, 5518821.0, 0, rangeFlagsDevice));
         rangeName = "8V";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,   8.0, 3355443.0, 4194304.0, 1, rangeFlagsDevice));
+            rngListUAll.append(new Mt310s2SenseRange(scpi, rangeName, true,   8.0, 3355443.0, 4194304.0, 1, rangeFlagsDevice));
         rangeName = "100mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,   0.1, 4026532.0, 5033165.0, 2, rangeFlagsDevice));
+            rngListUAll.append(new Mt310s2SenseRange(scpi, rangeName, true,   0.1, 4026532.0, 5033165.0, 2, rangeFlagsDevice));
 
-        channels.at(i)->setRangeList(rngList);
+        channels.at(i)->setRangeList(rngListUAll);
     }
 
-    for (i = 4; i < 7; i++) {
-        rngList.clear();
+    for (int i = 4; i < 7; i++) {
+        QList<SenseRangeCommon*> rngListIExceptAux;
 
         QString rangeName = "10A";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,  10.0, 3197613.0, 3997016.0,  0, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,  10.0, 3197613.0, 3997016.0,  0, rangeFlagsDevice));
         rangeName = "5A";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,   5.0, 3197613.0, 3997016.0,  1, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,   5.0, 3197613.0, 3997016.0,  1, rangeFlagsDevice));
         rangeName = "2.5A";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,   2.5, 3997016.0, 4996270.0,  2, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,   2.5, 3997016.0, 4996270.0,  2, rangeFlagsDevice));
         rangeName = "1.0A";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,   1.0, 4177527.0, 5221909.0,  3, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,   1.0, 4177527.0, 5221909.0,  3, rangeFlagsDevice));
         rangeName = "500mA";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,   0.5, 4177527.0, 5221909.0,  4, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,   0.5, 4177527.0, 5221909.0,  4, rangeFlagsDevice));
         rangeName = "250mA";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,  0.25, 4177527.0, 5221909.0,  5, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,  0.25, 4177527.0, 5221909.0,  5, rangeFlagsDevice));
         rangeName = "100mA";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,   0.1, 4177527.0, 5221909.0,  6, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,   0.1, 4177527.0, 5221909.0,  6, rangeFlagsDevice));
         rangeName = "50mA";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true,  0.05, 4177527.0, 5221909.0,  7, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true,  0.05, 4177527.0, 5221909.0,  7, rangeFlagsDevice));
         rangeName = "25mA";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, true, 0.025, 4177527.0, 5221909.0,  8, rangeFlagsDevice));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, true, 0.025, 4177527.0, 5221909.0,  8, rangeFlagsDevice));
 
         rangeName = "8V";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  8.0, 3355443.0, 4194304.0,  9, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  8.0, 3355443.0, 4194304.0,  9, rangeFlagsIntern));
         rangeName = "5V";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  5.0, 4194304.0, 5242880.0, 10, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  5.0, 4194304.0, 5242880.0, 10, rangeFlagsIntern));
         rangeName = "2V";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  2.0, 2835586.0, 3544483.0, 11, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  2.0, 2835586.0, 3544483.0, 11, rangeFlagsIntern));
         rangeName = "1V";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  1.0, 2835586.0, 3544483.0, 12, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  1.0, 2835586.0, 3544483.0, 12, rangeFlagsIntern));
         rangeName = "500mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.5, 3544482.0, 4430603.0, 13, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.5, 3544482.0, 4430603.0, 13, rangeFlagsIntern));
         rangeName = "200mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.2, 2684355.0, 3355444.0, 14, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.2, 2684355.0, 3355444.0, 14, rangeFlagsIntern));
         rangeName = "100mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.1, 3355443.0, 4194304.0, 15, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.1, 3355443.0, 4194304.0, 15, rangeFlagsIntern));
         rangeName = "50mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.05, 3355443.0, 4194304.0, 16, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.05, 3355443.0, 4194304.0, 16, rangeFlagsIntern));
         rangeName = "20mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.02, 2684355.0, 3355444.0, 17, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.02, 2684355.0, 3355444.0, 17, rangeFlagsIntern));
         rangeName = "10mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.01, 3355443.0, 4194304.0, 18, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.01, 3355443.0, 4194304.0, 18, rangeFlagsIntern));
         rangeName = "5mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,0.005, 3355443.0, 4194304.0, 19, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,0.005, 3355443.0, 4194304.0, 19, rangeFlagsIntern));
         rangeName = "2mV";
         if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-            rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,0.002, 2684355.0, 3355444.0, 20, rangeFlagsIntern));
+            rngListIExceptAux.append(new Mt310s2SenseRange(scpi, rangeName, false,0.002, 2684355.0, 3355444.0, 20, rangeFlagsIntern));
 
-        channels.at(i)->setRangeList(rngList);
+        channels.at(i)->setRangeList(rngListIExceptAux);
     }
 
     // IAUX
-    rngList.clear();
+    constexpr int phase = 7;
+    QList<SenseRangeCommon*> rngListIAux;
     // rValue = 1e-12 (yes a pico Ampere error - for 100% range sample value):
     // We did not yet find the place but it seems 0.0 turns into a divisor causing DSP to run almost infinite
-    // loop on range change when running power1module on a channnel supportin 0A
-    rngList.append(new Mt310s2SenseRange(scpi, SenseRangeCommon::rangeNameCurrentNull, true,    1e-12, 3197613.0, 3997016.0,  0, rangeFlagsDevice));
+    // loop on range change when running power1module on a channnel supporting 0A
+    rngListIAux.append(new Mt310s2SenseRange(scpi, SenseRangeCommon::rangeNameCurrentNull, true,    1e-12, 3197613.0, 3997016.0,  0, rangeFlagsDevice));
     QString rangeName = "8V";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,   8.0, 3355443.0, 4194304.0,  9, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,   8.0, 3355443.0, 4194304.0,  9, rangeFlagsIntern));
     rangeName = "5V";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,   5.0, 4194304.0, 5242880.0, 10, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,   5.0, 4194304.0, 5242880.0, 10, rangeFlagsIntern));
     rangeName = "2V";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,   2.0, 2835586.0, 3544483.0, 11, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,   2.0, 2835586.0, 3544483.0, 11, rangeFlagsIntern));
     rangeName = "1V";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,   1.0, 2835586.0, 3544483.0, 12, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,   1.0, 2835586.0, 3544483.0, 12, rangeFlagsIntern));
     rangeName = "500mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,   0.5, 3544482.0, 4430603.0, 13, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,   0.5, 3544482.0, 4430603.0, 13, rangeFlagsIntern));
     rangeName = "200mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,   0.2, 2684355.0, 3355444.0, 14, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,   0.2, 2684355.0, 3355444.0, 14, rangeFlagsIntern));
     rangeName = "100mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,   0.1, 3355443.0, 4194304.0, 15, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,   0.1, 3355443.0, 4194304.0, 15, rangeFlagsIntern));
     rangeName = "50mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.05, 3355443.0, 4194304.0, 16, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.05, 3355443.0, 4194304.0, 16, rangeFlagsIntern));
     rangeName = "20mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.02, 2684355.0, 3355444.0, 17, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.02, 2684355.0, 3355444.0, 17, rangeFlagsIntern));
     rangeName = "10mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.01, 3355443.0, 4194304.0, 18, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false,  0.01, 3355443.0, 4194304.0, 18, rangeFlagsIntern));
     rangeName = "5mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.005, 3355443.0, 4194304.0, 19, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.005, 3355443.0, 4194304.0, 19, rangeFlagsIntern));
     rangeName = "2mV";
-    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(i)->getName(), rangeName))
-        rngList.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.002, 2684355.0, 3355444.0, 20, rangeFlagsIntern));
+    if(isInvalidAdjDataOrChannelRangeAvail(adjData, channels.at(phase)->getName(), rangeName))
+        rngListIAux.append(new Mt310s2SenseRange(scpi, rangeName, false, 0.002, 2684355.0, 3355444.0, 20, rangeFlagsIntern));
 
-    channels.at(7)->setRangeList(rngList);
+    channels.at(7)->setRangeList(rngListIAux);
     return channels;
 }
 
