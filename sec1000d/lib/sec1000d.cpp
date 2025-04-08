ZeraGmbH/zenux-services

commit c801f69dc5b309f22b7c6dc3afb87f481f666273

com5003d/mt310s2d/sec1000d: Move early setup to ctor
Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

sec1000d/lib/sec1000d.cpp
@@ -45,6 +45,7 @@ cSEC1000dServer::cSEC1000dServer(SettingsContainerPtr settings,
 {
     doConfiguration();
     init();
+    earlySetup();
 }
 
 void cSEC1000dServer::init()
@@ -163,8 +164,6 @@ void cSEC1000dServer::doSetupServer()
     }
     else
     {
-        earlySetup();
-
         SECServer = this;
 
         initSCPIConnections();

com5003d/lib/com5003d.cpp
@@ -29,6 +29,7 @@ cCOM5003dServer::cCOM5003dServer(SettingsContainerPtr settings,
 {
     doConfiguration();
     init();
+    earlySetup();
 }
 
 void cCOM5003dServer::init()
@@ -307,7 +308,6 @@ void cCOM5003dServer::earlySetup()
 void cCOM5003dServer::doSetupServer()
 {
     qInfo("Starting doSetupServer");
-    earlySetup();
 
     initSCPIConnections();
 

mt310s2d/lib/mt310s2d.cpp
@@ -41,6 +41,7 @@ cMT310S2dServer::cMT310S2dServer(SettingsContainerPtr settings,
 {
     doConfiguration();
     init();
+    earlySetup();
 }
 
 void cMT310S2dServer::init()
@@ -232,7 +233,6 @@ void cMT310S2dServer::doSetupServer()
             emit abortInit();
         }
         else {
-            earlySetup();
             initSCPIConnections();
 
             // after init. we once poll the devices connected at power up

commit f5e0047a3ab30b36bf3df475afa954a8f272c1dc

sec1000d: Fix regression
This was over-enthusiasically introduced in

commit c801f69dc5b309f22b7c6dc3afb87f481f666273
Author: Andreas Müller <schnitzeltony@gmail.com>
Date:   Thu Apr 3 14:00:44 2025 +0200

    com5003d/mt310s2d/sec1000d: Move early setup to ctor

Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

sec1000d/lib/sec1000d.cpp
@@ -49,7 +49,6 @@ cSEC1000dServer::cSEC1000dServer(SettingsContainerPtr settings,
 {
     doConfiguration(ecUnitCount);
     init();
-    earlySetup();
 }
 
 void cSEC1000dServer::init()
@@ -168,6 +167,8 @@ void cSEC1000dServer::doSetupServer()
     }
     else
     {
+        earlySetup();
+
         SECServer = this;
 
         initSCPIConnections();