99cfccda48bbaeb2b6b7fa719afbbee8d9191a10
Andreas Müller <schnitzeltony@gmail.com>
2025-04-14 23:09:47 +0200

    test_regression_sec1000: Introduce a test on interrupt notification count
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- sec1000d/tests/test_regression_sec1000.cpp
+++ sec1000d/tests/test_regression_sec1000.cpp
@@ -82,8 +82,71 @@ void test_regression_sec1000::interruptDeviceNodeIo()
     QVERIFY(TestLogHelpers::compareAndLogOnDiff(expected, dumped));
 }
 
+void test_regression_sec1000::interruptNotifications()
+{
+    QFETCH_GLOBAL(int, ecUnitCount);
+    QStringList ecalChannels = claimAllEcChannels(ecUnitCount);
+
+    int answersOnRegister = 0;
+    collectServerSends(answersOnRegister);
+    registerInterruptNotifiers(ecalChannels);
+    QCOMPARE(answersOnRegister, ecalChannels.count());
+
+    prepareInterruptMaskValue(ecalChannels);
+    int serverNotifications = 0;
+    collectServerSends(serverNotifications);
+    fireInterrupt();
+    QCOMPARE(serverNotifications, ecalChannels.count());
+}
+
 void test_regression_sec1000::fireInterrupt()
 {
     kill(getpid(), SIGIO);
     TimeMachineObject::feedEventLoop();
 }
+
+QStringList test_regression_sec1000::claimAllEcChannels(int ecUnitCount)
+{
+    QString scpiClaimEcChannels = QString("ECAL:SET");
+    QString ecalChannelsStr = ScpiSingleTransactionBlocked::cmd(scpiClaimEcChannels, QString("%1").arg(ecUnitCount), 6305);
+    QStringList ecalChannels = ecalChannelsStr.split(";", Qt::SkipEmptyParts);
+    return ecalChannels;
+}
+
+void test_regression_sec1000::collectServerSends(int &serverSendCount)
+{
+    connect(m_secIFace.get(), &AbstractServerInterface::serverAnswer, this, [&](quint32 msgnr, quint8 reply, QVariant answer) {
+        Q_UNUSED(msgnr)
+        Q_UNUSED(reply)
+        Q_UNUSED(answer)
+        serverSendCount++;
+    });
+}
+
+void test_regression_sec1000::registerInterruptNotifiers(QStringList ecalChannels)
+{
+    for(const QString &ecChannel : ecalChannels) {
+        // This assembly language was stolen from secmodule (and friends)...
+        QString scpi = QString("ECAL:%1:R%2?").arg(ecChannel).arg(ECALCREG::INTREG);
+        m_secIFace->registerNotifier(scpi);
+    }
+    TimeMachineObject::feedEventLoop();
+}
+
+void test_regression_sec1000::prepareInterruptMaskValue(QStringList ecalChannels)
+{
+    QByteArray interruptResponseValues;
+    // Wow: In NotificationValue::setValue we learn that notifications are spawned on
+    // rising edge of interrupt flags only (makes sense but is hidden to dump readers like
+    // me). So hack some rising edges here...
+    for(int ecChannelNum = 0; ecChannelNum<ecalChannels.count(); ecChannelNum++) {
+        if(ecChannelNum % 2 == 0)
+            interruptResponseValues.append(0x0F);
+        else
+            interruptResponseValues.back()= 0xFF;
+    }
+    AbstractDeviceNodeSecPtr deviceNode = MockSingletonDeviceNodeSec::getInstancePtr();
+    MockDeviceNodeSec *secDeviceNode = static_cast<MockDeviceNodeSec*>(deviceNode.get());
+    secDeviceNode->setNextReadValue(interruptResponseValues);
+}
+
