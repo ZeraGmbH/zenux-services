fef5a5f6186562936455f9576a4888a734d118a4
amutalik <a.mutalik@zera.de>
2025-10-29 16:22:35 +0100

    Rename 'DelayedZeraMControllerTask' to 'AsyncZeraMControllerTask'
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- zenux-service-common/lib/micro-controller-io/asyncqueuedmcontrollerio.cpp
+++ zenux-service-common/lib/micro-controller-io/asyncqueuedmcontrollerio.cpp
@@ -1,15 +1,15 @@
 #include "asyncqueuedmcontrollerio.h"
-#include "delayedzeramcontrollertask.h"
+#include "asynczeramcontrollertask.h"
 
 void AsyncQueuedMControllerIo::startAsyncRangeIo(AbstractZeraMControllerDelayedPtr mcontroller,
                                                             ProtonetCommandPtr protoCmd,
                                                             const QString &rangeName)
 {
-    std::unique_ptr<DelayedZeraMControllerTask> task = DelayedZeraMControllerTask::create(
+    std::unique_ptr<AsyncZeraMControllerTask> task = AsyncZeraMControllerTask::create(
         std::move(mcontroller),
         protoCmd,
         rangeName);
-    connect(task.get(), &DelayedZeraMControllerTask::sigCmdDone, this, [this](ProtonetCommandPtr protoCmd, QString rangeName) {
+    connect(task.get(), &AsyncZeraMControllerTask::sigCmdDone, this, [this](ProtonetCommandPtr protoCmd, QString rangeName) {
         emit sigCmdDone(protoCmd, rangeName);
     });
     m_pendingTaskContainer.addSub(std::move(task));
