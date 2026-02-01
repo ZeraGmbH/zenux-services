4ac203f2e3c17f3c04be412bfae61cac4dae3585
Andreas Müller <schnitzeltony@gmail.com>
2026-02-01 13:36:00 +0100

    ScpiDelegate: Use more const ref parameters on construction
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zenux-service-common/lib/scpi-protobuf/scpidelegate.h
+++ zenux-service-common/lib/scpi-protobuf/scpidelegate.h
@@ -12,14 +12,14 @@ class ScpiDelegate: public QObject, public ScpiObject
 {
    Q_OBJECT
 public:
-    static std::shared_ptr<ScpiDelegate> create(QString cmdParent,
-                                                QString cmd,
+    static std::shared_ptr<ScpiDelegate> create(const QString &cmdParent,
+                                                const QString &cmd,
                                                 quint8 type,
                                                 std::shared_ptr<cSCPI> scpiInterface,
                                                 quint16 cmdCode,
                                                 NotificationString *notificationString = nullptr);
-    ScpiDelegate(QString cmdParent,
-                 QString cmd,
+    ScpiDelegate(const QString &cmdParent,
+                 const QString &cmd,
                  quint8 type,
                  quint16 cmdCode,
                  NotificationString *notificationString);
