67071718a934bf0b5ef5ca49895dfac5ca21521c
Andreas Müller <schnitzeltony@gmail.com>
2026-04-24 17:53:19 +0200

    JsonParamApi: Add setters for test / fix warnings on getters
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- source-control/lib-source-json-io/jsonparamapi.cpp
+++ source-control/lib-source-json-io/jsonparamapi.cpp
@@ -27,13 +27,29 @@ void JsonParamApi::setOn(bool on)
 double JsonParamApi::getRms(phaseType type, int phaseIdxBase0) const
 {
     QString phaseName = getPhaseName(type, phaseIdxBase0);
-    return m_params[phaseName].toObject()["rms"].toDouble(0.0);
+    return m_params[phaseName].toObject().value("rms").toDouble(0.0);
+}
+
+void JsonParamApi::setRms(phaseType type, int phaseIdxBase0, double value)
+{
+    QString phaseName = getPhaseName(type, phaseIdxBase0);
+    QJsonObject phaseValue = m_params[phaseName].toObject();
+    phaseValue["rms"] = value;
+    m_params[phaseName] = phaseValue;
 }
 
 double JsonParamApi::getAngle(phaseType type, int phaseIdxBase0) const
 {
     QString phaseName = getPhaseName(type, phaseIdxBase0);
-    return m_params[phaseName].toObject()["angle"].toDouble(0.0);
+    return m_params[phaseName].toObject().value("angle").toDouble(0.0);
+}
+
+void JsonParamApi::setAngle(phaseType type, int phaseIdxBase0, double value)
+{
+    QString phaseName = getPhaseName(type, phaseIdxBase0);
+    QJsonObject phaseValue = m_params[phaseName].toObject();
+    phaseValue["angle"] = value;
+    m_params[phaseName] = phaseValue;
 }
 
 bool JsonParamApi::getOn(phaseType type, int phaseIdxBase0) const
@@ -43,22 +59,37 @@ bool JsonParamApi::getOn(phaseType type, int phaseIdxBase0) const
     return obj.contains("on") && obj["on"].toBool();
 }
 
+void JsonParamApi::setOn(phaseType type, int phaseIdxBase0, bool on)
+{
+    QString phaseName = getPhaseName(type, phaseIdxBase0);
+    QJsonObject phaseValue = m_params[phaseName].toObject();
+    phaseValue["on"] = on;
+    m_params[phaseName] = phaseValue;
+}
+
 bool JsonParamApi::getFreqVarOn() const
 {
-    return m_params["Frequency"].toObject()["type"] == "var";
+    return m_params["Frequency"].toObject().value("type") == "var";
 }
 
 double JsonParamApi::getFreqVal() const
 {
-    return m_params["Frequency"].toObject()["val"].toDouble();
+    return m_params["Frequency"].toObject().value("val").toDouble();
 }
 
-QString JsonParamApi::getPhaseName(phaseType type, int phaseIdxBase0) const
+void JsonParamApi::setFreqVal(double value)
+{
+    QJsonObject freqValue = m_params["Frequency"].toObject();
+    freqValue["val"] = value;
+    m_params["Frequency"] = freqValue;
+}
+
+QString JsonParamApi::getPhaseName(phaseType type, int phaseIdxBase0)
 {
     return QString("%1%2").arg(getPhaseNamePrefix(type)).arg(phaseIdxBase0+1);
 }
 
-QString JsonParamApi::getPhaseNamePrefix(phaseType type) const
+QString JsonParamApi::getPhaseNamePrefix(phaseType type)
 {
     switch(type) {
     case phaseType::U:
--- source-control/lib-source-json-io/jsonparamapi.h
+++ source-control/lib-source-json-io/jsonparamapi.h
@@ -14,17 +14,27 @@ public:
     JsonParamApi();
     QJsonObject getParams() const;
     void setParams(QJsonObject params);
+
     bool getOn() const;
     void setOn(bool on);
+
     double getRms(phaseType type, int phaseIdxBase0) const;
+    void setRms(phaseType type, int phaseIdxBase0, double value);
+
     double getAngle(phaseType type, int phaseIdxBase0) const;
+    void setAngle(phaseType type, int phaseIdxBase0, double value);
+
     bool getOn(phaseType type, int phaseIdxBase0) const;
+    void setOn(phaseType type, int phaseIdxBase0, bool on);
+
     bool getFreqVarOn() const;
+
     double getFreqVal() const;
+    void setFreqVal(double value);
 
 private:
-    QString getPhaseName(phaseType type, int phaseIdxBase0) const;
-    QString getPhaseNamePrefix(phaseType type) const;
+    static QString getPhaseName(phaseType type, int phaseIdxBase0);
+    static QString getPhaseNamePrefix(phaseType type);
     QJsonObject m_params;
 };
 
