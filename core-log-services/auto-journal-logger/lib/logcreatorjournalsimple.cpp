8768d41dbe6676df6790f74e79c2108021447c86
Andreas Müller <schnitzeltony@gmail.com>
2026-07-21 09:13:35 +0200

    Revert "LogCreatorJournalSimple::storeLogs: Use journal2html.py"
    This reverts commit 2a12d252b05892f2b7833a92fff8611081fa157d.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- core-log-services/auto-journal-logger/lib/logcreatorjournalsimple.cpp
+++ core-log-services/auto-journal-logger/lib/logcreatorjournalsimple.cpp
@@ -6,7 +6,7 @@ bool LogCreatorJournalSimple::storeLogs(QString destinationDir)
     QString fileName = destinationDir + "/journal.log";
     for(int boot = 0; boot<3; boot++) {
         // Suggested display command: 'less -R <logfile>'
-        QString command = QString("journal2html.py -o short-precise --boot -%1 '%2-%1.html'").arg(boot).arg(fileName);
+        QString command = QString("SYSTEMD_COLORS=1 journalctl -o short-precise --boot -%1 >> '%2-%1'").arg(boot).arg(fileName);
         if(system(qPrintable(command)) != 0 && boot == 0) // at least one boot must be there
             return false;
     }
