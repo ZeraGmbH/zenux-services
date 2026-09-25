1b82902524b51b8afaf51981d94db51cf6d49ec4
Andreas Müller <schnitzeltony@gmail.com>
2026-09-25 09:40:22 +0200

    logcreatorjournalsimple: Format journal short-monotonic
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- core-log-services/auto-journal-logger/lib/logcreatorjournalsimple.cpp
+++ core-log-services/auto-journal-logger/lib/logcreatorjournalsimple.cpp
@@ -1,12 +1,10 @@
 #include "logcreatorjournalsimple.h"
-#include <QDateTime>
 
 bool LogCreatorJournalSimple::storeLogs(QString destinationDir)
 {
     QString fileName = destinationDir + "/journal.log";
     for(int boot = 0; boot<3; boot++) {
-        // Suggested display command: 'less -R <logfile>'
-        QString command = QString("SYSTEMD_COLORS=1 journalctl -o short-precise --boot -%1 >> '%2-%1'").arg(boot).arg(fileName);
+        QString command = QString("SYSTEMD_COLORS=1 journalctl -o short-monotonic --boot -%1 >> '%2-%1'").arg(boot).arg(fileName);
         if(system(qPrintable(command)) != 0 && boot == 0) // at least one boot must be there
             return false;
     }
